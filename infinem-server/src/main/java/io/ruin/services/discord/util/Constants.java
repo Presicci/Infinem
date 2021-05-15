9ac036bf0160587dd36eb103bd21aaaaef9ae741
Thomas Presicci <mrbennjerry@gmail.com>
2021-05-14 22:10:20 -0500

    Added discord webhook url

--- infinem-server/src/main/java/io/ruin/services/discord/util/Constants.java
+++ infinem-server/src/main/java/io/ruin/services/discord/util/Constants.java
@@ -2,6 +2,6 @@ package io.ruin.services.discord.util;
 
 public class Constants {
 
-    public static String WEBHOOK_URL = "";
+    public static String WEBHOOK_URL = "https://discord.com/api/webhooks/842960586421829633/2sKHQnJqRH9kzjVG40OYG3Ft7aJnLML91byDVIALBcRShDPbcIAGpWpRdehLZT61THNG";
 
 }
