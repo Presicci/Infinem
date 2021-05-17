d155712d0ae30b59e4b83a688b2585ff810c2c80
Thomas Presicci <mrbennjerry@gmail.com>
2021-05-17 00:43:29 -0500

    Added emotes to dart and bolt making, fixed some xp rates

--- infinem-server/src/main/java/io/ruin/model/skills/fletching/Dart.java
+++ infinem-server/src/main/java/io/ruin/model/skills/fletching/Dart.java
@@ -11,23 +11,23 @@ import static io.ruin.model.skills.Tool.FEATHER;
 
 public enum Dart {
 
-    BRONZE_DART(806, 819, 1, 1.8),
-    IRON_DART(807, 820, 22, 3.8),
-    STEEL_DART(808, 821, 37, 7.5),
-    MITHRIL_DART(809, 822, 52, 11.2),
-    ADAMANT_DART(810, 823, 67, 15.0),
-    RUNE_DART(811, 824, 81, 18.8),
-    DRAGON_DART(11230, 11232, 95, 25.0),
+    BRONZE_DART(806, 819, 1, 1.8, 8482),
+    IRON_DART(807, 820, 22, 3.8, 8483),
+    STEEL_DART(808, 821, 37, 7.5, 8484),
+    MITHRIL_DART(809, 822, 52, 11.2, 8485),
+    ADAMANT_DART(810, 823, 67, 15.0, 8486),
+    RUNE_DART(811, 824, 81, 18.8, 8487),
+    DRAGON_DART(11230, 11232, 95, 25.0, 8488),
 
-    BRONZE_BOLT(877, 9375, 9, 0.5),
-    IRON_BOLT(9140, 9377, 39, 1.5),
-    SILVER_BOLT(9145, 9382, 43, 3.0),
-    STEEL_BOLT(9141, 9378, 46, 3.5),
-    BROAD_BOLT(11875, 11876, 55, 2.5),
-    MITHRIL_BOLT(9142, 9379, 54, 5.0),
-    ADAMANT_BOLT(9143, 9380, 61, 7.0),
-    RUNE_BOLT(9144, 9381, 69, 10.0),
-    DRAGON_BOLTS(21905, 21930, 84, 12.0),
+    BRONZE_BOLT(877, 9375, 9, 0.5, 8463),
+    IRON_BOLT(9140, 9377, 39, 1.5, 8464),
+    SILVER_BOLT(9145, 9382, 43, 2.5, 8467),
+    STEEL_BOLT(9141, 9378, 46, 3.5, 8467),
+    BROAD_BOLT(11875, 11876, 55, 3, 8464),
+    MITHRIL_BOLT(9142, 9379, 54, 5.0, 8468),
+    ADAMANT_BOLT(9143, 9380, 61, 7.0, 8469),
+    RUNE_BOLT(9144, 9381, 69, 10.0, 8470),
+    DRAGON_BOLTS(21905, 21930, 84, 12.0, 8471),
     ;
 
     public final int lvlReq;
@@ -38,12 +38,15 @@ public enum Dart {
 
     public final String pluralName;
 
-    Dart(int dartId, int tipId, int lvlReq, double xp) {
+    public final int emote;
+
+    Dart(int dartId, int tipId, int lvlReq, double xp, int emote) {
         this.lvlReq = lvlReq;
         this.xp = xp;
         this.unfinishedId = tipId;
         this.finishedId = dartId;
         this.pluralName = ItemDef.get(dartId).name.toLowerCase() + (ItemDef.get(dartId).name.endsWith("s") ? "" : "s");
+        this.emote = emote;
     }
 
     private void make(Player player, Item tipItem, Item featherItem) {
@@ -57,6 +60,7 @@ public enum Dart {
         int toMake = Math.min(supplyCount, 10);
         tipItem.remove(toMake);
         featherItem.remove(toMake);
+        player.animate(emote);
         player.getInventory().add(finishedId, toMake);
         player.getStats().addXp(StatType.Fletching, xp * toMake, true);
         player.sendFilteredMessage("You make " + toMake + " " + pluralName + ".");
