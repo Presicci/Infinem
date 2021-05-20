e2f7996d3cda17b8e38eaca9712878ce8a627cb0
Thomas Presicci <mrbennjerry@gmail.com>
2021-05-20 02:40:39 -0500

    Added method for reading other format of xteas

--- infinem-server/src/main/java/io/ruin/data/impl/region_keys.java
+++ infinem-server/src/main/java/io/ruin/data/impl/region_keys.java
@@ -18,8 +18,18 @@ public class region_keys extends DataFile {
 
     public static final int[] NULL_KEYS = new int[4];
 
+    /*@Override
+    public String path() {
+        return "xteas.json";
+    }*/
+
+    private static final boolean xteasJson = false;
+
     @Override
     public String path() {
+        if (xteasJson) {
+            return "xteas.json";
+        }
         return "region_keys.json";
     }
 
@@ -31,36 +41,40 @@ public class region_keys extends DataFile {
     @Override
     public Object fromJson(String fileName, String json) {
 
-        Map<Integer, int[]> keys = JsonUtils.fromJson(json, Map.class, Integer.class, int[].class);
-        for(int regionId = 0; regionId < Region.LOADED.length; regionId++) {
-            Region region = new Region(regionId);
-            if((region.keys = keys.get(regionId)) != null && !isValid(region.id, region.keys)) {
-                //System.err.println("Invalid Keys for Region (" + regionId + "): base=(" + region.baseX + ", " + region.baseY + ") keys=" + Arrays.toString(region.keys));
-                region.keys = null;
+        Map<Integer, int[]> key_map;
+        if (xteasJson) {
+            key_map = Maps.newHashMap();
+            JsonArray array = (JsonArray) JsonParser.parseString(json);
+            Gson builder = new GsonBuilder().create();
+
+            //Load keys into map
+            for (int i = 0; i < array.size(); i++) {
+                JsonObject jObject = (JsonObject) array.get(i);
+                int regionId = jObject.get("mapsquare").getAsInt();
+                int[] keys = builder.fromJson(jObject.getAsJsonArray("key"), int[].class);
+                key_map.put(regionId, keys);
             }
-            Region.LOADED[regionId] = region;
+            //Populate regions list
+            for (int i = 0; i < Region.LOADED.length; i++) {
+                Region region = new Region(i);
+                if ((region.keys = key_map.get(i)) != null && !isValid(region.id, region.keys)) {
+                    region.keys = null; //Invalid keys
+                }
+                Region.LOADED[i] = region;
+            }
+        } else {
+            key_map = JsonUtils.fromJson(json, Map.class, Integer.class, int[].class);
+            for(int regionId = 0; regionId < Region.LOADED.length; regionId++) {
+                Region region = new Region(regionId);
+                if((region.keys = key_map.get(regionId)) != null && !isValid(region.id, region.keys)) {
+                    //System.err.println("Invalid Keys for Region (" + regionId + "): base=(" + region.baseX + ", " + region.baseY + ") keys=" + Arrays.toString(region.keys));
+                    region.keys = null;
+                }
+                Region.LOADED[regionId] = region;
+            }
         }
 
-//        Map<Integer, int[]> key_map = Maps.newHashMap();
-//        JsonParser parser = new JsonParser();
-//        JsonArray array = (JsonArray) parser.parse(json);
-//        Gson builder = new GsonBuilder().create();
-//
-//        //Load keys into map
-//        for (int i = 0; i < array.size(); i++) {
-//            JsonObject jObject = (JsonObject) array.get(i);
-//            int regionId = jObject.get("mapsquare").getAsInt();
-//            int[] keys = builder.fromJson(jObject.getAsJsonArray("key"), int[].class);
-//            key_map.put(regionId, keys);
-//        }
-//        //Populate regions list
-//        for (int i = 0; i < Region.LOADED.length; i++) {
-//            Region region = new Region(i);
-//            if ((region.keys = key_map.get(i)) != null && !isValid(region.id, region.keys)) {
-//                region.keys = null; //Invalid keys
-//            }
-//            Region.LOADED[i] = region;
-//        }
+
         //Load each region
         for(Region region : Region.LOADED) {
             try {
@@ -73,7 +87,7 @@ public class region_keys extends DataFile {
 
         MultiZone.load();
         DynamicMap.load();
-        return keys;
+        return key_map;
     }
 
     private static boolean isValid(int id, int[] keys) {
