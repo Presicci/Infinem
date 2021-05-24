d57a58c8868e845f633973b2a4d0e0af4062159e
Thomas Presicci <mrbennjerry@gmail.com>
2021-05-24 04:47:09 -0500

    Updated Lek's shop to use the new system

--- infinem-server/src/main/java/io/ruin/model/entity/npc/actions/tzhaar/TzHaarHurLek.java
+++ infinem-server/src/main/java/io/ruin/model/entity/npc/actions/tzhaar/TzHaarHurLek.java
@@ -1,5 +1,6 @@
 package io.ruin.model.entity.npc.actions.tzhaar;
 
+import io.ruin.cache.NPCDef;
 import io.ruin.model.entity.npc.NPCAction;
 import io.ruin.model.inter.dialogue.NPCDialogue;
 import io.ruin.model.inter.dialogue.OptionsDialogue;
@@ -13,7 +14,7 @@ public class TzHaarHurLek {
         NPCAction.register(2184, "talk-to", (player, npc) -> player.dialogue(
                 new NPCDialogue(npc, "Can I help you JalYt-Xil-" + player.getName() + "?"),
                 new OptionsDialogue(
-                        new Option("What do you have to trade?", () -> ShopManager.openIfExists(player, "")),//TODO Fill this out
+                        new Option("What do you have to trade?", NPCDef.get(2184).shops.get(0)::open),//TODO Fill this out
                         new Option("No I'm fine thanks.", () -> player.dialogue(new PlayerDialogue("No I'm fine thanks.")))
                 )
         ));
