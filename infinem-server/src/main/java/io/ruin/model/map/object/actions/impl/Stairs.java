1368ae8a43e611f0190f1db96d835a6a592d1a51
Thomas Presicci <mrbennjerry@gmail.com>
2021-05-25 18:28:29 -0500

    Added class for handling vertical stairs

---
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/Stairs.java
@@ -0,0 +1,34 @@
+package io.ruin.model.map.object.actions.impl;
+
+import io.ruin.model.entity.player.Player;
+import io.ruin.model.inter.dialogue.OptionsDialogue;
+import io.ruin.model.inter.utils.Option;
+import io.ruin.model.map.Position;
+import io.ruin.model.map.object.actions.ObjectAction;
+
+/**
+ * @author Mrbennjerry - https://github.com/Mrbennjerry
+ * Created on 5/25/2021
+ */
+public class Stairs {
+
+    public static void registerStair(int objectId, Position position) {
+        ObjectAction.register(objectId, position.getX(), position.getY(), position.getZ(), "climb-up", (player, obj) -> climbUp(player));
+        ObjectAction.register(objectId, position.getX(), position.getY(), position.getZ(), "climb-down", (player, obj) -> climbDown(player));
+        ObjectAction.register(objectId, position.getX(), position.getY(), position.getZ(), "climb", (player, obj) -> {
+            player.dialogue(
+                    new OptionsDialogue("Climb up or down the stairs?",
+                            new Option("Climb up the stairs.", () -> climbUp(player)),
+                            new Option("Climb down the stairs.", () -> climbDown(player))
+                    ));
+        });
+    }
+
+    private static void climbUp(Player player) {
+        player.getMovement().teleport(player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ() + 1);
+    }
+
+    private static void climbDown(Player player) {
+        player.getMovement().teleport(player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ() + 1);
+    }
+}
