e37069dac9c4aaadd35467bf7d2fac1994ca0e69
Thomas Presicci <mrbennjerry@gmail.com>
2021-05-26 00:01:40 -0500

    Fixed lumbridge castle stairs

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Lumbridge.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Lumbridge.java
@@ -17,11 +17,11 @@ public class Lumbridge {
          */
         Stairs.registerStair(16671, new Position(3204, 3207, 0));
         Stairs.registerStair(16672, new Position(3204, 3207, 1));
-        Stairs.registerStair(16673, new Position(3204, 3207, 2));
+        Stairs.registerStair(16673, new Position(3205, 3208, 2));
 
         Stairs.registerStair(16671, new Position(3204, 3229, 0));
         Stairs.registerStair(16672, new Position(3204, 3229, 1));
-        Stairs.registerStair(16673, new Position(3204, 3229, 2));
+        Stairs.registerStair(16673, new Position(3205, 3229, 2));
 
     }
 }
