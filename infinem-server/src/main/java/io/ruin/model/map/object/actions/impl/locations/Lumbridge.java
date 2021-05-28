f83dc53f2807e62ed8dc67be07408bb85a1c6e92
Thomas Presicci <mrbennjerry@gmail.com>
2021-05-28 09:49:29 -0500

    Added lumbridge castle cellar ladder and trapdoor

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Lumbridge.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Lumbridge.java
@@ -2,6 +2,7 @@ package io.ruin.model.map.object.actions.impl.locations;
 
 import io.ruin.model.map.Position;
 import io.ruin.model.map.object.actions.ObjectAction;
+import io.ruin.model.map.object.actions.impl.Ladder;
 import io.ruin.model.map.object.actions.impl.Stairs;
 
 /**
@@ -23,5 +24,8 @@ public class Lumbridge {
         Stairs.registerStair(16672, new Position(3204, 3229, 1));
         Stairs.registerStair(16673, new Position(3205, 3229, 2));
 
+        // Castle cellar
+        ObjectAction.register(17385, 3209, 9616, 0, "climb-up", (player, obj) -> Ladder.climb(player, 3210, 3216, 0, true, true, false));
+        ObjectAction.register(14880, 3209, 3216, 0, "climb-down", (player, obj) -> Ladder.climb(player, 3210, 9616, 0, true, true, false));
     }
 }
