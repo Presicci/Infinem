30953d2557672e1eb864558e58063d26906a9d09
Thomas Presicci <mrbennjerry@gmail.com>
2021-06-04 04:43:40 -0500

    Fully added anima farming and added anima patch to farming guild

---
+++ infinem-server/src/main/java/io/ruin/model/skills/farming/crop/impl/AnimaCrop.java
@@ -0,0 +1,83 @@
+package io.ruin.model.skills.farming.crop.impl;
+
+import io.ruin.api.utils.TimeUtils;
+import io.ruin.model.entity.player.PlayerCounter;
+import io.ruin.model.item.Item;
+import io.ruin.model.skills.farming.crop.Crop;
+
+/**
+ * @author Mrbennjerry - https://github.com/Mrbennjerry
+ * Created on 6/3/2021
+ */
+public enum AnimaCrop implements Crop {
+    BARLEY(22881, 76, 8, 100, 8),
+    IASOR(22883, 76, 8, 100, 17),
+    KRONOS(22885, 76, 8, 100, 26);
+
+    private final int seed, levelReq, totalStages, containerIndex;
+    private final double plantXP;
+
+    AnimaCrop(int seed, int levelReq, int totalStages, double plantXP, int containerIndex) {
+        this.seed = seed;
+        this.levelReq = levelReq;
+        this.totalStages = totalStages;
+        this.containerIndex = containerIndex;
+        this.plantXP = plantXP;
+    }
+
+    @Override
+    public int getSeed() {
+        return seed;
+    }
+
+    @Override
+    public int getLevelReq() {
+        return levelReq;
+    }
+
+    @Override
+    public int getTotalStages() {
+        return totalStages;
+    }
+
+    @Override
+    public int getContainerIndex() {
+        return containerIndex;
+    }
+
+    @Override
+    public int getProduceId() {
+        return -1;
+    }
+
+    @Override
+    public double getPlantXP() {
+        return plantXP;
+    }
+
+    @Override
+    public double getHarvestXP() {
+        return 0;
+    }
+
+    @Override
+    public PlayerCounter getCounter() {
+        return null;
+    }
+
+    @Override
+    public Item getPayment() {
+        return null;
+    }
+
+    @Override
+    public long getStageTime() {
+        return TimeUtils.getMinutesToMillis(640);
+    }
+
+    @Override
+    public double getDiseaseChance(int compostType) {
+        return 0;
+    }
+
+}
---
+++ infinem-server/src/main/java/io/ruin/model/skills/farming/patch/impl/AnimaPatch.java
@@ -0,0 +1,54 @@
+package io.ruin.model.skills.farming.patch.impl;
+
+import io.ruin.api.utils.Random;
+import io.ruin.api.utils.TimeUtils;
+import io.ruin.model.skills.farming.crop.Crop;
+import io.ruin.model.skills.farming.crop.impl.AnimaCrop;
+import io.ruin.model.skills.farming.crop.impl.BushCrop;
+import io.ruin.model.skills.farming.patch.Patch;
+import io.ruin.model.stat.StatType;
+
+/**
+ * @author Mrbennjerry - https://github.com/Mrbennjerry
+ * Created on 6/3/2021
+ */
+public class AnimaPatch extends Patch {
+
+    @Override
+    public int getCropVarpbitValue() {
+        int value = getPlantedCrop().getContainerIndex() + getStage();
+        return value;
+    }
+
+    @Override
+    public void cropInteract() {
+        if (getStage() == getPlantedCrop().getTotalStages()) {
+            clear();
+        }
+    }
+
+    @Override
+    public boolean canPlant(Crop crop) {
+        return crop instanceof AnimaCrop;
+    }
+
+    @Override
+    public boolean isDiseaseImmune() {
+        return false;
+    }
+
+    @Override
+    public int calculateProduceAmount() {
+        return 4;
+    }
+
+    @Override
+    public boolean requiresCure() {
+        return false;
+    }
+
+    @Override
+    public String getPatchName() {
+        return "an anima";
+    }
+}
