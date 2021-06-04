351dec067bf40264c373c21c4a871e8739afad8a
Thomas Presicci <mrbennjerry@gmail.com>
2021-06-04 04:43:21 -0500

    Fully added redwood tree farming and added redwood tree patch to farming guild

---
+++ infinem-server/src/main/java/io/ruin/model/skills/farming/crop/impl/RedwoodCrop.java
@@ -0,0 +1,106 @@
+package io.ruin.model.skills.farming.crop.impl;
+
+import io.ruin.api.utils.TimeUtils;
+import io.ruin.model.entity.player.PlayerCounter;
+import io.ruin.model.item.Item;
+import io.ruin.model.skills.farming.crop.TreeCrop;
+
+import static io.ruin.cache.ItemID.COINS_995;
+
+/**
+ * @author Mrbennjerry - https://github.com/Mrbennjerry
+ * Created on 6/3/2021
+ */
+public class RedwoodCrop implements TreeCrop {
+
+    public static final RedwoodCrop INSTANCE = new RedwoodCrop();
+
+    private RedwoodCrop() {
+
+    }
+
+
+    @Override
+    public int getSeed() {
+        return 22871;
+    }
+
+    @Override
+    public int getLevelReq() {
+        return 90;
+    }
+
+    @Override
+    public long getStageTime() {
+        return TimeUtils.getMinutesToMillis(640);
+    }
+
+    @Override
+    public int getTotalStages() {
+        return 10;
+    }
+
+    @Override
+    public double getDiseaseChance(int compostType) {
+        switch(compostType) {
+            case 3:
+                return 0.025 / getTotalStages();
+            case 2:
+                return 0.05 / getTotalStages();
+            case 1:
+                return 0.1 / getTotalStages();
+            case 0:
+            default:
+                return 0.15 / getTotalStages();
+        }
+    }
+
+    @Override
+    public double getPlantXP() {
+        return 230;
+    }
+
+    @Override
+    public int getContainerIndex() {
+        return 8;
+    }
+
+    @Override
+    public int getProduceId() {
+        return 19669;
+    }
+
+    @Override
+    public double getHarvestXP() {
+        return 0;
+    }
+
+    public double getCheckHealthXP() {
+        return 22450;
+    }
+
+    @Override
+    public PlayerCounter getCounter() {
+        return PlayerCounter.GROWN_REDWOOD_TREE;
+    }
+
+    @Override
+    public int getSapling() {
+        return 22859;
+    }
+
+    @Override
+    public int getSeedling() {
+        return 22850;
+    }
+
+    @Override
+    public int getWateredSeedling() {
+        return 22854;
+    }
+
+    @Override
+    public Item getPayment() {
+        return new Item(COINS_995, 30000);
+    }
+}
---
+++ infinem-server/src/main/java/io/ruin/model/skills/farming/patch/impl/RedwoodPatch.java
@@ -0,0 +1,162 @@
+package io.ruin.model.skills.farming.patch.impl;
+
+import io.ruin.api.utils.Random;
+import io.ruin.model.map.object.actions.ObjectAction;
+import io.ruin.model.skills.farming.crop.Crop;
+import io.ruin.model.skills.farming.crop.impl.RedwoodCrop;
+import io.ruin.model.skills.farming.patch.Patch;
+import io.ruin.model.skills.woodcutting.Tree;
+import io.ruin.model.skills.woodcutting.Woodcutting;
+import io.ruin.model.skills.woodcutting.WoodcuttingGuild;
+import io.ruin.model.stat.StatType;
+
+/**
+ * @author Mrbennjerry - https://github.com/Mrbennjerry
+ * Created on 6/3/2021
+ */
+public class RedwoodPatch extends Patch {
+
+    // 37 check health
+    @Override
+    public int getCropVarpbitValue() {
+        int val = getPlantedCrop().getContainerIndex() + getStage();
+        if (isDiseased()) {
+            return val + 11;
+        }
+        if (isDead()) {
+            return val + 20;
+        }
+        if (getStage() == getPlantedCrop().getTotalStages()) {
+            val = 37;
+        }
+        if (getStage() == getPlantedCrop().getTotalStages() + 1) {
+            val = 18;
+        }
+        int harvest = getHarvested();
+        if (harvest > 0) {
+            val = 40 + harvest;
+        }
+        return val;
+    }
+
+    @Override
+    public void cropInteract() {
+        if (getStage() == getPlantedCrop().getTotalStages()) {
+            checkHealth();
+        } else if (getStage() == getPlantedCrop().getTotalStages() + 1) {
+            if (player.getPosition().getZ() != 1) {
+                player.sendMessage("You can't clear this by yourself, talk to Alexandra.");
+            } else {
+                chop();
+            }
+        }
+    }
+
+    @Override
+    public void clear() {
+        if (getPlantedCrop() == null) {
+            player.sendMessage("This patch doesn't have anything planted on it.");
+            return;
+        }
+        player.startEvent(event -> {
+            player.animate(831);
+            event.delay(Random.get(2, 4));
+            player.resetAnimation();
+            reset(false);
+            player.sendMessage("You clear the patch for new crops.");
+        });
+    }
+
+    private void chop() {
+        Woodcutting.chop(Tree.REDWOOD, player, () -> false, (worldEvent) -> {
+            player.sendFilteredMessage("You chop down all the bark on this side of the tree.");
+            int direction = -1;
+            if (player.getPosition().getX() == 1225) {
+                westHarvested = true;
+                direction = 0;
+            } else if (player.getPosition().getX() == 1232) {
+                eastHarvested = true;
+                direction = 1;
+            } else if (player.getPosition().getY() == 3758) {
+                northHarvested = true;
+                direction = 2;
+            } else if (player.getPosition().getY() == 3751) {
+                southHarvested = true;
+                direction = 3;
+            }
+            update();
+            worldEvent.delay(Tree.REDWOOD.respawnTime);
+            if (getPlantedCrop() == null || getStage() < getPlantedCrop().getTotalStages())
+                return; // player cleared tree
+            switch (direction) {
+                case 0:
+                    westHarvested = false;
+                    break;
+                case 1:
+                    eastHarvested = false;
+                    break;
+                case 2:
+                    northHarvested = false;
+                    break;
+                case 3:
+                    southHarvested = false;
+                    break;
+            }
+            update();
+        });
+    }
+
+    private void checkHealth() {
+        player.sendMessage("You examine the tree and find that it is in perfect health.");
+        getPlantedCrop().getCounter().increment(player, 1);
+        player.getStats().addXp(StatType.Farming, getPlantedCrop().getHarvestXP(), true);
+        northHarvested = false;
+        westHarvested = false;
+        eastHarvested = false;
+        southHarvested = false;
+        advanceStage();
+        update();
+    }
+
+    private boolean northHarvested, westHarvested, eastHarvested, southHarvested;
+
+    private int getHarvested() {
+        return (northHarvested ? 1 : 0) + (westHarvested ? 2 : 0) + (eastHarvested ? 4 : 0) + (southHarvested ? 8 : 0);
+    }
+
+    @Override
+    public boolean canPlant(Crop crop) {
+        return crop instanceof RedwoodCrop;
+    }
+
+    @Override
+    public boolean isDiseaseImmune() {
+        return false;
+    }
+
+    @Override
+    public int calculateProduceAmount() {
+        return 0; // we dont use this
+    }
+
+    @Override
+    public boolean requiresCure() {
+        return false;
+    }
+
+    @Override
+    public String getPatchName() {
+        return "a redwood tree";
+    }
+
+    static {
+        /**
+         * Redwood tree entrance to upper level
+         */
+        ObjectAction.register(34477, 1224, 3755, 0, "climb-up", (player, obj) -> WoodcuttingGuild.ropeLadder(player, obj, 1, 828, 1));
+        ObjectAction.register(34477, 1233, 3755, 0, "climb-up", (player, obj) -> WoodcuttingGuild.ropeLadder(player, obj, -1, 828, 1));
+
+        ObjectAction.register(34478, 1233, 3755, 1, "climb-down", (player, obj) -> WoodcuttingGuild.ropeLadder(player, obj, 0, 827, 0));
+        ObjectAction.register(34478, 1224, 3755, 1, "climb-down", (player, obj) -> WoodcuttingGuild.ropeLadder(player, obj, 0, 827, 0));
+    }
+}
