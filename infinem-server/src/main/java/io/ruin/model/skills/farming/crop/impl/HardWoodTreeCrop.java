8e100589f5b621a6ba76f2e360fccaddf13af2d8
Thomas Presicci <mrbennjerry@gmail.com>
2021-06-06 10:33:34 -0500

    Changed formatting

--- infinem-server/src/main/java/io/ruin/model/skills/farming/crop/impl/HardWoodTreeCrop.java
+++ infinem-server/src/main/java/io/ruin/model/skills/farming/crop/impl/HardWoodTreeCrop.java
@@ -5,38 +5,26 @@ import io.ruin.model.entity.player.PlayerCounter;
 import io.ruin.model.item.Item;
 import io.ruin.model.skills.farming.crop.TreeCrop;
 import io.ruin.model.skills.woodcutting.Tree;
+import lombok.AllArgsConstructor;
 
 /**
  * @author Mrbennjerry - https://github.com/Mrbennjerry
  * Created on 6/6/2021
  */
+@AllArgsConstructor
 public enum HardWoodTreeCrop implements TreeCrop {
     TEAK(21486, 21477, 21469, 21473, Tree.TEAK, 35, 35, 7290, 7, TimeUtils.getMinutesToMillis(640), new Item(995,  4000), 8, PlayerCounter.GROWN_TEAK),
     MAHOGANY(21488, 21480, 21471, 21475, Tree.MAHOGANY, 55, 68, 15720, 8, TimeUtils.getMinutesToMillis(730), new Item(995,  8000), 30, PlayerCounter.GROWN_MAHOGANY);
 
-
-    HardWoodTreeCrop(int seedId, int sapling, int seedling, int wateredSeedling, Tree treeType, int levelReq, double plantXP, double harvestXP, int totalStages, long stageTime, Item payment, int containerIndex, PlayerCounter counter) {
-        this.plantXP = plantXP;
-        this.checkHealthXP = harvestXP;
-        this.seedId = seedId;
-        this.treeType = treeType;
-        this.levelReq = levelReq;
-        this.containerIndex = containerIndex;
-        this.totalStages = totalStages;
-        this.counter = counter;
-        this.payment = payment;
-        this.sapling = sapling;
-        this.seedling = seedling;
-        this.wateredSeedling = wateredSeedling;
-        this.stagetime = stageTime;
-    }
-
-    private final double plantXP, checkHealthXP;
+    private final int seedId, sapling, seedling, wateredSeedling;
     private final Tree treeType;
-    private final int seedId, levelReq, containerIndex, sapling, seedling, wateredSeedling;
+    private final int levelReq;
+    private final double plantXP, checkHealthXP;
     private final int totalStages;
-    private final PlayerCounter counter;
     private final long stagetime;
+    private Item payment;
+    private final int containerIndex;
+    private final PlayerCounter counter;
 
     @Override
     public int getSeed() {
@@ -106,8 +94,6 @@ public enum HardWoodTreeCrop implements TreeCrop {
     public Item getPayment() {
         return payment;
     }
-
-    private Item payment;
 
     @Override
     public int getSapling() {
