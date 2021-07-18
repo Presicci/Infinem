6af963a47032ea00c1e8d764817ae900f3a01a6f
Thomas Presicci <mrbennjerry@gmail.com>
2021-07-18 15:48:30 -0500

    Converted event boss discord message to use webhooks instead

--- infinem-server/src/main/java/io/ruin/services/discord/impl/EventBossEmbedMessage.java
+++ infinem-server/src/main/java/io/ruin/services/discord/impl/EventBossEmbedMessage.java
@@ -1,7 +1,15 @@
 package io.ruin.services.discord.impl;
 
+import io.ruin.api.utils.ServerWrapper;
 import io.ruin.content.activities.event.impl.eventboss.EventBossType;
+import io.ruin.model.World;
 import io.ruin.services.discord.DiscordConnection;
+import io.ruin.services.discord.Webhook;
+import io.ruin.services.discord.util.Constants;
+import io.ruin.services.discord.util.Embed;
+import io.ruin.services.discord.util.Image;
+import io.ruin.services.discord.util.Message;
+import io.ruin.services.discord.util.Thumbnail;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 import java.awt.*;
@@ -13,12 +21,39 @@ import java.awt.*;
  */
 public class EventBossEmbedMessage {
     public static void sendDiscordMessage(EventBossType boss, String location) {
-        EmbedBuilder eb = new EmbedBuilder();
-        eb.setTitle("An Event Boss Has Spawned!");
-        eb.setDescription(location);
-        eb.setImage(boss.getEmbedUrl());
-        eb.setColor(new Color(0xD80066A8, true));
-        DiscordConnection.post(842960546298855486L, eb.build());
-        System.out.println("Embed sent!");
+        if (!World.isLive()){
+            return;
+        }
+        try {
+            Webhook webhook = new Webhook(Constants.WEBHOOK_URL);
+            Message message = new Message();
+
+            Embed embedMessage = new Embed();
+            embedMessage.setTitle("An event boss has spawned!");
+            embedMessage.setDescription(location);
+            embedMessage.setColor(14230283);
+
+            /*
+             * Thumbnail
+             */
+            Thumbnail thumbnail = new Thumbnail();
+            thumbnail.setUrl("https://oldschool.runescape.wiki/images/a/a1/Skull_%28status%29_icon.png?fa6d8");
+            embedMessage.setThumbnail(thumbnail);
+
+            /*
+             * Image
+             */
+            Image image = new Image();
+            image.setUrl(boss.getEmbedUrl());
+            embedMessage.setImage(image);
+
+            /*
+             * Fire the message
+             */
+            message.setEmbeds(embedMessage);
+            webhook.sendMessage(message.toJson());
+        } catch (Exception e) {
+            ServerWrapper.logError("Failed to send discord embed", e);
+        }
     }
 }
