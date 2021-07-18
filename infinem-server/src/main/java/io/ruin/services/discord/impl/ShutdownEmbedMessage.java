11d2cd2127bbb3d00d1dc555279eddd4d3a819a8
Thomas Presicci <mrbennjerry@gmail.com>
2021-07-18 15:48:45 -0500

    Removed footer

--- infinem-server/src/main/java/io/ruin/services/discord/impl/ShutdownEmbedMessage.java
+++ infinem-server/src/main/java/io/ruin/services/discord/impl/ShutdownEmbedMessage.java
@@ -1,6 +1,7 @@
 package io.ruin.services.discord.impl;
 
 import io.ruin.api.utils.ServerWrapper;
+import io.ruin.content.activities.event.impl.eventboss.EventBossType;
 import io.ruin.model.World;
 import io.ruin.services.discord.Webhook;
 import io.ruin.services.discord.util.*;
@@ -26,13 +27,12 @@ public class ShutdownEmbedMessage {
             Thumbnail thumbnail = new Thumbnail();
             thumbnail.setUrl("https://static.runelite.net/cache/item/icon/" + 13307 + ".png");
             embedMessage.setThumbnail(thumbnail);
-
             /*
              * Footer
              */
-            Footer footer = new Footer();
-            footer.setText(World.type.getWorldName());
-            embedMessage.setFooter(footer);
+            //Footer footer = new Footer();
+            //footer.setText(World.type.getWorldName());
+            //embedMessage.setFooter(footer);
 
             /*
              * Fire the message
