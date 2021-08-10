d8638fcb23e7896311c9bd5553ef5ceb49f6eafc
Thomas Presicci <mrbennjerry@gmail.com>
2021-08-10 01:53:42 -0500

    Changed worldtype to Eco

--- runelite/runelite-client/src/main/java/net/runelite/standalone/CustomMain.java
+++ runelite/runelite-client/src/main/java/net/runelite/standalone/CustomMain.java
@@ -5,6 +5,6 @@ public final class CustomMain
 
 	private static final int WIDTH = 781;
 	private static final int HEIGHT = 543;
-    public static CustomWorldType worldType = CustomWorldType.DEV;
+    public static CustomWorldType worldType = CustomWorldType.ECO;
 
 }
