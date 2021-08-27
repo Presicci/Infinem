d998d480b5f496f08ceca3feeb803cb4ea356b05
Thomas Presicci <mrbennjerry@gmail.com>
2021-08-27 00:34:46 -0500

    Added class to keep named final variables for different dialogue animations

---
+++ infinem-server/src/main/java/io/ruin/model/inter/dialogue/DialogueAnimations.java
@@ -0,0 +1,11 @@
+package io.ruin.model.inter.dialogue;
+
+/**
+ * @author Mrbennjerry - https://github.com/Mrbennjerry
+ * Created on 8/26/2021
+ */
+public class DialogueAnimations {
+
+    public static final int HAPPY = 588, LAUGHING = 605, CHEWING = 609, SAD = 610;
+
+}
