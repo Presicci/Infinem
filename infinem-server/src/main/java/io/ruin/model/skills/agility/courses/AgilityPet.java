74bb73a1f4d7f23cc083766d9098d35a154f4994
Thomas Presicci <thomaspresicci@gmail.com>
2021-09-22 12:31:39 -0500

    Added osrs pet formula

--- infinem-server/src/main/java/io/ruin/model/skills/agility/courses/AgilityPet.java
+++ infinem-server/src/main/java/io/ruin/model/skills/agility/courses/AgilityPet.java
@@ -3,12 +3,12 @@ package io.ruin.model.skills.agility.courses;
 import io.ruin.api.utils.Random;
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.item.actions.impl.Pet;
+import io.ruin.model.stat.StatType;
 
 public class AgilityPet {
 
     public static void rollForPet(Player player, int chance) {
-        if (Random.rollDie(chance))
+        if (Random.rollDie(chance - (player.getStats().get(StatType.Agility).currentLevel * 25)))
             Pet.GIANT_SQUIRREL.unlock(player);
     }
-
 }
