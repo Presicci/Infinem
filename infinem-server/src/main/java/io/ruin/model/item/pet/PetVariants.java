ed781ecddd3507eb73e8e82a4d9f7f67dca72b5f
Thomas Presicci <mrbennjerry@gmail.com>
2021-09-23 12:22:15 -0500

    Added baby chin variants

--- infinem-server/src/main/java/io/ruin/model/item/pet/PetVariants.java
+++ infinem-server/src/main/java/io/ruin/model/item/pet/PetVariants.java
@@ -6,6 +6,8 @@ package io.ruin.model.item.pet;
  */
 public enum PetVariants {
 
+    BABY_CHIN(Pet.BABY_CHINCHOMPA_BLACK, Pet.BABY_CHINCHOMPA_GOLD, Pet.BABY_CHINCHOMPA_GREY, Pet.BABY_CHINCHOMPA_RED),
+
     RIFT_GUARDIAN(Pet.RIFT_GUARDIAN_FIRE, Pet.RIFT_GUARDIAN_AIR, Pet.RIFT_GUARDIAN_MIND, Pet.RIFT_GUARDIAN_WATER,
             Pet.RIFT_GUARDIAN_EARTH, Pet.RIFT_GUARDIAN_BODY, Pet.RIFT_GUARDIAN_COSMIC, Pet.RIFT_GUARDIAN_CHAOS,
             Pet.RIFT_GUARDIAN_NATURE, Pet.RIFT_GUARDIAN_LAW, Pet.RIFT_GUARDIAN_DEATH, Pet.RIFT_GUARDIAN_SOUL,
@@ -15,5 +17,9 @@ public enum PetVariants {
 
     PetVariants(Pet... pets) {
         this.pets = pets;
+    }
+
+    public Pet[] getPets() {
+        return pets;
     }
 }
