ad0047024547e7c27d7cfaf43b75f54c590f7219
Thomas Presicci <mrbennjerry@gmail.com>
2021-09-22 19:06:04 -0500

    Added a class to hold arrays of pet variants

---
+++ infinem-server/src/main/java/io/ruin/model/item/pet/PetVariants.java
@@ -0,0 +1,19 @@
+package io.ruin.model.item.pet;
+
+/**
+ * @author Mrbennjerry - https://github.com/Mrbennjerry
+ * Created on 9/22/2021
+ */
+public enum PetVariants {
+
+    RIFT_GUARDIAN(Pet.RIFT_GUARDIAN_FIRE, Pet.RIFT_GUARDIAN_AIR, Pet.RIFT_GUARDIAN_MIND, Pet.RIFT_GUARDIAN_WATER,
+            Pet.RIFT_GUARDIAN_EARTH, Pet.RIFT_GUARDIAN_BODY, Pet.RIFT_GUARDIAN_COSMIC, Pet.RIFT_GUARDIAN_CHAOS,
+            Pet.RIFT_GUARDIAN_NATURE, Pet.RIFT_GUARDIAN_LAW, Pet.RIFT_GUARDIAN_DEATH, Pet.RIFT_GUARDIAN_SOUL,
+            Pet.RIFT_GUARDIAN_ASTRAL, Pet.RIFT_GUARDIAN_BLOOD, Pet.RIFT_GUARDIAN_WRATH);
+
+    private Pet[] pets;
+
+    PetVariants(Pet... pets) {
+        this.pets = pets;
+    }
+}
