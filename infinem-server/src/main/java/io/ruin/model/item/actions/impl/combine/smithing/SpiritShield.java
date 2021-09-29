45c58ef2bc3e6d0b459cd64cd55086be8c19bdb2
Thomas Presicci <thomaspresicci@gmail.com>
2021-09-29 13:24:23 -0500

    Moved sigil attaching code out of lambda

--- infinem-server/src/main/java/io/ruin/model/item/actions/impl/combine/smithing/SpiritShield.java
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/combine/smithing/SpiritShield.java
@@ -1,5 +1,6 @@
 package io.ruin.model.item.actions.impl.combine.smithing;
 
+import io.ruin.model.entity.player.Player;
 import io.ruin.model.inter.dialogue.ItemDialogue;
 import io.ruin.model.item.Item;
 import io.ruin.model.item.actions.ItemItemAction;
@@ -17,6 +18,36 @@ public class SpiritShield {
     private static final int SPECTRAL_SIGIL = 12823;
     private static final int ELYSIAN_SIGIL = 12819;
     private static final int DIVINE_SIGIL = 30187;
+
+    private static void attachSigil(Player player, Item item, Object object) {
+        if(player.getStats().get(StatType.Prayer).currentLevel < 90 || player.getStats().get(StatType.Smithing).currentLevel < 85) {
+            player.sendMessage("You don't have the skills required to make this. You need 85 Smithing and 90 Prayer.");
+            return;
+        }
+        if(!player.getInventory().hasId(BLESSED_SPIRIT_SHIELD)) {
+            player.sendMessage("You don't have a spirit shield to attach your " + item.getDef().name + " to.");
+            return;
+        }
+
+        Item hammer = player.getInventory().findItem(Tool.HAMMER);
+        if(hammer == null) {
+            player.sendMessage("You need a hammer to do this.");
+            return;
+        }
+
+        player.startEvent(event -> {
+            player.lock();
+            player.animate(898);
+            event.delay(6);
+            player.animate(898);
+            event.delay(6);
+            player.getInventory().remove(BLESSED_SPIRIT_SHIELD, 1);
+            player.dialogue(new ItemDialogue().one(item.getId() == DIVINE_SIGIL ? 30191 : item.getId() - 2, "You successfully combine the " + item.getDef().name + " with the shield."));
+            player.getStats().addXp(StatType.Smithing, 1800, true);
+            item.setId(item.getId() == DIVINE_SIGIL ? 30191 : item.getId() - 2);
+            player.unlock();
+        });
+    }
 
     static {
         /**
@@ -38,35 +69,7 @@ public class SpiritShield {
          */
         int[] sigils = {ARCANE_SIGIL, SPECTRAL_SIGIL, ELYSIAN_SIGIL, DIVINE_SIGIL};
         for (int itemID : sigils) {
-            ItemObjectAction.register(itemID, "anvil", (player, item, obj) -> {
-                if(player.getStats().get(StatType.Prayer).currentLevel < 90 || player.getStats().get(StatType.Smithing).currentLevel < 85) {
-                    player.sendMessage("You don't have the skills required to make this. You need 85 Smithing and 90 Prayer.");
-                    return;
-                }
-                if(!player.getInventory().hasId(BLESSED_SPIRIT_SHIELD)) {
-                    player.sendMessage("You don't have a spirit shield to attach your " + item.getDef().name + " to.");
-                    return;
-                }
-
-                Item hammer = player.getInventory().findItem(Tool.HAMMER);
-                if(hammer == null) {
-                    player.sendMessage("You need a hammer to do this.");
-                    return;
-                }
-
-                player.startEvent(event -> {
-                    player.lock();
-                    player.animate(898);
-                    event.delay(6);
-                    player.animate(898);
-                    event.delay(6);
-                    player.getInventory().remove(BLESSED_SPIRIT_SHIELD, 1);
-                    player.dialogue(new ItemDialogue().one(itemID == DIVINE_SIGIL ? 30191 : item.getId() - 2, "You successfully combine the " + item.getDef().name + " with the shield."));
-                    player.getStats().addXp(StatType.Smithing, 1800, true);
-                    item.setId(itemID == DIVINE_SIGIL ? 30191 : item.getId() - 2);
-                    player.unlock();
-                });
-            });
+            ItemObjectAction.register(itemID, "anvil", SpiritShield::attachSigil);
         }
     }
 }
