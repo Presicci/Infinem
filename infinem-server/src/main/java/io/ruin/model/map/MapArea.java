1ea18be49237bfd288429feb8aa509548f12b39a
Thomas Presicci <mrbennjerry@gmail.com>
2021-09-28 22:06:35 -0500

    Renamed MapAreas.java to MapArea.java

--- infinem-server/src/main/java/io/ruin/model/map/MapArea.java
+++ infinem-server/src/main/java/io/ruin/model/map/MapArea.java
@@ -4,17 +4,17 @@ package io.ruin.model.map;
  * @author Mrbennjerry - https://github.com/Mrbennjerry
  * Created on 8/19/2021
  */
-public enum MapAreas {
+public enum MapArea {
     LUMBRIDGE_CASTLE(12850),
     VARROCK(3170, 3376, 3299, 3506, 0);
 
     private Bounds bounds;
 
-    MapAreas(int regionId) {
+    MapArea(int regionId) {
         this.bounds = Bounds.fromRegion(regionId);
     };
 
-    MapAreas(int southWestX, int southWestY, int northEastX, int northEastY, int z) {
+    MapArea(int southWestX, int southWestY, int northEastX, int northEastY, int z) {
         this.bounds = new Bounds(new Position(southWestX, southWestY), new Position(northEastX, northEastY), z);
     }
 }
