480c2928e95a96be3c68f528d4efafe77a12700c
Thomas Presicci <mrbennjerry@gmail.com>
2021-09-30 02:40:53 -0500

    Added @Getter and dragon forge area

--- infinem-server/src/main/java/io/ruin/model/map/MapArea.java
+++ infinem-server/src/main/java/io/ruin/model/map/MapArea.java
@@ -1,12 +1,16 @@
 package io.ruin.model.map;
 
+import lombok.Getter;
+
 /**
  * @author Mrbennjerry - https://github.com/Mrbennjerry
  * Created on 8/19/2021
  */
+@Getter
 public enum MapArea {
     LUMBRIDGE_CASTLE(12850),
-    VARROCK(3170, 3376, 3299, 3506, 0);
+    VARROCK(3170, 3376, 3299, 3506, 0),
+    DRAGON_FORGE(1744, 5277, 1760, 5293, 1);
 
     private Bounds bounds;
 
