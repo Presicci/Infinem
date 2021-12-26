98f954e90fe6d376f5c70fece344c92861b7881d
Presicci <mrbennjerry@gmail.com>
2021-12-26 15:02:29 -0600

    Added hasPrayerCape method

--- infinem-server/src/main/java/io/ruin/model/item/actions/impl/skillcapes/PrayerSkillCape.java
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/skillcapes/PrayerSkillCape.java
@@ -2,6 +2,7 @@ package io.ruin.model.item.actions.impl.skillcapes;
 
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.item.containers.Equipment;
+import io.ruin.model.item.containers.Inventory;
 import io.ruin.model.stat.StatType;
 
 /**
@@ -16,5 +17,10 @@ public class PrayerSkillCape {
     public static boolean wearingPrayerCape(Player player) {
         int cape = player.getEquipment().getId(Equipment.SLOT_CAPE);
         return cape == CAPE || cape == TRIMMED_CAPE || cape == 13342 || cape == MASTER_CAPE;
+    }
+
+    public static boolean hasPrayerCape(Player player) {
+        Inventory inv = player.getInventory();
+        return inv.contains(CAPE) || inv.contains(TRIMMED_CAPE) || inv.contains(13342) || inv.contains(MASTER_CAPE);
     }
 }
