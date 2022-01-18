5e6a613185d90a422ef23fde7d666e031372a552
Presicci <mrbennjerry@gmail.com>
2022-01-18 14:30:24 -0600

    Added lizardman lair and temple objects

---
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/LizardmanLair.java
@@ -0,0 +1,68 @@
+package io.ruin.model.map.object.actions.impl.dungeons;
+
+import io.ruin.model.entity.npc.actions.traveling.Traveling;
+import io.ruin.model.entity.player.Player;
+import io.ruin.model.map.Position;
+import io.ruin.model.map.object.actions.ObjectAction;
+
+public class LizardmanLair {
+
+    public static void handlePosition30383(Player player) {
+        if (player.getPosition().getX() == 1305 && player.getPosition().getY() == 9957) {
+            Traveling.fadeTravel(player, new Position(1305, 9953, 0));
+        } else {
+            if (player.getPosition().getX() == 1318 && player.getPosition().getY() == 9960) {
+                Traveling.fadeTravel(player, new Position(1318, 9956, 0));
+            }
+        }
+    }
+
+    public static void handlePosition30382(Player player) {
+        if (player.getPosition().getX() == 1305 && player.getPosition().getY() == 9953) {
+            Traveling.fadeTravel(player, new Position(1305, 9957, 0));
+        } else {
+            if (player.getPosition().getX() == 1318 && player.getPosition().getY() == 9956) {
+                Traveling.fadeTravel(player, new Position(1318, 9960, 0));
+            }
+        }
+    }
+
+    public static void handlePosition30384(Player player) {
+        if (player.getPosition().getX() == 1295 && player.getPosition().getY() == 9959) {
+            Traveling.fadeTravel(player, new Position(1299, 9959, 0));
+        } else {
+            if (player.getPosition().getX() == 1319 && player.getPosition().getY() == 9966) {
+                Traveling.fadeTravel(player, new Position(1323, 9966, 0));
+            }
+        }
+    }
+
+    public static void handlePosition30385(Player player) {
+        if (player.getPosition().getX() == 1299 && player.getPosition().getY() == 9959) {
+            Traveling.fadeTravel(player, new Position(1295, 9959, 0));
+        } else {
+            if (player.getPosition().getX() == 1323 && player.getPosition().getY() == 9966) {
+                Traveling.fadeTravel(player, new Position(1319, 9966, 0));
+            }
+        }
+    }
+
+    static {
+        ObjectAction.register(30383, "squeeze-through", (player, obj) -> handlePosition30383(player));
+        ObjectAction.register(30383, "squeeze-through", (player, obj) -> handlePosition30383(player));
+
+        ObjectAction.register(30382, "squeeze-through", (player, obj) -> handlePosition30382(player));
+        ObjectAction.register(30382, "squeeze-through", (player, obj) -> handlePosition30382(player));
+
+        ObjectAction.register(30384, "squeeze-through", (player, obj) -> handlePosition30384(player));
+        ObjectAction.register(30384, "squeeze-through", (player, obj) -> handlePosition30384(player));
+
+        ObjectAction.register(30385, "squeeze-through", (player, obj) -> handlePosition30385(player));
+        ObjectAction.register(30385, "squeeze-through", (player, obj) -> handlePosition30385(player));
+
+        // Entrance/Exit
+        ObjectAction.register(30380, "enter", (player, obj) -> Traveling.fadeTravel(player, new Position(1305, 9973, 0)));
+        ObjectAction.register(30381, "squeeze-through", (player, obj) -> Traveling.fadeTravel(player, new Position(1309, 3574, 0)));
+    }
+}
+
---
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/LizardmanTemple.java
@@ -0,0 +1,88 @@
+package io.ruin.model.map.object.actions.impl.dungeons;
+
+import io.ruin.model.entity.npc.actions.traveling.Traveling;
+import io.ruin.model.entity.player.Player;
+import io.ruin.model.entity.shared.StepType;
+import io.ruin.model.map.Position;
+import io.ruin.model.map.object.GameObject;
+import io.ruin.model.map.object.actions.ObjectAction;
+
+public class LizardmanTemple {
+
+    private static void BarrierNSRoomOne(Player player, GameObject barrier) {
+        player.startEvent(event -> {
+            if(player.getAbsY() == 10091) {
+                player.stepAbs(player.getAbsX(), 10093, StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            } else {
+                player.stepAbs(player.getAbsX(), 10091, StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            }
+        });
+    }
+
+    private static void BarrierCorridorOne(Player player, GameObject barrier) {
+        player.startEvent(event -> {
+            if(player.getAbsX() == 1296) {
+                player.stepAbs(player.getAbsX()+2, player.getAbsY(), StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            } else {
+                player.stepAbs(player.getAbsX()-2, player.getAbsY(), StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            }
+        });
+    }
+
+    private static void BarrierRoomTwo(Player player, GameObject barrier) {
+        player.startEvent(event -> {
+            if(player.getAbsX() == 1307) {
+                player.stepAbs(player.getAbsX()+2, player.getAbsY(), StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            } else {
+                player.stepAbs(player.getAbsX()-2, player.getAbsY(), StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            }
+        });
+    }
+
+    private static void BarrierCorridorTwo(Player player, GameObject barrier) {
+        player.startEvent(event -> {
+            if(player.getAbsX() == 1316) {
+                player.stepAbs(player.getAbsX()+2, player.getAbsY(), StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            } else {
+                player.stepAbs(player.getAbsX()-2, player.getAbsY(), StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            }
+        });
+    }
+
+    private static void BarrierRoomThree(Player player, GameObject barrier) {
+        player.startEvent(event -> {
+            if(player.getAbsX() == 1324) {
+                player.stepAbs(player.getAbsX()+2, player.getAbsY(), StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            } else {
+                player.stepAbs(player.getAbsX()-2, player.getAbsY(), StepType.FORCE_WALK);
+                player.sendFilteredMessage("You pass through the Barrier.");
+            }
+        });
+    }
+
+    static {
+        // not working? TODO
+        ObjectAction.register(34642,"pass", LizardmanTemple::BarrierNSRoomOne);
+        ObjectAction.register(34643,"pass", LizardmanTemple::BarrierCorridorOne);
+        ObjectAction.register(34644,"pass", LizardmanTemple::BarrierRoomTwo);
+        ObjectAction.register(34645,"pass", LizardmanTemple::BarrierCorridorTwo);
+        ObjectAction.register(34646,"pass", LizardmanTemple::BarrierRoomThree);
+
+        // Entrance / exit
+        ObjectAction.register(34405, "enter", (player, obj) -> Traveling.fadeTravel(player, new Position(1312, 10086, 0)));
+        ObjectAction.register(34404, "enter", (player, obj) -> Traveling.fadeTravel(player, new Position(1330, 10070, 0)));
+        ObjectAction.register(34403, "enter", (player, obj) -> Traveling.fadeTravel(player, new Position(1314, 10064, 0)));
+        ObjectAction.register(34402, "enter", (player, obj) -> Traveling.fadeTravel(player, new Position(1292, 10058, 0)));
+        ObjectAction.register(34422, "jump-in", (player, obj) -> Traveling.fadeTravel(player, new Position(1284, 3678, 0)));
+    }
+}
+
