1280b67e98037ab10822376dd3172c90e7ca635b
Presicci <mrbennjerry@gmail.com>
2022-01-18 14:47:28 -0600

    Added tower of voices stairs

---
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/prifddinas/TowerOfVoices.java
@@ -0,0 +1,13 @@
+package io.ruin.model.map.object.actions.impl.locations.prifddinas;
+
+import io.ruin.model.entity.npc.actions.traveling.Traveling;
+import io.ruin.model.map.Position;
+import io.ruin.model.map.object.actions.ObjectAction;
+
+public class TowerOfVoices {
+
+    static {
+        ObjectAction.register(36390, 1, (player, obj) -> Traveling.fadeTravel(player, new Position(3263, 6078, 0)));
+        ObjectAction.register(36387, 1, (player, obj) -> Traveling.fadeTravel(player, new Position(3268, 6082, 2)));
+    }
+}
