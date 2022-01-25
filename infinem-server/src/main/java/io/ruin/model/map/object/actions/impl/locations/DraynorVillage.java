0145764f86f2a470232f1abf699eab3cbb2925ad
Presicci <mrbennjerry@gmail.com>
2022-01-25 08:50:00 -0600

    Added ladders in draynor sewer and fixed trapdoors

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/DraynorVillage.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/DraynorVillage.java
@@ -1,7 +1,9 @@
 package io.ruin.model.map.object.actions.impl.locations;
 
+import io.ruin.model.entity.player.Player;
 import io.ruin.model.map.Direction;
 import io.ruin.model.map.Position;
+import io.ruin.model.map.object.GameObject;
 import io.ruin.model.map.object.actions.ObjectAction;
 import io.ruin.model.map.object.actions.impl.Ladder;
 import io.ruin.model.map.object.actions.impl.Stairs;
@@ -12,6 +14,19 @@ import io.ruin.model.map.object.actions.impl.Trapdoor;
  * Created on 10/5/2021
  */
 public class DraynorVillage {
+
+    /*
+     * Has to be done like this because the objects aren't present at runtime and thus
+     * cannot be registered with coordinates
+     */
+    private static void climbDownTrapdoor(Player player, GameObject obj) {
+        if (obj.getPosition().getX() == 3084) {
+            Trapdoor.climbDown(player, new Position(3085, 9672, 0));
+        } else {
+            Trapdoor.climbDown(player, new Position(3118, 9644, 0));
+        }
+    }
+
     static {
         //  Wise old man's house
         Stairs.registerStair(16670, new Position(3090, 3251), Direction.EAST, 4);
@@ -21,6 +36,9 @@ public class DraynorVillage {
         Stairs.registerStair(15648, new Position(3100, 3266, 1), Direction.WEST, 4);
         //  Sewer
         ObjectAction.register(6434, "open", (player, obj) -> Trapdoor.open(player, obj, 6435));
-        ObjectAction.register(6435, "climb-down", (player, obj) -> Trapdoor.climbDown(player, new Position(3085, 9672, 0)));
+        ObjectAction.register(6435, "climb-down", DraynorVillage::climbDownTrapdoor);
+        // Sewer ladders
+        ObjectAction.register(17385, 3084, 9672, 0, "climb-up", (player, obj) -> Ladder.climb(player, 3084, 3273, 0, true, true, false));
+        ObjectAction.register(6436, 3118, 9643, 0, "climb-up", (player, obj) -> Ladder.climb(player, 3118, 3243, 0, true, true, false));
     }
 }
