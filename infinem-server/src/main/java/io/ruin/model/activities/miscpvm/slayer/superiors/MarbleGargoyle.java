60186f42009d7777a10e33e8087804dcf8349a65
Presicci <mrbennjerry@gmail.com>
2022-03-17 00:51:21 -0500

    Started marble gargoyle combat

---
+++ infinem-server/src/main/java/io/ruin/model/activities/miscpvm/slayer/superiors/MarbleGargoyle.java
@@ -0,0 +1,25 @@
+package io.ruin.model.activities.miscpvm.slayer.superiors;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 3/16/2022
+ */
+public class MarbleGargoyle extends Superior {
+    //TODO find gfx for spec attack and ranged attack, then code it
+    //https://www.youtube.com/watch?v=Tm11cugG2gs
+    //https://oldschool.runescape.wiki/w/Marble_gargoyle
+
+    @Override
+    public void follow() {
+        follow(1);
+    }
+
+    @Override
+    public boolean attack() {
+        if(withinDistance(1)) {
+            basicAttack(info.attack_animation, info.attack_style, info.max_damage);
+            return true;
+        }
+        return false;
+    }
+}
