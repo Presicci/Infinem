a3b740336660286307a4974b9bdbba1f23c52bd7
Presicci <mrbennjerry@gmail.com>
2022-03-18 01:11:24 -0500

    Added task only kurask area

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/FremennikSlayerDungeon.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/FremennikSlayerDungeon.java
@@ -1,10 +1,14 @@
 package io.ruin.model.map.object.actions.impl.dungeons;
 
 import io.ruin.model.entity.player.Player;
+import io.ruin.model.inter.dialogue.NPCDialogue;
+import io.ruin.model.inter.utils.Config;
 import io.ruin.model.map.Direction;
 import io.ruin.model.map.Position;
 import io.ruin.model.map.object.GameObject;
 import io.ruin.model.map.object.actions.ObjectAction;
+import io.ruin.model.skills.agility.shortcut.Stile;
+import io.ruin.model.skills.slayer.SlayerCreature;
 import io.ruin.model.stat.StatType;
 
 public class FremennikSlayerDungeon {
@@ -38,6 +42,16 @@ public class FremennikSlayerDungeon {
         ObjectAction.register(16539, 2734, 10008, 0, "squeeze-through", (player, obj) -> squeezeThroughCrack(player, obj, new Position(2730, 10008, 0), 62)); // crack
         ObjectAction.register(16539, 2731, 10008, 0, "squeeze-through", (player, obj) -> squeezeThroughCrack(player, obj, new Position(2735, 10008, 0), 62)); // crack
 
+        ObjectAction.register(29993, 2703, 9990, 0, "climb", (player, obj) -> {
+            final int task = Config.SLAYER_TASK_1.get(player);
+            int am = Config.SLAYER_TASK_AMOUNT.get(player);
+            SlayerCreature creature = SlayerCreature.lookup(task);
+            //if ((creature == SlayerCreature.KURASK && am > 0) || player.getPosition().getY() < obj.getPosition().getY()) {
+                Stile.shortcutN(player, obj, 1);
+            //} else {
+             //   player.dialogue(new NPCDialogue(7518, "Naughty human! You not hunting the Kurask! Jelly's Kurasks only for people on Slayer tasks."));
+           // }
+        });
     }
 
     private static void squeezeThroughCrack(Player player, GameObject crack, Position destination, int levelReq) {
