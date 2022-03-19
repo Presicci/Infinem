a5710a88e69062847d26a3aae35e2314b65a9b86
Presicci <mrbennjerry@gmail.com>
2022-03-19 02:38:28 -0500

    Added west bank cellar entrance/exit

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Varrock.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Varrock.java
@@ -2,6 +2,7 @@ package io.ruin.model.map.object.actions.impl.locations;
 
 import io.ruin.model.map.Direction;
 import io.ruin.model.map.Position;
+import io.ruin.model.map.object.actions.ObjectAction;
 import io.ruin.model.map.object.actions.impl.Stairs;
 
 public class Varrock {
@@ -10,5 +11,8 @@ public class Varrock {
         // Champion's guild stairs
         Stairs.registerStair(11797, new Position(3188, 3355), Direction.SOUTH, 4);
         Stairs.registerStair(11799, new Position(3188, 3355, 1), Direction.NORTH, 4);
+
+        ObjectAction.register(11800, 3187, 3433, 0, "climb-down", (player, obj) -> player.getMovement().teleport(3190, 9834, 0));
+        ObjectAction.register(11805, 3187, 9833, 0, "climb-up", (player, obj) -> player.getMovement().teleport(3186, 3434, 0));
     }
 }
