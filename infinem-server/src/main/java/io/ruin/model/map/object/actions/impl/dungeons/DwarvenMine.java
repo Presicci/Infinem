7434c0cc85b02e4ca2701a556c269eca5a5ecf4c
Presicci <mrbennjerry@gmail.com>
2022-03-20 01:35:10 -0500

    Added dwarven mind entrance/exit

---
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/DwarvenMine.java
@@ -0,0 +1,19 @@
+package io.ruin.model.map.object.actions.impl.dungeons;
+
+import io.ruin.model.map.object.actions.ObjectAction;
+import io.ruin.model.map.object.actions.impl.Ladder;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 3/19/2022
+ */
+public class DwarvenMine {
+
+    static {
+        /*
+         * Dwarven entrance
+         */
+        ObjectAction.register(11867, 3019, 3450, 0, "climb-down", (player, obj) -> Ladder.climb(player, 3020, 9850, 0, false, true, false));
+        ObjectAction.register(17387, 3019, 9850, 0, "climb-up", (player, obj) -> Ladder.climb(player, 3018, 3450, 0, true, true, false));
+    }
+}
