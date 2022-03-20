b4f720cc93cb479e6abb43aa5731a803201e14f0
Presicci <mrbennjerry@gmail.com>
2022-03-20 17:38:19 -0500

    Reworked crystal chest to be identical to osrs

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/chests/CrystalChest.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/chests/CrystalChest.java
@@ -3,114 +3,58 @@ package io.ruin.model.map.object.actions.impl.chests;
 import io.ruin.api.utils.Random;
 import io.ruin.model.World;
 import io.ruin.model.item.Item;
+import io.ruin.model.item.Items;
+import io.ruin.model.item.loot.LootItem;
+import io.ruin.model.item.loot.LootTable;
 import io.ruin.model.map.object.actions.ObjectAction;
 import io.ruin.utility.Broadcast;
+
+import java.util.List;
 
 import static io.ruin.cache.ItemID.COINS_995;
 
 public class CrystalChest {
 
-    private static final Item[][] LOOTS = {
-            /**
-             * Spinach roll & coins
-            */
-            new Item[]{
-                    new Item(1969, 1),
-                    new Item(COINS_995, 50000)
-            },
-            /**
-             * Raw swordfish & coins
-            */
-            new Item[]{
-                    new Item(COINS_995, 50000),
-                    new Item(372, 30)
-            },
-            /**
-             * Runes
-            */
-            new Item[]{
-                    new Item(560, 500),
-                    new Item(566, 450),
-                    new Item(565, 450)
-            },
-            /**
-             * Coal ore
-            */
-            new Item[]{
-                    new Item(454, 75)
-            },
-            /**
-             * Gems
-            */
-            new Item[]{
-                    new Item(1620, 10)
-            },
-            /**
-             * Tooth half of a key & coins
-            */
-            new Item[]{
-                    new Item(COINS_995, 50000),
-                    new Item(985, 1)
-            },
-            /**
-             * Runite bars
-            */
-            new Item[]{
-                    new Item(2364, 5)
-            },
-            /**
-             * Loop half of a key
-            */
-            new Item[]{
-                    new Item(COINS_995, 50000),
-                    new Item(987, 1)
-            },
-            /**
-             * Iron ore
-            */
-            new Item[]{
-                    new Item(445, 50)
-            },
-            /**
-             * Adamant sq
-            */
-            new Item[]{
-                    new Item(1202, 2)
-            },
-            /**
-             * Rune platelegs/plateskirt
-            */
-            new Item[]{
-                    new Item(1080, 2),
-                    new Item(1094, 1)
-            },
-
-            // Coins
-            new Item[]{
-                    new Item(995, 150000)
-            },
-
-            // Herb box
-            new Item[]{
-                    new Item(11738, 1)
-            }
-    };
-
-    private static final Item[] RARE_LOOT = {
-            /**
-             * New crystal bow
-            */
-            new Item(4212, 1),
-            /**
-             * New crystal shield
-            */
-            new Item(4224, 1),
-            /**
-             * New crystal hally
-            */
-            new Item(13091, 1),
-    };
-
+    private static final LootTable FOOD = new LootTable()
+            .addTable(34,   // Spinach Roll
+                    new LootItem(Items.SPINACH_ROLL, 1, 0),
+                    new LootItem(Items.COINS, 2000, 0)
+            ).addTable(12,   // Runes
+                    new LootItem(Items.AIR_RUNE, 50, 0),
+                    new LootItem(Items.WATER_RUNE, 50, 0),
+                    new LootItem(Items.EARTH_RUNE, 50, 0),
+                    new LootItem(Items.FIRE_RUNE, 50, 0),
+                    new LootItem(Items.BODY_RUNE, 50, 0),
+                    new LootItem(Items.MIND_RUNE, 50, 0),
+                    new LootItem(Items.CHAOS_RUNE, 10, 0),
+                    new LootItem(Items.DEATH_RUNE, 10, 0),
+                    new LootItem(Items.COSMIC_RUNE, 10, 0),
+                    new LootItem(Items.NATURE_RUNE, 10, 0),
+                    new LootItem(Items.LAW_RUNE, 10, 0)
+            ).addTable(12,   // Gems
+                    new LootItem(Items.RUBY, 2, 0),
+                    new LootItem(Items.DIAMOND, 2, 0)
+            ).addTable(12,   // Runite bars
+                    new LootItem(Items.RUNITE_BAR, 3, 0)
+            ).addTable(10,   // Crystalk key halves
+                    new LootItem(Items.COINS, 750, 0),
+                    new LootItem(Items.LOOP_HALF_OF_KEY, 1, 1),
+                    new LootItem(Items.TOOTH_HALF_OF_KEY, 1, 1)
+            ).addTable(10,   // Iron ores
+                    new LootItem(Items.IRON_ORE_NOTE, 150, 0)
+            ).addTable(10,   // Coal
+                    new LootItem(Items.COAL_NOTE, 100, 0)
+            ).addTable(8,   // Raw Swordfish
+                    new LootItem(Items.COINS, 1000, 0),
+                    new LootItem(Items.RAW_SWORDFISH, 5, 0)
+            ).addTable(2,   // Adamant sq shield
+                    new LootItem(Items.ADAMANT_SQ_SHIELD, 1, 0)
+            ).addTable(12,   // Rune platelegs or plateskirt
+                    new LootItem(Items.RUNE_PLATELEGS, 1, 1),
+                    new LootItem(Items.RUNE_PLATESKIRT, 1, 1)
+            ).addTable(17,   // "NOTHING"
+                    new LootItem(Items.COINS, 5000, 0)
+            );
 
     static {
         ObjectAction.register(172, "open", (player, obj) -> {
@@ -131,20 +75,9 @@ public class CrystalChest {
                     obj.setId(obj.originalId);
                 });
                 crystalKey.setId(1631); //dragonstone
-                if(Random.get() <= 0.004) { //1/250
-                    /**
-                     * Rare loot
-                     */
-                    Item loot = RARE_LOOT[Random.get(RARE_LOOT.length - 1)];
-                    player.getInventory().add(loot.getId(), loot.getAmount());
-                    Broadcast.WORLD.sendNews(player.getName() + " just received " + loot.getDef().descriptiveName + " from the crystal chest!");
-                } else {
-                    /**
-                     * Regular loot
-                     */
-                    Item[] loot = LOOTS[Random.get(LOOTS.length - 1)];
-                    for(Item item : loot)
-                        player.getInventory().addOrDrop(item.getId(), item.getAmount());
+                List<Item> loot = FOOD.rollItems(true);
+                for(Item item : loot) {
+                    player.getInventory().addOrDrop(item.getId(), item.getAmount());
                 }
                 event.delay(1);
                 player.unlock();
