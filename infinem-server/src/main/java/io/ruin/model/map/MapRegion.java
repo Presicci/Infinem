87694ad8a403c5e80bad6ddb0b8b40ffb3213705
Presicci <mrbennjerry@gmail.com>
2022-03-23 11:23:59 -0500

    Added MapRegion enum

---
+++ infinem-server/src/main/java/io/ruin/model/map/MapRegion.java
@@ -0,0 +1,10 @@
+package io.ruin.model.map;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 3/21/2022
+ */
+public enum MapRegion {
+    ASGARNIA, DESERT, FELDIP_HILLS, FREMENNIK_PROVINCE, KANDARIN,
+    KARAMJA, MISTHALIN, MORYTANIA, WESTERN_PROVINCE, WILDERNESS, ZEAH
+}
