94e6420c18123b2550ad7793b65f6962030dfb2f
Presicci <mrbennjerry@gmail.com>
2022-03-25 01:23:49 -0500

    Added sara wizard combat

---
+++ infinem-server/src/main/java/io/ruin/model/activities/miscpvm/clues/SaradominWizard.java
@@ -0,0 +1,50 @@
+package io.ruin.model.activities.miscpvm.clues;
+
+import io.ruin.model.activities.miscpvm.basic.BasicPoisonCombat;
+import io.ruin.model.combat.AttackStyle;
+import io.ruin.model.combat.Hit;
+import io.ruin.model.map.Projectile;
+import io.ruin.model.skills.prayer.Prayer;
+
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 3/25/2022
+ */
+public class SaradominWizard extends BasicPoisonCombat {
+
+    private static final Projectile PROJ = new Projectile(64, 0);
+
+    @Override
+    public void init() {
+        npc.forceText("For Saradomin!");
+    }
+
+    @Override
+    public void follow() {
+        follow(1);
+    }
+
+    @Override
+    public boolean attack() {
+        if (withinDistance(1) && target.player != null && !target.player.getPrayer().isActive(Prayer.PROTECT_FROM_MELEE)) {
+            super.attack();
+        }
+        if (!withinDistance(8))
+            return false;
+        npc.animate(811);
+        int delay = PROJ.send(npc, target);
+        Hit hit = new Hit(npc, AttackStyle.MAGIC).clientDelay(delay).randDamage(20);;
+        hit.postDamage(t -> {
+            if (hit.damage > 0) {
+                t.graphics(76, 128, 0);
+                t.privateSound(1659);
+            } else {
+                t.graphics(85, 124, 0);
+                hit.hide();
+            }
+        });
+        target.hit(hit);
+        return true;
+    }
+}
