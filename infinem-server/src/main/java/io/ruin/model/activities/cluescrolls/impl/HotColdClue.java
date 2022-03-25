550108dd782a6f54e2eba2195286f4b07778880d
Presicci <mrbennjerry@gmail.com>
2022-03-24 23:05:48 -0500

    Moved ancient wizard and brassican mage handling to ClueEnemies

--- infinem-server/src/main/java/io/ruin/model/activities/cluescrolls/impl/HotColdClue.java
+++ infinem-server/src/main/java/io/ruin/model/activities/cluescrolls/impl/HotColdClue.java
@@ -2,13 +2,13 @@ package io.ruin.model.activities.cluescrolls.impl;
 
 import io.ruin.api.utils.Random;
 import io.ruin.model.activities.cluescrolls.Clue;
+import io.ruin.model.activities.cluescrolls.ClueEnemies;
 import io.ruin.model.activities.cluescrolls.ClueType;
 import io.ruin.model.combat.Hit;
 import io.ruin.model.entity.attributes.AttributeKey;
 import io.ruin.model.entity.npc.NPC;
 import io.ruin.model.entity.npc.NPCAction;
 import io.ruin.model.entity.player.Player;
-import io.ruin.model.entity.shared.listeners.DeathListener;
 import io.ruin.model.inter.InterfaceType;
 import io.ruin.model.inter.dialogue.*;
 import io.ruin.model.inter.utils.Option;
@@ -18,7 +18,6 @@ import io.ruin.model.map.Bounds;
 import io.ruin.model.map.MapRegion;
 import io.ruin.model.map.Position;
 import io.ruin.model.map.Tile;
-import io.ruin.model.map.route.routes.ProjectileRoute;
 import lombok.AllArgsConstructor;
 
 /**
@@ -128,76 +127,7 @@ public class HotColdClue extends Clue {
         for(int x = originalBounds.swX; x <= originalBounds.neX; x++) {
             for(int y = originalBounds.swY; y <= originalBounds.neY; y++) {
                 Position position = new Position(x, y, originalPosition.getZ());
-                Tile.get(position, true).digAction = (player) -> {
-                    int killedWizard = player.attributeOr(AttributeKey.KILLED_WIZARD, 0);
-                    if (killedWizard == 3 || clue.type == ClueType.BEGINNER) {
-                        player.clearAttribute(AttributeKey.KILLED_WIZARD);
-                        clue.advance(player);
-                    } else {
-                        boolean spawnedWizard = player.attributeOr(AttributeKey.SPAWNED_WIZARD, false);
-                        if (!spawnedWizard) {
-                            player.putAttribute(AttributeKey.SPAWNED_WIZARD, true);
-                            // Spawn a brassican mage in singles, ancient wizards in multi
-                            if (player.inMulti()) {
-                                NPC melee = new NPC(7309);
-                                NPC ranged = new NPC(7308);
-                                NPC magic = new NPC(7307);
-                                NPC[] wizards = new NPC[] { melee, ranged, magic };
-                                for (NPC wizard : wizards) {
-                                    wizard.targetPlayer(player, true);
-                                    wizard.removeOnDeath();
-                                    wizard.removeIfIdle(player);
-                                    wizard.removalAction = (p -> {
-                                        p.clearAttribute(AttributeKey.SPAWNED_WIZARD);
-                                    });
-                                    wizard.deathStartListener = (DeathListener.SimpleKiller) killer -> {
-                                        int killed = player.attributeOr(AttributeKey.KILLED_WIZARD, 0);
-                                        player.putAttribute(AttributeKey.KILLED_WIZARD, killed + 1);
-                                    };
-                                }
-                                Position[] positions = new Position[2];
-                                for (int index = 0; index < 3; index++) {
-                                    NPC wizard = wizards[index];
-                                    Bounds bounds = new Bounds(player.getPosition(), 5);
-                                    boolean hasSpawned = false;
-                                    while (!hasSpawned) {
-                                        Position pos = bounds.randomPosition();
-                                        if (ProjectileRoute.allow(player, pos) && !pos.equals(player.getPosition())
-                                                && (positions[0] == null || !pos.equals(positions[0]))
-                                                && (positions[1] == null || !pos.equals(positions[1]))) {
-                                            wizard.spawn(pos);
-                                            wizard.attackTargetPlayer(() -> !player.getPosition().isWithinDistance(wizard.getPosition()));
-                                            if (index < 2)
-                                                positions[index] = pos;
-                                            hasSpawned = true;
-                                        }
-                                    }
-                                }
-                            } else {
-                                NPC npc = new NPC(7310);
-                                npc.targetPlayer(player, true);
-                                npc.removeOnDeath();
-                                npc.removeIfIdle(player);
-                                npc.removalAction = (p -> {
-                                    p.clearAttribute(AttributeKey.SPAWNED_WIZARD);
-                                });
-                                npc.deathStartListener = (DeathListener.SimpleKiller) killer -> {
-                                    player.putAttribute(AttributeKey.KILLED_WIZARD, 3);
-                                };
-                                Bounds bounds = new Bounds(player.getPosition(), 3);
-                                boolean hasSpawned = false;
-                                while (!hasSpawned) {
-                                    Position pos = bounds.randomPosition();
-                                    if (ProjectileRoute.allow(player, pos) && !pos.equals(player.getPosition())) {
-                                        npc.spawn(pos);
-                                        npc.attackTargetPlayer(() -> !player.getPosition().isWithinDistance(npc.getPosition()));
-                                        hasSpawned = true;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                };
+                Tile.get(position, true).digAction = (player) -> ClueEnemies.ancientOrBrassicanDig(clue, player);
             }
         }
     }
