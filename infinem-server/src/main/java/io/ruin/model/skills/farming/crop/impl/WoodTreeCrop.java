120ccb01680194fd25c0fa040c86625357fe4f3c
Presicci <mrbennjerry@gmail.com>
2022-04-01 14:03:49 -0500

    Added correct farmer payments for wood tree patches

--- infinem-server/src/main/java/io/ruin/model/skills/farming/crop/impl/WoodTreeCrop.java
+++ infinem-server/src/main/java/io/ruin/model/skills/farming/crop/impl/WoodTreeCrop.java
@@ -3,17 +3,18 @@ package io.ruin.model.skills.farming.crop.impl;
 import io.ruin.api.utils.TimeUtils;
 import io.ruin.model.entity.player.PlayerCounter;
 import io.ruin.model.item.Item;
+import io.ruin.model.item.Items;
 import io.ruin.model.skills.farming.crop.TreeCrop;
 import io.ruin.model.skills.woodcutting.Tree;
 
 import static io.ruin.cache.ItemID.COINS_995;
 
 public enum WoodTreeCrop implements TreeCrop {
-	OAK(5312, 6043, 5370, 5358, 5364, Tree.OAK, 15, 14, 467.3, 4, new Item(COINS_995,  4000), 8, 22483, PlayerCounter.GROWN_OAK),
-	WILLOW(5313, 6045, 5371, 5359, 5365, Tree.WILLOW, 30, 25, 1456.5, 6, new Item(COINS_995,  8000), 15, 16059, PlayerCounter.GROWN_WILLOW),
-	MAPLE(5314, 6047, 5372, 5360, 5366, Tree.MAPLE, 45, 45, 3403.4, 8, new Item(COINS_995,  20000), 24, 14052, PlayerCounter.GROWN_MAPLE),
-	YEW(5315, 6049, 5373, 5361, 5367, Tree.YEW, 60, 81, 7069, 10, new Item(COINS_995,  70000), 35, 11242, PlayerCounter.GROWN_YEW),
-	MAGIC(5316,  6051, 5374, 5362, 5368, Tree.MAGIC, 75, 145.5, 13768.3, 12, new Item(COINS_995,  120000), 48, 9368, PlayerCounter.GROWN_MAGIC);
+	OAK(5312, 6043, 5370, 5358, 5364, Tree.OAK, 15, 14, 467.3, 4, new Item(Items.TOMATOES_5,  1), 8, 22483, PlayerCounter.GROWN_OAK),
+	WILLOW(5313, 6045, 5371, 5359, 5365, Tree.WILLOW, 30, 25, 1456.5, 6, new Item(Items.APPLES_5,  1), 15, 16059, PlayerCounter.GROWN_WILLOW),
+	MAPLE(5314, 6047, 5372, 5360, 5366, Tree.MAPLE, 45, 45, 3403.4, 8, new Item(Items.ORANGES_5,  1), 24, 14052, PlayerCounter.GROWN_MAPLE),
+	YEW(5315, 6049, 5373, 5361, 5367, Tree.YEW, 60, 81, 7069, 10, new Item(Items.CACTUS_SPINE,  10), 35, 11242, PlayerCounter.GROWN_YEW),
+	MAGIC(5316,  6051, 5374, 5362, 5368, Tree.MAGIC, 75, 145.5, 13768.3, 12, new Item(Items.COCONUT,  25), 48, 9368, PlayerCounter.GROWN_MAGIC);
 	
 	
 	WoodTreeCrop(int seedId, int roots, int sapling, int seedling, int wateredSeedling, Tree treeType, int levelReq, double plantXP, double harvestXP, int totalStages, Item payment, int containerIndex, int petOdds, PlayerCounter counter) {
