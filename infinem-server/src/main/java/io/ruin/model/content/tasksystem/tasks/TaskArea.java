e1521c0eb5e9c4d81a96983a23e849b23fc8a8b2
Presicci <mrbennjerry@gmail.com>
2022-04-04 18:06:45 -0500

    Added toString overriding for task areas

--- infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/TaskArea.java
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/TaskArea.java
@@ -5,5 +5,20 @@ package io.ruin.model.content.tasksystem.tasks;
  * Created on 8/17/2021
  */
 public enum TaskArea {
-     TIRANNWN, KANDARIN, FREMENNIK, KARAMJA, ASGARNIA, MISTHALIN, WILDERNESS, DESERT, MORYTANIA, ZEAH, GENERAL
+     TIRANNWN, KANDARIN, FREMENNIK, KARAMJA, ASGARNIA, MISTHALIN, WILDERNESS, DESERT, MORYTANIA, ZEAH, GENERAL("general/multiple regions");
+
+     public final String toString;
+
+     TaskArea() {
+          this.toString = super.toString();
+     }
+
+     TaskArea(String toString) {
+          this.toString = toString;
+     }
+
+     @Override
+     public String toString() {
+          return toString;
+     }
 }
