70e5bc3415a1dca9a3fe0a3c59c7db98aaf70701
Presicci <mrbennjerry@gmail.com>
2022-04-04 17:23:57 -0500

    Added equipTask handling for fire cape and infernal cape

---
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/EquipTasks.java
@@ -0,0 +1,20 @@
+package io.ruin.model.content.tasksystem.tasks.impl;
+
+import io.ruin.model.item.Items;
+import io.ruin.model.item.containers.equipment.EquipAction;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 4/4/2022
+ */
+public class EquipTasks {
+
+    static {
+        EquipAction.register(Items.FIRE_CAPE, (player -> {
+            player.getTaskManager().doLookupByUUID(418, 1);
+        }));
+        EquipAction.register(Items.INFERNAL_CAPE, (player -> {
+            player.getTaskManager().doLookupByUUID(429, 1);
+        }));
+    }
+}
