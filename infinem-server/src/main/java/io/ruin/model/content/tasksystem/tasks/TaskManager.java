c217300282e16bc9623b04feb8d4d83e2c6a2f20
Presicci <mrbennjerry@gmail.com>
2022-04-05 12:26:08 -0500

    Added category and trigger lookup that is way faster

--- infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/TaskManager.java
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/TaskManager.java
@@ -180,6 +180,79 @@ public class TaskManager {
         });
     }
 
+    public void doLookupByCategoryAndTrigger(TaskCategory category, String trigger, int amount, MapArea mapArea, boolean incremental) {
+        long currentTime = System.currentTimeMillis();
+        Server.gameDb.execute(connection -> {
+            PreparedStatement statement = null;
+            ResultSet rs = null;
+            try {
+                statement = connection.prepareStatement("SELECT * FROM task_list WHERE category = ? AND required_object = ?");
+                statement.setString(1, StringUtils.capitalizeFirst(category.toString().toLowerCase()));
+                statement.setString(2, trigger.toLowerCase());
+                rs = statement.executeQuery();
+                while (rs.next()) {
+                    int uuid = rs.getInt("uuid");
+                    if (completeTasks.contains(uuid)) {
+                        continue;
+                    }
+                    String area = rs.getString("maparea");
+                    if (area.trim().length() > 0 && (mapArea == null || !area.toLowerCase().equalsIgnoreCase(mapArea.toString().toLowerCase()))) {
+                        System.out.println("exit on map");
+                        continue;
+                    }
+                    int requiredAmount = rs.getInt("required_amount");
+                    int currentAmount = inProgressTasks.get(uuid) == null ? 0 : inProgressTasks.get(uuid);
+                    if (requiredAmount > 1) {
+                        if (incremental) {
+                            currentAmount += amount;
+                            if (currentAmount < requiredAmount) {
+                                inProgressTasks.put(uuid, currentAmount);
+                                System.out.println("Task #" + uuid + " progress: " + currentAmount + "/" + requiredAmount);
+                                continue;
+                            } else {
+                                inProgressTasks.remove(uuid);
+                            }
+                        } else {
+                            if (amount < requiredAmount) {
+                                System.out.println("exit on amt");
+                                continue;
+                            }
+                        }
+                    }
+                    String name = rs.getString("name");
+                    String taskArea = rs.getString("region").toLowerCase();
+                    TaskArea finalTaskarea = null;
+                    for (TaskArea ta : TaskArea.values()) {
+                        if (taskArea.trim().equalsIgnoreCase(ta.toString().toLowerCase())) {
+                            finalTaskarea = ta;
+                            break;
+                        }
+                    }
+                    String difficulty = rs.getString("difficulty").toLowerCase();
+                    TaskDifficulty finalDifficulty = null;
+                    for (TaskDifficulty diff : TaskDifficulty.values()) {
+                        if (difficulty.equalsIgnoreCase(diff.toString().toLowerCase())) {
+                            finalDifficulty = diff;
+                            break;
+                        }
+                    }
+                    if (finalDifficulty == null || finalTaskarea == null) {
+                        System.out.println("TASK ERROR! diff=" + difficulty.trim() + " / " + finalDifficulty + ", area=" + taskArea.trim() + " / " + finalTaskarea);
+                    } else {
+                        TaskArea finalTaskarea1 = finalTaskarea;
+                        TaskDifficulty finalDifficulty1 = finalDifficulty;
+                        player.addEvent(e -> {  // addEvent here to prevent sending packets in a thread
+                            completeTask(name, uuid, finalTaskarea1, finalDifficulty1);
+                        });
+                    }
+                }
+            } finally {
+                DatabaseUtils.close(statement, rs);
+                System.out.println(System.currentTimeMillis() - currentTime);
+            }
+        });
+    }
+
     public void doLookupByUUID(int uuid, int amount) {
         //  Task already completed, abort
         if (completeTasks.contains(uuid)) {
