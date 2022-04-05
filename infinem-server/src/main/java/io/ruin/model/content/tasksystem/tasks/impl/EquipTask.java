26c27ee21754df18c706a7fb8410f2e128751221
Presicci <mrbennjerry@gmail.com>
2022-04-05 10:16:46 -0500

    Converted class to an enum

---
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/EquipTask.java
@@ -0,0 +1,31 @@
+package io.ruin.model.content.tasksystem.tasks.impl;
+
+import io.ruin.model.item.Items;
+import io.ruin.model.item.containers.equipment.EquipAction;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 4/4/2022
+ */
+public enum EquipTask {
+
+    FIRE_CAPE(Items.FIRE_CAPE, 418),
+    INFERNAL_CAPE(Items.INFERNAL_CAPE, 429),
+    SPINY_HELMET(Items.SPINY_HELMET, 29),
+    ;
+
+    private final int itemId, uuid;
+
+    EquipTask(int itemId, int uuid) {
+        this.itemId = itemId;
+        this.uuid = uuid;
+    }
+
+    static {
+        for (EquipTask task : values()) {
+            EquipAction.register(task.itemId, (player -> {
+                player.getTaskManager().doLookupByUUID(task.uuid, 1);
+            }));
+        }
+    }
+}
