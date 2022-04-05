d199589e4e27ec9494d73862056dc651cfbd43bb
Presicci <mrbennjerry@gmail.com>
2022-04-05 16:14:02 -0500

    Category and trigger lookup can now find multitriggers

--- infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/TaskManager.java
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/TaskManager.java
@@ -195,9 +195,9 @@ public class TaskManager {
             PreparedStatement statement = null;
             ResultSet rs = null;
             try {
-                statement = connection.prepareStatement("SELECT * FROM task_list WHERE category = ? AND required_object = ?");
+                statement = connection.prepareStatement("SELECT * FROM task_list WHERE category = ? AND required_object LIKE ?");
                 statement.setString(1, StringUtils.capitalizeFirst(category.toString().toLowerCase()));
-                statement.setString(2, trigger.toLowerCase());
+                statement.setString(2, "%" + trigger.trim().toLowerCase() + "%");
                 rs = statement.executeQuery();
                 while (rs.next()) {
                     int uuid = rs.getInt("uuid");
