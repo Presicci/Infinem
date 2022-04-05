a3bc8ac340b5b2156e96b62da10153eb74a033b4
Presicci <mrbennjerry@gmail.com>
2022-04-05 17:07:37 -0500

    Updated varrock to use region ids

--- infinem-server/src/main/java/io/ruin/model/map/MapArea.java
+++ infinem-server/src/main/java/io/ruin/model/map/MapArea.java
@@ -10,15 +10,15 @@ import lombok.Getter;
 @Getter
 public enum MapArea {
     LUMBRIDGE_CASTLE(12850),
-    VARROCK(3170, 3376, 3299, 3506, 0),
+    VARROCK(12598, 12597, 12596, 12854, 12853, 12852, 13107, 13109, 13108),
     DRAGON_FORGE(1744, 5277, 1760, 5293, 1),
     WIZARD_TOWER(12337),
     ;
 
     private final Bounds bounds;
 
-    MapArea(int regionId) {
-        this.bounds = Bounds.fromRegion(regionId);
+    MapArea(int... regionId) {
+        this.bounds = Bounds.fromRegions(regionId);
     };
 
     MapArea(int southWestX, int southWestY, int northEastX, int northEastY, int z) {
