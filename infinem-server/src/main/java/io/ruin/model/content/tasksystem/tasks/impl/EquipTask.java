f6917c70fd64f732361f06c2d01bd8a5bd825e07
Presicci <mrbennjerry@gmail.com>
2022-04-05 17:04:15 -0500

    Added fancy/fighting boots task handling

--- infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/EquipTask.java
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/EquipTask.java
@@ -12,7 +12,9 @@ public enum EquipTask {
     FIRE_CAPE(Items.FIRE_CAPE, 418),
     INFERNAL_CAPE(Items.INFERNAL_CAPE, 429),
     SPINY_HELMET(Items.SPINY_HELMET, 29),
-    DRAGON_DEFENDER(Items.DRAGON_DEFENDER, 462)
+    DRAGON_DEFENDER(Items.DRAGON_DEFENDER, 462),
+    FANCY_BOOTS(Items.FANCY_BOOTS, 341),
+    FIGHTING_BOOTS(Items.FIGHTING_BOOTS, 341),
     ;
 
     private final int itemId, uuid;
