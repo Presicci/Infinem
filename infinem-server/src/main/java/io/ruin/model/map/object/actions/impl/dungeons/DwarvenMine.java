78fb26c822fcedcd9ecce4fe04fea67725feadf6
Presicci <mrbennjerry@gmail.com>
2022-04-14 23:03:38 -0500

    Fixed climb down position of ladder

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/DwarvenMine.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/DwarvenMine.java
@@ -13,7 +13,7 @@ public class DwarvenMine {
         /*
          * Dwarven entrance
          */
-        ObjectAction.register(11867, 3019, 3450, 0, "climb-down", (player, obj) -> Ladder.climb(player, 3020, 9850, 0, false, true, false));
+        ObjectAction.register(11867, 3019, 3450, 0, "climb-down", (player, obj) -> Ladder.climb(player, 3018, 9850, 0, false, true, false));
         ObjectAction.register(17387, 3019, 9850, 0, "climb-up", (player, obj) -> Ladder.climb(player, 3018, 3450, 0, true, true, false));
     }
 }
