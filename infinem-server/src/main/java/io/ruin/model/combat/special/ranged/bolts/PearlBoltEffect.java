1b85dcac697867cec26b2cd8ccf86bd625a9cc23
Presicci <mrbennjerry@gmail.com>
2022-06-26 15:29:51 -0500

    Fixed pearl bolt effect

--- infinem-server/src/main/java/io/ruin/model/combat/special/ranged/bolts/PearlBoltEffect.java
+++ infinem-server/src/main/java/io/ruin/model/combat/special/ranged/bolts/PearlBoltEffect.java
@@ -5,8 +5,11 @@ import io.ruin.cache.NPCDef;
 import io.ruin.model.combat.Hit;
 import io.ruin.model.entity.Entity;
 import io.ruin.model.entity.npc.NPC;
+import io.ruin.model.item.Items;
 import io.ruin.model.stat.StatType;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
 /**
@@ -16,19 +19,26 @@ import java.util.function.BiFunction;
 public class PearlBoltEffect implements BiFunction<Entity, Hit, Boolean> {
 
     private static final String[] fieryMonsters = {
-            "fire giant", "pyrefiend", "fire elemental", "bronze dragon", "iron dragon", "steel dragon", "mithril dragon", "adamant dragon", "rune dragon",
+            "fire giant", "pyrefiend", "pyrelord", "fire elemental", "bronze dragon", "iron dragon", "steel dragon", "mithril dragon", "adamant dragon", "rune dragon",
             "blue dragon", "green dragon", "red dragon", "black dragon", "king black dragon", "lava dragon"
     };
 
     @Override
     public Boolean apply(Entity target, Hit hit) {
+        if (hit.attacker.player == null)
+            return false;
         if(!Random.rollPercent(6))
             return false;
-        int newDamage = hit.damage + (int) (hit.attacker.player != null ? hit.attacker.player.getStats().get(StatType.Ranged).currentLevel * 0.5D : 0);
-        for (String name : fieryMonsters) {
-            if (target.npc != null && NPCDef.get(((NPC) target).getId()).name.equalsIgnoreCase(name)) {
-                newDamage *= 1.3;   // 30% damage boost against fire monsters
-            }
+        int newDamage = hit.damage;
+        if (target.player != null) {
+            boolean hasFire = target.player.getEquipment().hasAtLeastOneOf(Items.FIRE_CAPE, Items.STAFF_OF_FIRE, Items.FIRE_BATTLESTAFF, Items.MYSTIC_FIRE_STAFF);
+            boolean hasWater = target.player.getEquipment().hasAtLeastOneOf(Items.STAFF_OF_WATER, Items.WATER_BATTLESTAFF, Items.MYSTIC_WATER_STAFF);
+            if (hasWater)
+                return false;
+            newDamage += (int) (hit.attacker.player.getStats().get(StatType.Ranged).currentLevel * (hasFire ? 0.06 : 0.05));
+        } else if (target.npc != null) {
+            boolean fireMonster = (Arrays.stream(fieryMonsters).anyMatch(NPCDef.get(((NPC) target).getId()).name::equalsIgnoreCase));
+            newDamage += (int) (hit.attacker.player.getStats().get(StatType.Ranged).currentLevel * (fireMonster ? 0.06 : 0.05));
         }
         target.graphics(750);
         hit.fixedDamage(newDamage);
