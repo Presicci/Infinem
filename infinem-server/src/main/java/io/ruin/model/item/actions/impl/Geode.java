ce4661f4b94dc4ca673fa16445620be2c30f7f23
Presicci <mrbennjerry@gmail.com>
2022-07-05 22:46:37 -0500

    Made geode rolling more accurate to osrs, but with a much increased beginner chance

--- infinem-server/src/main/java/io/ruin/model/item/actions/impl/Geode.java
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/Geode.java
@@ -6,22 +6,29 @@ import io.ruin.model.item.actions.ItemAction;
 
 public enum Geode {
 
-    BEGINNER(23442, ClueType.BEGINNER.boxId),
-    EASY(20358, ClueType.EASY.boxId),
-    MEDIUM(20360, ClueType.MEDIUM.boxId),
-    HARD(20362, ClueType.HARD.boxId),
-    ELITE(20364, ClueType.ELITE.boxId);
+    BEGINNER(23442, ClueType.BEGINNER.boxId, 5),
+    EASY(20358, ClueType.EASY.boxId, 4),
+    MEDIUM(20360, ClueType.MEDIUM.boxId, 3),
+    HARD(20362, ClueType.HARD.boxId, 2),
+    ELITE(20364, ClueType.ELITE.boxId, 1);
 
-    public final int itemID, reward;
+    public final int itemID, reward, weight;
 
-    Geode(int itemID, int reward) {
+    Geode(int itemID, int reward, int weight) {
         this.itemID = itemID;
         this.reward = reward;
+        this.weight = weight;
     }
 
     public static int getRandomGeode() {
-        int randomGeode = Random.get(Geode.values().length - 1);
-        return Geode.values()[randomGeode].itemID;
+        int roll = Random.get(1, 11);
+        for (Geode geode : Geode.values()){
+            roll -= geode.weight;
+            if (roll <= 0) {
+                return geode.itemID;
+            }
+        }
+        return BEGINNER.itemID; // Default case
     }
 
     static {
