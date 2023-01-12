1a407c3a33ec1f4191f0efed42f6639ac26e4523
Presicci <mrbennjerry@gmail.com>
2023-01-12 16:57:04 -0600

    Added all shooting star locations from OSRS

--- infinem-server/src/main/java/io/ruin/model/skills/mining/ShootingStar.java
+++ infinem-server/src/main/java/io/ruin/model/skills/mining/ShootingStar.java
@@ -178,7 +178,109 @@ public enum ShootingStar {
 
     @Getter
     public enum StarLocation {
-        ASGARNIA(new Position(3018, 3443, 0));
+        ASGARNIA(
+                new Position(3018, 3443, 0),    // Dwarven mine
+                new Position(3030, 3349, 0),    // Mining guild
+                new Position(2906, 3355, 0),    // West Fally mine
+                new Position(2881, 3475, 0),    // Taverly
+                new Position(2940, 3280, 0),    // Crafting guild
+                new Position(2973, 3241, 0)     // Rimmington mine
+        ),
+        CRANDOR_AND_KARAMJA(
+                new Position(2822, 3238, 0),    // South crandor mine
+                new Position(2835, 3296, 0),    // North crandor mine
+                new Position(2734, 3221, 0),    // North brimhaven mine
+                new Position(2741, 3142, 0),    // South brimhaven mine
+                new Position(2844, 3038, 0),    // Karamja jungle mine
+                new Position(2827, 2998, 0)     // Shilo village mine
+        ),
+        FELDIP_HILLS_AND_THE_ISLE_OF_SOULS(
+                new Position(2571, 2964, 0),    // Feldip hills
+                new Position(2630, 2994, 0),    // Rantz's cave
+                new Position(2567, 2859, 0),    // Corsair cove
+                new Position(2482, 2886, 0),    // Corsair cove resource area
+                new Position(2468, 2843, 0),    // Myth's guild
+                new Position(2199, 2793, 0)     // Isle of souls mine
+        ),
+        FOSSIL_ISLAND_AND_MOS_LEHARMLESS(
+                new Position(3774, 3814, 0),    // Fossil island mine
+                new Position(3817, 3803, 0),    // Volcanic mine entrance
+                new Position(3686, 2970, 0)     // Mos Le'Harmless
+        ),
+        FREMENNIK_LANDS_AND_LUNAR_ISLE(
+                new Position(2683, 3699, 0),    // Rellekka mine
+                new Position(2726, 3683, 0),    // Keldagrim entrance mine
+                new Position(2528, 3887, 0),    // Miscellania mine
+                new Position(2392, 3814, 0),    // Jatizso mine entrance
+                new Position(2374, 3832, 0),    // Central Fremennik isles mine
+                new Position(2137, 3940, 0)     // Lunar isle mine entrance
+        ),
+        GREAT_KOUREND(
+                new Position(1778, 3493, 0),// Hosidius mine
+                new Position(1593, 3647, 0),// Shayzien mine
+                new Position(1768, 3711, 0),// Port piscarilius mine
+                new Position(1760, 3856, 0),// Dense essence mine
+                new Position(1438, 3840, 0),// Lavakite mine
+                new Position(1532, 3748, 0)// Lovakengj
+        ),
+        KANDARIN(
+                new Position(2805, 3435, 0),    // Catherby bank
+                new Position(2603, 3087, 0),    // Yanille bank
+                new Position(2624, 3142, 0),    // Port khazard mine
+                new Position(2704, 3333, 0),    // Legend's guild mine
+                new Position(2589, 3478, 0),    // Coal trucks
+                new Position(2608, 3233, 0)     // South-east ardy mine
+        ),
+        KEBOS_LOWLANDS(
+                new Position(1209, 3650, 0),    // Kebow lowlands mine
+                new Position(1279, 3818, 0),    // Mount karuulm mine
+                new Position(1322, 3816, 0),    // Mount karuulm bank
+                new Position(1258, 3564, 0)     // Mount quidamortem
+        ),
+        KHARIDIAN_DESERT(
+                new Position(3297, 3299, 0),    // Al kharid mine
+                new Position(3276, 3164, 0),    // Al kharid bank
+                new Position(3424, 3160, 0),    // Uzer mine
+                new Position(3170, 2911, 0),    // Desert quarry
+                new Position(3318, 2869, 0),    // Agility pyramid mine
+                new Position(3435, 2890, 0)     // Nardah
+        ),
+        MISTHALIN(
+                new Position(3230, 3154, 0),    // East lumbridge swamp mine
+                new Position(3152, 3152, 0),    // West lumbridge swamp mine
+                new Position(3094, 3237, 0),    // Draynor village
+                new Position(3257, 3409, 0),    // Varrock
+                new Position(3287, 3355, 0),    // South-east varrock mine
+                new Position(3176, 3361, 0)     // South-west varrock mine
+        ),
+        MORYTANIA(
+                new Position(3505, 3485, 0),    // Canifis
+                new Position(3499, 3219, 0),    // Burgh de Rott
+                new Position(3449, 3232, 0),    // Abandoned mine
+                new Position(3650, 3213, 0),    // Ver sinhaza
+                new Position(3635, 3340, 0)     // Daeyalt essence mine
+        ),
+        PISCATORIS_AND_THE_GNOME_STRONGHOLD(
+                new Position(2341, 3636, 0),    // Piscatoris mine
+                new Position(2444, 3490, 0),    // Grand tree
+                new Position(2448, 3436, 0)     // Tree gnome stronghold
+        ),
+        TIRANNWN(
+                new Position(2269, 3159, 0),    // Isafdar mine
+                new Position(2319, 3268, 0),    // Arandar mine
+                new Position(2329, 3163, 0),    // Lletya
+                new Position(3273, 6054, 0),    // Trahaearn mine
+                new Position(2175, 3410, 0)     // Mynydd mine
+        ),
+        WILDERNESS(
+                new Position(3108, 3570, 0),    // South wilderness mine
+                new Position(3018, 3593, 0),    // South-west wilderness mine
+                new Position(3091, 3755, 0),    // Bandit camp mine
+                new Position(3057, 3887, 0),    // Lava maze runite mine
+                new Position(3188, 3932, 0),    // Resource area
+                new Position(3092, 3962, 0),    // Mage arena
+                new Position(3049, 3941, 0)     // Pirate's hideout mine
+        );
 
         private final Position[] positions;
 
