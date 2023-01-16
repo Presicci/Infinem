f33d1b348fc3026934b95a9f9b90081f3100b4c8
Presicci <mrbennjerry@gmail.com>
2023-01-16 17:23:10 -0600

    Refactored

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/CorsairCoveDungeon.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/CorsairCoveDungeon.java
@@ -7,7 +7,7 @@ import io.ruin.model.map.object.actions.impl.Ladder;
  * @author Mrbennjerry - https://github.com/Mrbennjerry
  * Created on 11/11/2021
  */
-public class FeldipHills {
+public class CorsairCoveDungeon {
     static {
         ObjectAction.register(31791, 2523, 2861, 0, "enter", (player, obj) -> Ladder.climb(player, 2012, 9004, 1, false, true, false));
         ObjectAction.register(31790, 2012, 9005, 1, "climb", (player, obj) -> Ladder.climb(player, 2523, 2860, 0, true, true, false));
