a5dc201a153e24729883b681cd2eef845470b476
Presicci <mrbennjerry@gmail.com>
2023-01-16 17:31:49 -0600

    Added resource area entrance

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/CorsairCoveDungeon.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/CorsairCoveDungeon.java
@@ -1,5 +1,6 @@
 package io.ruin.model.map.object.actions.impl.dungeons;
 
+import io.ruin.model.entity.npc.actions.traveling.Traveling;
 import io.ruin.model.map.object.actions.ObjectAction;
 import io.ruin.model.map.object.actions.impl.Ladder;
 
@@ -9,7 +10,11 @@ import io.ruin.model.map.object.actions.impl.Ladder;
  */
 public class CorsairCoveDungeon {
     static {
+        // Ogress ladder
         ObjectAction.register(31791, 2523, 2861, 0, "enter", (player, obj) -> Ladder.climb(player, 2012, 9004, 1, false, true, false));
         ObjectAction.register(31790, 2012, 9005, 1, "climb", (player, obj) -> Ladder.climb(player, 2523, 2860, 0, true, true, false));
+        // Resource area cave entrance
+        ObjectAction.register(31766, 2482, 2890, 0, "enter", (player, obj) -> Traveling.fadeTravel(player, 1971, 9035, 1));
+        ObjectAction.register(31806, 1970, 9033, 1, "enter", (player, obj) -> Traveling.fadeTravel(player, 2483, 2889, 0));
     }
 }
