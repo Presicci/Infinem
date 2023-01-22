2e7718e034f13530dc334fe9e32fdd7ca9e8b056
Presicci <mrbennjerry@gmail.com>
2023-01-22 13:13:27 -0600

    Made a final list of all items that are used in tutor dialogues

---
+++ infinem-server/src/main/java/io/ruin/model/inter/dialogue/TutorDialogueItems.java
@@ -0,0 +1,52 @@
+package io.ruin.model.inter.dialogue;
+
+/**
+ * ItemIDs that are used in dialogue for tutor npcs or the like.
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 1/22/2023
+ */
+public interface TutorDialogueItems {
+    /*
+     * Tutor items have a blue background in the circle
+     */
+    public static final int LUMBRIDGE_GUIDE = 22567,
+            BANK_TUTOR = 22568,
+            SECURITY_TUTOR = 22569,
+            WOODCUTTING_TUTOR = 22570,
+            MINING_TUTOR = 22571,
+            FISHING_TUTOR = 22572,
+            HUNTER_TUTOR = 22573,
+            SMITHING_TUTOR = 22574,
+            CRAFTING_TUTOR = 22575,
+            COOKING_TUTOR = 22576,
+            COMBAT_TUTOR = 22577,
+            PRAYER_TUTOR = 22578,
+            DANGER_TUTOR = 22579,
+            IRONMAN_TUTOR = 22580,
+            WHITE_DEADMAN_TUTOR = 22581,
+            DEADMAN_TUTOR = 22582,
+            GRAVESTONE_TUTOR = 22583,
+            WEALTH_TUTOR = 22584,
+            BOND_TUTOR = 22585,
+            SKILL_TUTOR = 24360;
+
+    /*
+     * These just have a gray background
+     */
+    public static final int BANK = 5080,
+            MINE = 5082,
+            FURNACE = 5084,
+            ANVIL = 5086,
+            FISH = 5088,
+            RANGE = 5090,
+            QUEST = 5092,
+            YELLOW_QUEST = 24129,
+            PURPLE_QUEST = 22802,
+            GENERAL_STORE = 5094,
+            QUESTION_MARK = 5079,
+            DUNGEON = 22564,
+            WATER_SOURCE = 22563,
+            TRANSPORTATION = 22562,
+            TAVERN = 22560,
+            POLL = 22561;
+}
