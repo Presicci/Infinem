48aa65420737053ce60f3c3d7fc49ddb723132c0
Presicci <mrbennjerry@gmail.com>
2023-01-23 17:01:39 -0600

    Added ardy castle staircase

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Ardougne.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Ardougne.java
@@ -1,7 +1,10 @@
 package io.ruin.model.map.object.actions.impl.locations;
 
+import io.ruin.model.map.Direction;
+import io.ruin.model.map.Position;
 import io.ruin.model.map.object.actions.ObjectAction;
 import io.ruin.model.map.object.actions.impl.Ladder;
+import io.ruin.model.map.object.actions.impl.Stairs;
 
 /**
  * @author Mrbennjerry - https://github.com/Mrbennjerry
@@ -13,5 +16,7 @@ public class Ardougne {
         // Chaos druid basement
         ObjectAction.register(17385, 2562, 9756, 0, "climb-up", (player, obj) -> Ladder.climb(player, 2563, 3356, 0, true, true, false));
         ObjectAction.register(17384, 2562, 3356, 0, "climb-down", (player, obj) -> Ladder.climb(player, 2563, 9756, 0, false, true, false));
+        Stairs.registerStair(15645, new Position(2571, 3295, 0), Direction.SOUTH, 4);
+        Stairs.registerStair(15648, new Position(2571, 3295, 1), Direction.NORTH, 4);
     }
 }
