9a824bfe2e3ea2ff6fd0fd093b0f8a9e2d02c23c
Presicci <mrbennjerry@gmail.com>
2023-01-28 17:55:24 -0600

    Added waystones to kourend, varrock and camelot

--- infinem-server/src/main/java/io/ruin/model/content/waystones/Waystone.java
+++ infinem-server/src/main/java/io/ruin/model/content/waystones/Waystone.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Waystone {
+    // Varrock? Catherby?
     FALADOR(40408, new Position(2968, 3385), new Position(2967, 3385)),
     LUMBRIDGE(40409, new Position(3238, 3222), new Position(3237, 3222)),
     ARDOUGNE(40410, new Position(2680, 3308), new Position(2681, 3307)),
@@ -16,9 +17,10 @@ public enum Waystone {
     CANIFIS(40413, new Position(3494, 3493), new Position(3495, 3492)),
     PRIFDDINAS(40414, new Position(3266, 6068), new Position(3265, 6068)),
     BRIMHAVEN(40415, new Position(2804, 3186), new Position(2805, 3185)),
-    FEROX_ENCLAVE(40416, new Position(3146, 3631), new Position(3146, 3630))
-
-    ;
+    FEROX_ENCLAVE(40416, new Position(3146, 3631), new Position(3146, 3630)),
+    KOUREND(40414, new Position(1664, 3674), new Position(1662, 3676)),
+    VARROCK(40409, new Position(3223, 3426), new Position(3223, 3428)),
+    CAMELOT(40410, new Position(2751, 3480), new Position(2751, 3479));
 
     private final int objectId;
     private final Position objectPosition, teleportPostion;
