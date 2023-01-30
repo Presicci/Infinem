b8cd24ab2867f8ed82a06cc833b7bf1a4ff0832f
Presicci <mrbennjerry@gmail.com>
2023-01-29 22:27:35 -0600

    Added Waystone unlocking and prif waystone requirements

--- infinem-server/src/main/java/io/ruin/model/content/waystones/WaystoneNetwork.java
+++ infinem-server/src/main/java/io/ruin/model/content/waystones/WaystoneNetwork.java
@@ -1,11 +1,13 @@
 package io.ruin.model.content.waystones;
 
 import io.ruin.model.entity.player.Player;
+import io.ruin.model.inter.dialogue.MessageDialogue;
 import io.ruin.model.inter.handlers.OptionScroll;
 import io.ruin.model.inter.utils.Option;
 import io.ruin.model.item.actions.ItemAction;
 import io.ruin.model.map.object.GameObject;
 import io.ruin.model.map.object.actions.ObjectAction;
+import io.ruin.model.map.object.actions.impl.locations.prifddinas.CityEntrance;
 import io.ruin.model.skills.magic.spells.modern.ModernTeleport;
 
 import java.util.Arrays;
@@ -19,20 +21,40 @@ public class WaystoneNetwork {
     private static final int PORTABLE_WAYSTONE = 26549;
 
     private static void channelWaystone(Player player, Waystone waystone) {
-        player.startEvent(e -> ModernTeleport.teleport(player, waystone.getTeleportPostion().getX(), waystone.getTeleportPostion().getY(), waystone.getTeleportPostion().getZ()));
+        if (waystone == Waystone.PRIFDDINAS && !CityEntrance.prifSkillCheck(player)) {
+            return;
+        }
+        if (isWaystoneUnlocked(player, waystone)) {
+            player.startEvent(e -> ModernTeleport.teleport(player, waystone.getTeleportPostion().getX(), waystone.getTeleportPostion().getY(), waystone.getTeleportPostion().getZ()));
+        } else {
+            player.dialogue(new MessageDialogue("You do not have that Waystone unlocked."));
+        }
     }
 
-    // TODO <str> when waystone isnt unlocked
+    private static void unlockWaystone(Player player, Waystone waystone) {
+        player.animate(832);
+        player.getUnlockedWaystones().add(waystone);
+    }
+
+    private static boolean isWaystoneUnlocked(Player player, Waystone waystone) {
+        return player.getUnlockedWaystones().contains(waystone);
+    }
+
     private static void selectLocation(Player player) {
         OptionScroll.open(player, "Waystone Locations", true, Arrays.stream(Waystone.values())
-                .map(stone -> new Option(stone.getName(), () -> channelWaystone(player, stone)))
+                .map(stone -> new Option((player.getUnlockedWaystones().contains(stone) ? "" : "<str>") + stone.getName(), () -> channelWaystone(player, stone)))
                 .toArray(Option[]::new));
     }
 
-    private static void selectLocation(Player player, GameObject object) {
+    private static void selectLocation(Player player, GameObject object, Waystone waystone) {
+        if (!isWaystoneUnlocked(player, waystone)) {
+            unlockWaystone(player, waystone);
+            player.sendMessage("You unlock the " + waystone.getName() + " waystone.");
+            return;
+        }
         OptionScroll.open(player, "Waystone Locations", true, Arrays.stream(Waystone.values())
-                .filter(e -> e.getObjectId() != object.id)
-                .map(stone -> new Option(stone.getName(), () -> channelWaystone(player, stone)))
+                .filter(e -> e != waystone)
+                .map(stone -> new Option((player.getUnlockedWaystones().contains(stone) ? "" : "<str>") + stone.getName(), () -> channelWaystone(player, stone)))
                 .toArray(Option[]::new));
     }
 
@@ -40,7 +62,7 @@ public class WaystoneNetwork {
         ItemAction.registerInventory(PORTABLE_WAYSTONE, "teleport", ((player, item) -> selectLocation(player)));
         ItemAction.registerEquipment(PORTABLE_WAYSTONE, "teleport", ((player, item) -> selectLocation(player)));
         for (Waystone waystone : Waystone.values()) {
-            ObjectAction.register(waystone.getObjectId(), waystone.getObjectPosition(), 1, WaystoneNetwork::selectLocation);
+            ObjectAction.register(waystone.getObjectId(), waystone.getObjectPosition(), 1, (player, obj) -> selectLocation(player, obj, waystone));
         }
     }
 }
