2885b30bdc5f06b6ee0d19a64051f30f2b51b1a9
Presicci <mrbennjerry@gmail.com>
2023-02-15 17:35:46 -0600

    Fertile soil can now be used similarly to regular super compost, and gives experience

--- infinem-server/src/main/java/io/ruin/model/skills/magic/spells/lunar/FertileSoil.java
+++ infinem-server/src/main/java/io/ruin/model/skills/magic/spells/lunar/FertileSoil.java
@@ -6,9 +6,11 @@ import io.ruin.model.skills.farming.patch.Patch;
 import io.ruin.model.skills.farming.patch.impl.CompostBin;
 import io.ruin.model.skills.magic.Spell;
 import io.ruin.model.skills.magic.rune.Rune;
+import io.ruin.model.stat.StatType;
 
 public class FertileSoil extends Spell {
-
+//TODO ultracompost handling
+//https://oldschool.runescape.wiki/w/Fertile_Soil
     public FertileSoil() {
         Item[] runes = {
                 Rune.NATURE.toItem(2),
@@ -25,19 +27,18 @@ public class FertileSoil extends Spell {
                 p.sendMessage("No, that would be silly.");
                 return false;
             }
-            if (patch.getCompost() != 0) {
+            if (patch.getCompost() >= 2) {
                 p.sendMessage("That patch has already been treated.");
                 return false;
             }
-            if (patch.getPlantedCrop() != null && patch.getStage() >= patch.getPlantedCrop().getTotalStages()) {
+            if (patch.isFullyGrown()) {
                 p.sendMessage("Composting it isn't going to make it any bigger.");
                 return false;
             }
             p.animate(4413);
             World.sendGraphics(724, 0, 0, obj.x, obj.y, obj.z);
+            p.getStats().addXp(StatType.Magic, 87, true);
             patch.setCompost(2); // super
-            if (patch.getPlantedCrop() != null && patch.getStage() < patch.getPlantedCrop().getTotalStages())
-                patch.setProduceCount(patch.calculateProduceAmount());
             return true;
         });
     }
