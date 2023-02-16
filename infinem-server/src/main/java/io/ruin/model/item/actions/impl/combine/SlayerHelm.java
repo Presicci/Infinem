ee5f6b6f66267247f698135f540727f5af488062
Presicci <mrbennjerry@gmail.com>
2023-02-16 17:24:39 -0600

    Moved slayer helm variant handling to SlayerHelmVariant enum

--- infinem-server/src/main/java/io/ruin/model/item/actions/impl/combine/SlayerHelm.java
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/combine/SlayerHelm.java
@@ -31,36 +31,14 @@ public class SlayerHelm {
     public static final int SLAYER_HELM = 11864;
     public static final int SLAYER_HELM_IMBUE = 11865;
 
-    public static final int RED_SLAYER_HELM = 19647;
-    public static final int RED_HELM_IMBUE = 19649;
-    public static final int GREEN_SLAYER_HELM = 19643;
-    public static final int GREEN_HELM_IMBUE = 19645;
-    public static final int BLACK_SLAYER_HELM = 19639;
-    public static final int BLACK_HELM_IMBUE = 19641;
-    public static final int PURPLE_SLAYER_HELM = 21264;
-    public static final int PURPLE_HELM_IMBUE = 21266;
-    public static final int TURQUOISE_SLAYER_HELM = 21888;
-    public static final int TURQUOISE_HELM_IMBUE = 21890;
-    public static final int HYDRA_SLAYER_HELM = ItemID.HYDRA_SLAYER_HELMET;
-    public static final int HYDRA_HELM_IMBUE = ItemID.HYDRA_SLAYER_HELMET_I;
-
-    public static final int ABYSSAL_HEAD = 7979;
-    public static final int KQ_HEAD = 7981;
-    public static final int KBD_HEADS = 7980;
-    public static final int DARK_CLAW = 21275;
-    public static final int VORKATHS_HEAD = 2425;
-    public static final int VORKATH_SLAYER_HELM = ItemID.TURQUOISE_SLAYER_HELMET;
-    public static final int VORKATH_SLAYER_HELM_IMBUE = ItemID.TURQUOISE_SLAYER_HELMET_I;
-    public static final int HYDRAS_HEAD = ItemID.ALCHEMICAL_HYDRA_HEADS;
-
     private static final List<Integer> MELEE_BOOST_HELMS = Arrays.asList(
             8901, 8903, 8905, 8907, 8909, 8911, 8913, 8915, 8917, 8919, 8921, // black masks
-            SLAYER_HELM, RED_SLAYER_HELM, GREEN_SLAYER_HELM, BLACK_SLAYER_HELM, PURPLE_SLAYER_HELM, TURQUOISE_SLAYER_HELM, HYDRA_SLAYER_HELM
+            SLAYER_HELM
     );
 
     private static final List<Integer> ALL_BOOST_HELMS = Arrays.asList(
             8901, 8903, 8905, 8907, 8909, 8911, 8913, 8915, 8917, 8919, 8921, // black masks
-            SLAYER_HELM_IMBUE, RED_HELM_IMBUE, GREEN_HELM_IMBUE, BLACK_HELM_IMBUE, PURPLE_HELM_IMBUE, TURQUOISE_HELM_IMBUE, HYDRA_HELM_IMBUE
+            SLAYER_HELM_IMBUE
     );
 
     static {
@@ -70,7 +48,8 @@ public class SlayerHelm {
 
 
     public static boolean boost(Player player, Entity target, Hit hit) {
-        if (hit.attackStyle != null && target.npc != null && (Slayer.isTask(player, target.npc) || target.npc.getId() == 7413)) { // npc 7413 = undead combat dummy, always counts as task for max hit
+        if (hit.attackStyle != null && target.npc != null && (Slayer.isTask(player, target.npc)
+                || target.npc.getId() == 7413)) { // npc 7413 = undead combat dummy, always counts as task for max hit
             ItemDef helm = player.getEquipment().getDef(Equipment.SLOT_HAT);
             if (helm == null)
                 return false;
@@ -89,13 +68,13 @@ public class SlayerHelm {
 
     private static void makeMask(Player player, boolean imbued) {
         if(Config.SLAYER_UNLOCKED_HELM.get(player) != 1) {
-            player.sendMessage("You have not unlocked the ability to combine these items.");
+            player.sendMessage("You have not unlocked the ability to make a slayer helm.");
             return;
         }
 
         ArrayList<Item> items = player.getInventory().collectOneOfEach(NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, imbued ? BLACK_MASK_IMBUE : BLACK_MASK);
         if(items == null) {
-            player.sendMessage("You need a nosepeg, facemask, earmuffs, spiny helmet, enchanted gem and a black mask in your inventory in order to construct a Slayer helm.");
+            player.sendMessage("You need a nosepeg, facemask, earmuffs, spiny helmet, enchanted gem and a black mask in your inventory in order to make a Slayer helm.");
             return;
         }
         if(player.getStats().get(StatType.Crafting).currentLevel < 55) {
@@ -105,17 +84,6 @@ public class SlayerHelm {
         items.forEach(Item::remove);
         player.getInventory().add(imbued ? SLAYER_HELM_IMBUE : SLAYER_HELM, 1);
         player.sendMessage("You combine the items into a Slayer helm.");
-    }
-
-    private static void colorizeHelm(Player player, Item primary, Item secondary, int result, Config config) {
-        if(config.get(player) != 1) {
-            player.sendMessage("You need to unlock this ability from a Slayer master first!");
-            return;
-        }
-
-        primary.remove();
-        secondary.remove();
-        player.getInventory().add(result, 1);
     }
 
     private static void disassemble(Player player, Item item, int...itemIds) {
@@ -156,47 +124,19 @@ public class SlayerHelm {
         ItemItemAction.register(SPINY_HELM, BLACK_MASK_IMBUE, (player, primary, secondary) -> makeMask(player,  true));
 
         /*
-         * Colorizing
-         */
-        ItemItemAction.register(ABYSSAL_HEAD, SLAYER_HELM, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, RED_SLAYER_HELM, Config.UNHOLY_HELMET));
-        ItemItemAction.register(ABYSSAL_HEAD, SLAYER_HELM_IMBUE, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, RED_HELM_IMBUE, Config.UNHOLY_HELMET));
-        ItemItemAction.register(KBD_HEADS, SLAYER_HELM, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, BLACK_SLAYER_HELM, Config.KING_BLACK_BONNET));
-        ItemItemAction.register(KBD_HEADS, SLAYER_HELM_IMBUE, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, BLACK_HELM_IMBUE, Config.KING_BLACK_BONNET));
-        ItemItemAction.register(KQ_HEAD, SLAYER_HELM, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, GREEN_SLAYER_HELM, Config.KALPHITE_KHAT));
-        ItemItemAction.register(KQ_HEAD, SLAYER_HELM_IMBUE, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, GREEN_HELM_IMBUE, Config.KALPHITE_KHAT));
-        ItemItemAction.register(DARK_CLAW, SLAYER_HELM, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, PURPLE_SLAYER_HELM, Config.DARK_MANTLE));
-        ItemItemAction.register(DARK_CLAW, SLAYER_HELM_IMBUE, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, PURPLE_HELM_IMBUE, Config.DARK_MANTLE));
-        ItemItemAction.register(VORKATHS_HEAD, SLAYER_HELM, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, TURQUOISE_SLAYER_HELM, Config.UNDEAD_HEAD));
-        ItemItemAction.register(VORKATHS_HEAD, SLAYER_HELM_IMBUE, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, TURQUOISE_HELM_IMBUE, Config.UNDEAD_HEAD));
-        ItemItemAction.register(HYDRAS_HEAD, SLAYER_HELM, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, HYDRA_SLAYER_HELM, Config.UNDEAD_HEAD));
-        ItemItemAction.register(HYDRAS_HEAD, SLAYER_HELM_IMBUE, (player, primary, secondary) -> colorizeHelm(player, primary, secondary, HYDRA_HELM_IMBUE, Config.UNDEAD_HEAD));
-
-        /*
          * Disassemble
          */
         ItemAction.registerInventory(SLAYER_HELM, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK));
         ItemAction.registerInventory(SLAYER_HELM_IMBUE, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK_IMBUE));
-        ItemAction.registerInventory(RED_SLAYER_HELM, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK, ABYSSAL_HEAD));
-        ItemAction.registerInventory(RED_HELM_IMBUE, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK_IMBUE, ABYSSAL_HEAD));
-        ItemAction.registerInventory(GREEN_SLAYER_HELM, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK, KQ_HEAD));
-        ItemAction.registerInventory(GREEN_HELM_IMBUE, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK_IMBUE, KQ_HEAD));
-        ItemAction.registerInventory(BLACK_SLAYER_HELM, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK, KBD_HEADS));
-        ItemAction.registerInventory(BLACK_HELM_IMBUE, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK_IMBUE, KBD_HEADS));
-        ItemAction.registerInventory(PURPLE_SLAYER_HELM, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK, DARK_CLAW));
-        ItemAction.registerInventory(PURPLE_HELM_IMBUE, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK_IMBUE, DARK_CLAW));
-        ItemAction.registerInventory(TURQUOISE_SLAYER_HELM, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK, VORKATHS_HEAD));
-        ItemAction.registerInventory(TURQUOISE_HELM_IMBUE, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK_IMBUE, VORKATHS_HEAD));
-        ItemAction.registerInventory(HYDRA_SLAYER_HELM, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK, HYDRAS_HEAD));
-        ItemAction.registerInventory(HYDRA_HELM_IMBUE, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK_IMBUE, HYDRAS_HEAD));
 
-        for (int helm : Arrays.asList(SLAYER_HELM, SLAYER_HELM_IMBUE, RED_HELM_IMBUE, RED_SLAYER_HELM, GREEN_SLAYER_HELM, GREEN_HELM_IMBUE, BLACK_HELM_IMBUE, BLACK_SLAYER_HELM, PURPLE_HELM_IMBUE, PURPLE_SLAYER_HELM, TURQUOISE_SLAYER_HELM, TURQUOISE_HELM_IMBUE , HYDRA_SLAYER_HELM, HYDRA_HELM_IMBUE)) {
+        for (int helm : Arrays.asList(SLAYER_HELM, SLAYER_HELM_IMBUE)) {
             ItemAction.registerEquipment(helm, 4, (player, item) -> SlayerMaster.checkTask(player));
             ItemAction.registerEquipment(helm, "log", (player, item) -> KillCounter.openOwnSlayer(player));
             ItemAction.registerInventory(helm, "check", (player, item) -> SlayerMaster.checkTask(player));
-            ItemAction.registerInventory(ItemID.ENCHANTED_GEM, "check", (player, item) -> SlayerMaster.checkTask(player));
-            ItemAction.registerInventory(ItemID.ENCHANTED_GEM, "activate", (player, item) -> SlayerMaster.checkTask(player));
-            ItemAction.registerInventory(ItemID.ENCHANTED_GEM, "log", (player, item) -> KillCounter.openOwnSlayer(player));
         }
+        ItemAction.registerInventory(ItemID.ENCHANTED_GEM, "check", (player, item) -> SlayerMaster.checkTask(player));
+        ItemAction.registerInventory(ItemID.ENCHANTED_GEM, "activate", (player, item) -> SlayerMaster.checkTask(player));
+        ItemAction.registerInventory(ItemID.ENCHANTED_GEM, "log", (player, item) -> KillCounter.openOwnSlayer(player));
     }
 
 }
---
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/combine/SlayerHelmVariant.java
@@ -0,0 +1,87 @@
+package io.ruin.model.item.actions.impl.combine;
+
+import io.ruin.cache.ItemDef;
+import io.ruin.model.entity.player.KillCounter;
+import io.ruin.model.entity.player.Player;
+import io.ruin.model.inter.dialogue.YesNoDialogue;
+import io.ruin.model.inter.utils.Config;
+import io.ruin.model.item.Item;
+import io.ruin.model.item.Items;
+import io.ruin.model.item.actions.ItemAction;
+import io.ruin.model.item.actions.ItemItemAction;
+import io.ruin.model.skills.slayer.SlayerMaster;
+import lombok.AllArgsConstructor;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 2/16/2023
+ */
+@AllArgsConstructor
+public enum SlayerHelmVariant {
+    BLACK(19639, 19641, Items.KBD_HEADS, Config.KING_BLACK_BONNET),
+    GREEN(19643, 19645, Items.KQ_HEAD, Config.KALPHITE_KHAT),
+    RED(19647, 19649, Items.ABYSSAL_HEAD, Config.UNHOLY_HELMET),
+    PURPLE(21264, 21266, 21275, Config.DARK_MANTLE),
+    TURQUOISE(21888, 21890, 2425, Config.UNDEAD_HEAD),
+    HYDRA(23073, 23075, 23077, Config.USE_MORE_HEAD),
+    TWISTED(24370, 24444, 24466, null),   // Twisted Vision
+    TZTOK(25898, 25900, -1, null),
+    VAMPYRIC(25904, 25906, -1, null),
+    TZKAL(25910, 25912, -1, null);
+
+    private final int helmId, imbuedHelmId, requiredId;
+    private final Config configRequirement;
+
+    private void colorize(Player player, Item helm, Item requiredItem, boolean imbued) {
+        if (configRequirement != null && configRequirement.get(player) != 1) {
+            player.sendMessage("You need to unlock the ability to do this from a slayer master.");
+            return;
+        }
+        if (!isSlayerHelm(helm)) {
+            player.sendMessage("Nothing interesting happens.");
+            return;
+        }
+        player.dialogue(
+                new YesNoDialogue("Are you sure you want to recolour your Slayer helm?",
+                        "You can disassemble the helm for your " + requiredItem.getDef().name + " back.",
+                        imbued ? imbuedHelmId : helmId, 1, () -> {
+                    requiredItem.remove();
+                    helm.setId(imbued ? imbuedHelmId : helmId);
+                })
+        );
+    }
+
+    private void disassemble(Player player, Item helm, boolean imbued) {
+        if (player.getInventory().getFreeSlots() < 1) {
+            player.sendMessage("You need a free inventory slot to do this.");
+            return;
+        }
+        helm.setId(imbued ? Items.SLAYER_HELMET_I : Items.SLAYER_HELMET);
+        if (requiredId != -1) {
+            player.getInventory().add(requiredId, 1);
+        }
+    }
+
+    private static boolean isSlayerHelm(Item item) {
+        return item.getId() == Items.SLAYER_HELMET || item.getId() == Items.SLAYER_HELMET_I;
+    }
+
+    static {
+        for (SlayerHelmVariant variant : SlayerHelmVariant.values()) {
+            ItemAction.registerEquipment(variant.helmId, 4, (player, item) -> SlayerMaster.checkTask(player));
+            ItemAction.registerEquipment(variant.helmId, "log", (player, item) -> KillCounter.openOwnSlayer(player));
+            ItemAction.registerInventory(variant.helmId, "check", (player, item) -> SlayerMaster.checkTask(player));
+            ItemAction.registerInventory(variant.helmId, "disassemble", (player, item) -> variant.disassemble(player, item, false));
+            ItemAction.registerEquipment(variant.imbuedHelmId, 4, (player, item) -> SlayerMaster.checkTask(player));
+            ItemAction.registerEquipment(variant.imbuedHelmId, "log", (player, item) -> KillCounter.openOwnSlayer(player));
+            ItemAction.registerInventory(variant.imbuedHelmId, "check", (player, item) -> SlayerMaster.checkTask(player));
+            ItemAction.registerInventory(variant.imbuedHelmId, "disassemble", (player, item) -> variant.disassemble(player, item, true));
+            if (variant.requiredId != -1) {
+                ItemItemAction.register(Items.SLAYER_HELMET, variant.requiredId, ((player, primary, secondary) -> variant.colorize(player, primary, secondary, false)));
+                ItemItemAction.register(Items.SLAYER_HELMET_I, variant.requiredId, ((player, primary, secondary) -> variant.colorize(player, primary, secondary, true)));
+            }
+            ItemDef.get(variant.helmId).slayerBoostMelee = true;
+            ItemDef.get(variant.imbuedHelmId).slayerBoostAll = true;
+        }
+    }
+}
