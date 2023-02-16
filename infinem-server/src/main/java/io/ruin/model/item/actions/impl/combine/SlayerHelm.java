eca2a52a55d5153f726e1dfcabc7aa1a3610d143
Presicci <mrbennjerry@gmail.com>
2023-02-16 17:58:20 -0600

    Creating/disassembling slayer helms will now preserve upgrades Slayer helms can now be made with any charge of mask

--- infinem-server/src/main/java/io/ruin/model/item/actions/impl/combine/SlayerHelm.java
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/combine/SlayerHelm.java
@@ -8,6 +8,7 @@ import io.ruin.model.entity.player.KillCounter;
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.inter.utils.Config;
 import io.ruin.model.item.Item;
+import io.ruin.model.item.Items;
 import io.ruin.model.item.actions.ItemAction;
 import io.ruin.model.item.actions.ItemItemAction;
 import io.ruin.model.item.containers.Equipment;
@@ -25,25 +26,25 @@ public class SlayerHelm {
     public static final int FACEMASK = 4164;
     public static final int GEM = 4155;
     public static final int EARMUFFS = 4166;
-    public static final int BLACK_MASK = 8901;
-    public static final int BLACK_MASK_IMBUE = 11784;
     public static final int SPINY_HELM = 4551;
     public static final int SLAYER_HELM = 11864;
     public static final int SLAYER_HELM_IMBUE = 11865;
 
-    private static final List<Integer> MELEE_BOOST_HELMS = Arrays.asList(
-            8901, 8903, 8905, 8907, 8909, 8911, 8913, 8915, 8917, 8919, 8921, // black masks
-            SLAYER_HELM
+    private static final List<Integer> MASKS = Arrays.asList(
+            8901, 8903, 8905, 8907, 8909, 8911, 8913, 8915, 8917, 8919, 8921
     );
 
-    private static final List<Integer> ALL_BOOST_HELMS = Arrays.asList(
-            11774, 11775, 11776, 11777, 11778, 11779, 11780, 11781, 11782, 11783, 11784, // black masks
-            SLAYER_HELM_IMBUE
+    private static final List<Integer> IMBUED_MASKS = Arrays.asList(
+            11774, 11775, 11776, 11777, 11778, 11779, 11780, 11781, 11782, 11783, 11784
     );
+
+    private static final List<Integer> MASK_PARTS = Arrays.asList(4155, 4166, 4551, 4168, 4164);
 
     static {
-        MELEE_BOOST_HELMS.stream().map(ItemDef::get).forEach(def -> def.slayerBoostMelee = true);
-        ALL_BOOST_HELMS.stream().map(ItemDef::get).forEach(def -> def.slayerBoostAll = true);
+        MASKS.stream().map(ItemDef::get).forEach(def -> def.slayerBoostMelee = true);
+        IMBUED_MASKS.stream().map(ItemDef::get).forEach(def -> def.slayerBoostAll = true);
+        ItemDef.get(SLAYER_HELM).slayerBoostMelee = true;
+        ItemDef.get(SLAYER_HELM_IMBUE).slayerBoostAll = true;
     }
 
 
@@ -66,15 +67,20 @@ public class SlayerHelm {
         return false;
     }
 
-    private static void makeMask(Player player, boolean imbued) {
+    private static void makeMask(Player player) {
         if(Config.SLAYER_UNLOCKED_HELM.get(player) != 1) {
             player.sendMessage("You have not unlocked the ability to make a slayer helm.");
             return;
         }
-
-        ArrayList<Item> items = player.getInventory().collectOneOfEach(NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, imbued ? BLACK_MASK_IMBUE : BLACK_MASK);
-        if(items == null) {
-            player.sendMessage("You need a nosepeg, facemask, earmuffs, spiny helmet, enchanted gem and a black mask in your inventory in order to make a Slayer helm.");
+        boolean imbued = true;
+        ArrayList<Item> items = player.getInventory().collectOneOfEach(MASK_PARTS.stream().mapToInt(Integer::intValue).toArray());
+        Item mask = player.getInventory().findFirst(IMBUED_MASKS.stream().mapToInt(Integer::intValue).toArray());
+        if (mask == null) {
+            mask = player.getInventory().findFirst(MASKS.stream().mapToInt(Integer::intValue).toArray());
+            imbued = false;
+        }
+        if(items == null || mask == null) {
+            player.sendMessage("You need a nosepeg, facemask, earmuffs, spiny helmet, enchanted gem and a black mask to make a Slayer helm.");
             return;
         }
         if(player.getStats().get(StatType.Crafting).currentLevel < 55) {
@@ -82,17 +88,20 @@ public class SlayerHelm {
             return;
         }
         items.forEach(Item::remove);
-        player.getInventory().add(imbued ? SLAYER_HELM_IMBUE : SLAYER_HELM, 1);
+        mask.setId(imbued ? SLAYER_HELM_IMBUE : SLAYER_HELM);
         player.sendMessage("You combine the items into a Slayer helm.");
     }
 
-    private static void disassemble(Player player, Item item, int...itemIds) {
-        if(player.getInventory().getFreeSlots() < (itemIds.length - 1)) { //-1 because the item itself is being removed, which makes for 1 more slot.
+    private static void disassemble(Player player, Item item, boolean imbued) {
+        if(player.getInventory().getFreeSlots() < (MASK_PARTS.size())) {
             player.sendMessage("You don't have enough space to do this.");
             return;
         }
-        item.remove();
-        for(int itemId : itemIds)
+        if (imbued)
+            item.setId(Items.BLACK_MASK_I);
+        else
+            item.setId(Items.BLACK_MASK);
+        for(int itemId : MASK_PARTS)
             player.getInventory().add(itemId, 1);
         player.sendMessage("You disassemble your Slayer helm.");
     }
@@ -101,33 +110,23 @@ public class SlayerHelm {
         /*
          * Item combining
          */
-        ItemItemAction.register(NOSE_PEG, FACEMASK, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(NOSE_PEG, GEM, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(NOSE_PEG, EARMUFFS, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(NOSE_PEG, BLACK_MASK, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(NOSE_PEG, BLACK_MASK_IMBUE, (player, primary, secondary) -> makeMask(player,  true));
-        ItemItemAction.register(NOSE_PEG, SPINY_HELM, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(FACEMASK, GEM, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(FACEMASK, EARMUFFS, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(FACEMASK, FACEMASK, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(FACEMASK, BLACK_MASK, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(FACEMASK, BLACK_MASK_IMBUE, (player, primary, secondary) -> makeMask(player,  true));
-        ItemItemAction.register(FACEMASK, SPINY_HELM, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(GEM, EARMUFFS, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(GEM, BLACK_MASK, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(GEM, BLACK_MASK_IMBUE, (player, primary, secondary) -> makeMask(player,  true));
-        ItemItemAction.register(GEM, SPINY_HELM, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(EARMUFFS, BLACK_MASK, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(EARMUFFS, BLACK_MASK_IMBUE, (player, primary, secondary) -> makeMask(player,  true));
-        ItemItemAction.register(EARMUFFS, SPINY_HELM, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(SPINY_HELM, BLACK_MASK, (player, primary, secondary) -> makeMask(player,  false));
-        ItemItemAction.register(SPINY_HELM, BLACK_MASK_IMBUE, (player, primary, secondary) -> makeMask(player,  true));
+        for (int index = 0; index < MASK_PARTS.size(); index++) {
+            for (int itemId : MASK_PARTS.subList(index + 1, MASK_PARTS.size())) {
+                ItemItemAction.register(itemId, MASK_PARTS.get(index), (player, primary, secondary) -> makeMask(player));
+            }
+            for (int maskId : MASKS) {
+                ItemItemAction.register(maskId, MASK_PARTS.get(index), (player, primary, secondary) -> makeMask(player));
+            }
+            for (int maskId : IMBUED_MASKS) {
+                ItemItemAction.register(maskId, MASK_PARTS.get(index), (player, primary, secondary) -> makeMask(player));
+            }
+        }
 
         /*
          * Disassemble
          */
-        ItemAction.registerInventory(SLAYER_HELM, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK));
-        ItemAction.registerInventory(SLAYER_HELM_IMBUE, "disassemble", (player, item) -> disassemble(player, item, NOSE_PEG, FACEMASK, GEM, EARMUFFS, SPINY_HELM, BLACK_MASK_IMBUE));
+        ItemAction.registerInventory(SLAYER_HELM, "disassemble", (player, item) -> disassemble(player, item, false));
+        ItemAction.registerInventory(SLAYER_HELM_IMBUE, "disassemble", (player, item) -> disassemble(player, item, true));
 
         for (int helm : Arrays.asList(SLAYER_HELM, SLAYER_HELM_IMBUE)) {
             ItemAction.registerEquipment(helm, 4, (player, item) -> SlayerMaster.checkTask(player));
