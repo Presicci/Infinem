47f15327fc73949adc3f4b820c70ad96133fb03e
Presicci <mrbennjerry@gmail.com>
2023-02-20 14:41:35 -0600

    Fixed house exit for Rimmington

--- infinem-server/src/main/java/io/ruin/model/skills/construction/HouseLocation.java
+++ infinem-server/src/main/java/io/ruin/model/skills/construction/HouseLocation.java
@@ -10,7 +10,7 @@ import io.ruin.model.map.Position;
 import io.ruin.model.map.object.actions.ObjectAction;
 
 public enum HouseLocation {
-    RIMMINGTON(15478, 1, 5000, new Position(2030, 3570, 0)),
+    RIMMINGTON(15478, 1, 5000, new Position(2953, 3224, 0)),
     TAVERLEY(15477, 10, 5000, new Position(2893, 3465, 0)),
     POLLNIVNEACH(15479, 20, 7500, new Position(3340, 3003, 0)),
     GREAT_KOUREND(28822, 25, 8750, new Position(1742, 3517, 0)),
