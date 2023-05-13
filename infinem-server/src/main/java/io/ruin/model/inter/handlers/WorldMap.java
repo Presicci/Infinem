28ab5b6c955e5fb64b6e15ba3e4ff18e36753527
Thomas Presicci <thomaspresicci@gmail.com>
2023-05-12 22:11:44 -0500

    Updated world map to 199

--- infinem-server/src/main/java/io/ruin/model/inter/handlers/WorldMap.java
+++ infinem-server/src/main/java/io/ruin/model/inter/handlers/WorldMap.java
@@ -7,6 +7,9 @@ import io.ruin.model.inter.actions.SimpleAction;
 
 public class WorldMap {
     static {
-        InterfaceHandler.register(Interface.WORLD_MAP, h -> h.actions[37] = (SimpleAction) p -> p.closeInterface(InterfaceType.WORLD_MAP));
+        InterfaceHandler.register(Interface.WORLD_MAP, h -> {
+            h.actions[38] = (SimpleAction) p -> p.closeInterface(InterfaceType.WORLD_MAP);
+            h.actions[4] = (SimpleAction) p -> p.closeInterface(InterfaceType.WORLD_MAP);
+        });
     }
 }
