3305c98b62aa6ee06232b519cf6d49f8e23e466a
Thomas Presicci <thomaspresicci@gmail.com>
2023-05-22 17:15:47 -0500

    Reordered charter points so the ordinals match the varpbit indexes of the npcs

--- infinem-server/src/main/java/io/ruin/model/content/charterships/CharterPoints.java
+++ infinem-server/src/main/java/io/ruin/model/content/charterships/CharterPoints.java
@@ -11,17 +11,17 @@ import lombok.AllArgsConstructor;
 public enum CharterPoints {
         BRIMHAVEN(new Position(2760, 3238, 0), 17),
         CATHERBY(new Position(2792, 3414, 0), 8),
-        CORSAIR_COVE(new Position(2589, 2851, 0), 32),
-        MUSA_POINT(new Position(2954, 3158, 0), 14),
         MOS_LEHARMLESS(new Position(3671, 2931, 0), 26),
+        MUSA_POINT(new Position(2954, 3158, 0), 14),
         PORT_KHAZARD(new Position(2674, 3144, 0), 20),
         PORT_PHASMATYS(new Position(3702, 3503, 0), 5),
         PORT_SARIM(new Position(3038, 3192, 0), 23),
         SHIP_YARD(new Position(3001, 3032, 0), 11),
         PORT_TYRAS(new Position(2142, 3122, 0), 2),
-        CRANDOR(new Position(3853, 3235, 0), 29),
-        PRIF(new Position(2157, 3330, 0), 33);
+        CORSAIR_COVE(new Position(2589, 2851, 0), 32),
+        PRIF(new Position(2157, 3330, 0), 33),
+        CRANDOR(new Position(3853, 3235, 0), 29);
 
-        public Position pos;
-        public int component;
+        public final Position pos;
+        public final int component;
 }
