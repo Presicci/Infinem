8c1195c7c4f5f1f6c896ddd38340ca7ef60bbf82
Thomas Presicci <thomaspresicci@gmail.com>
2023-05-23 13:38:35 -0500

    Added heroes guild stairs and ladders

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/HeroesGuild.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/HeroesGuild.java
@@ -5,6 +5,7 @@ import io.ruin.model.inter.dialogue.MessageDialogue;
 import io.ruin.model.map.Direction;
 import io.ruin.model.map.object.GameObject;
 import io.ruin.model.map.object.actions.ObjectAction;
+import io.ruin.model.map.object.actions.impl.Ladder;
 import io.ruin.model.map.object.actions.impl.PassableDoor;
 import io.ruin.model.stat.StatList;
 import io.ruin.model.stat.StatType;
@@ -30,5 +31,11 @@ public class HeroesGuild {
         // Guild front door
         ObjectAction.register(2624, 2902, 3510, 0, "open", ((player, obj) -> guildDoor(player, obj, 2)));
         ObjectAction.register(2625, 2902, 3511, 0, "open", ((player, obj) -> guildDoor(player, obj, 0)));
+        // Stairs
+        ObjectAction.register(16671, 2895, 3513, 0, "climb-up", ((player, obj) -> player.getMovement().teleport(2897, 3513, 1)));
+        ObjectAction.register(16673, 2896, 3513, 1, "climb-down", ((player, obj) -> player.getMovement().teleport(2896, 3512, 0)));
+        // Dungeon
+        ObjectAction.register(17384, 2892, 3507, 0, "climb-down", ((player, obj) -> Ladder.climb(player, 2893, 9907, 0, false, true, false)));
+        ObjectAction.register(17387, 2892, 9907, 0, "climb-up", ((player, obj) -> Ladder.climb(player, 2893, 3507, 0, true, true, false)));
     }
 }
