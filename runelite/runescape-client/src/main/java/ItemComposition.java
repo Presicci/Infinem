3aa78d08cc1c9c5861b1c82c31372257ea975e53
Thomas Presicci <thomaspresicci@gmail.com>
2023-05-24 14:52:04 -0500

    Updated item def for new client

--- runelite/runescape-client/src/main/java/ItemComposition.java
+++ runelite/runescape-client/src/main/java/ItemComposition.java
@@ -383,6 +383,10 @@ public class ItemComposition extends DualNode {
 			this.isStackable = 1;
 		} else if (var2 == 12) {
 			this.price = var1.readInt();
+		} else if(var2 == 13) { // wearPos1
+			var1.readByte();
+		} else if(var2 == 14) { // wearPos2
+			var1.readByte();
 		} else if (var2 == 16) {
 			this.isMembersOnly = true;
 		} else if (var2 == 23) {
@@ -395,6 +399,8 @@ public class ItemComposition extends DualNode {
 			this.femaleOffset = var1.readUnsignedByte();
 		} else if (var2 == 26) {
 			this.femaleModel1 = var1.readUnsignedShort();
+		} else if(var2 == 27) { // wearPos3
+			var1.readByte();
 		} else if (var2 >= 30 && var2 < 35) {
 			this.groundActions[var2 - 30] = var1.readStringCp1252NullTerminated();
 			if (this.groundActions[var2 - 30].equalsIgnoreCase("Hidden")) {
@@ -427,6 +433,8 @@ public class ItemComposition extends DualNode {
 				this.shiftClickIndex = var1.readByte();
 			} else if (var2 == 65) {
 				this.isTradable = true;
+			} else if(var2 == 75) { // weight
+				var1.readShort();
 			} else if (var2 == 78) {
 				this.maleModel2 = var1.readUnsignedShort();
 			} else if (var2 == 79) {
