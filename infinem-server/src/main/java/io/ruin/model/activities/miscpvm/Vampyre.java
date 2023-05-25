34595df01260ba205368c6ba05a46bb1feb51825
Thomas Presicci <thomaspresicci@gmail.com>
2023-05-24 19:09:20 -0500

    Added Vampyres only being damaged by silver

---
+++ infinem-server/src/main/java/io/ruin/model/activities/miscpvm/Vampyre.java
@@ -0,0 +1,53 @@
+package io.ruin.model.activities.miscpvm;
+
+import io.ruin.model.combat.Hit;
+import io.ruin.model.entity.npc.NPCCombat;
+import io.ruin.model.entity.shared.listeners.HitListener;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 5/24/2023
+ */
+public class Vampyre extends NPCCombat {
+
+    @Override
+    public void init() {
+        npc.hitListener = new HitListener().preDefend(this::preDefend);
+    }
+
+    private boolean isSilver(String name) {
+        name = name.toLowerCase();
+        return name.contains("silver") || name.contains("ivandis") || name.contains("blisterwood");
+    }
+
+    public void preDefend(Hit hit) {
+        boolean block = false;
+        if (hit.attacker != null && hit.attacker.player != null) {
+            if (hit.attackStyle.isMelee() && (hit.attackWeapon == null || !isSilver(hit.attackWeapon.name))) {
+                block = true;
+            } else if (hit.attackStyle.isRanged() && (hit.rangedAmmo == null || !isSilver(hit.rangedAmmo.name))) {
+                block = true;
+            } else if (hit.attackStyle.isMagic()) {
+                block = true;
+            }
+            if (block) {
+                hit.block();
+                hit.attacker.player.sendMessage("Vampyres can only be harmed by silver weapons.");
+            }
+        }
+    }
+
+    @Override
+    public void follow() {
+        follow(1);
+    }
+
+    @Override
+    public boolean attack() {
+        if (!withinDistance(1))
+            return false;
+        basicAttack();
+        return true;
+
+    }
+}
