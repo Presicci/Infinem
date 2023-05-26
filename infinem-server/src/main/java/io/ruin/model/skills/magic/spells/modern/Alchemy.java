2cf31da6dc516f1e673c2a4bf39deae153e2fd82
Thomas Presicci <thomaspresicci@gmail.com>
2023-05-26 16:58:37 -0500

    Fixed alchemy warning allowing items to be alched twice

--- infinem-server/src/main/java/io/ruin/model/skills/magic/spells/modern/Alchemy.java
+++ infinem-server/src/main/java/io/ruin/model/skills/magic/spells/modern/Alchemy.java
@@ -14,6 +14,8 @@ import io.ruin.model.item.Items;
 import io.ruin.model.item.actions.impl.jewellery.BraceletOfEthereum;
 import io.ruin.model.skills.magic.Spell;
 import io.ruin.model.skills.magic.rune.Rune;
+import io.ruin.model.skills.magic.rune.RuneRemoval;
+import io.ruin.model.stat.StatType;
 
 import static io.ruin.cache.ItemID.COINS_995;
 
@@ -40,17 +42,24 @@ public class Alchemy extends Spell {
 
     public Alchemy(boolean low) {
         Level level = low ? Level.LOW : Level.HIGH;
-        registerItem(level.levelReq, level.xp, true, level.runes, (player, item) -> {
-            if (player.alchDelay.isDelayed()) {
-                // queue next alch
-                player.startEvent(event -> {
-                    event.delay(player.alchDelay.remaining());
-                    itemAction.accept(player, item);
-                });
-                return false;
+        itemAction = (p, i) -> {
+            if (!p.getStats().check(StatType.Magic, level.levelReq, "cast this spell"))
+                return;
+            RuneRemoval r = null;
+            if (level.runes != null && (r = RuneRemoval.get(p, level.runes)) == null) {
+                p.sendMessage("You don't have enough runes to cast this spell.");
+                return;
             }
-            return tryAlch(player, item, level);
-        });
+            if (p.alchDelay.isDelayed()) {
+                // queue next alch
+                p.startEvent(event -> {
+                    event.delay(p.alchDelay.remaining());
+                    itemAction.accept(p, i);
+                });
+                return;
+            }
+            tryAlch(p, i, r, level);
+        };
         clickAction = (p, i) -> {
             p.sendMessage("" + i);
             if (i == 9) {
@@ -79,10 +88,10 @@ public class Alchemy extends Spell {
         );
     }
 
-    private static boolean tryAlch(Player player, Item item, Level level) {
+    private static void tryAlch(Player player, Item item, RuneRemoval runes, Level level) {
         if (item.getId() == 7462 || item.getId() == Preset.DRAGON_2H_SWORD || item.getId() == BraceletOfEthereum.REVENANT_ETHER) {
             player.sendMessage("You can't alchemise this item.");
-            return false;
+            return;
         }
 
         int value = level == Level.LOW ? item.getDef().lowAlchValue : item.getDef().highAlchValue;
@@ -90,34 +99,34 @@ public class Alchemy extends Spell {
             value = item.getDef().fromNote().highAlchValue;
         if (value == 0 || item.getId() == COINS_995) {
             player.sendMessage("You can't alchemize this item.");
-            return false;
+            return;
         }
         if (item.getAmount() == 1 && item.getDef().stackable && !player.getInventory().hasRoomFor(COINS_995)) {
             player.sendMessage("Not enough space in your inventory.");
-            return false;
+            return;
         }
         if ((item.getId() == Rune.NATURE.getId() && item.getAmount() < level.runes[0].getAmount() + 1)
                 || (item.getId() == Rune.FIRE.getId() && item.getAmount() < level.runes[1].getAmount() + 1)) {
             player.sendMessage("You don't have the required runes to cast this spell.");
-            return false;
+            return;
         }
         if(item.getDef().free) {
             player.sendMessage("You can't alchemize this item.");
-            return false;
+            return;
         }
-        if (Config.ALCH_THRESHOLD.get(player) > 0 && item.getDef().value > Config.ALCH_THRESHOLD.get(player)) {
+        if (item.getDef().value > Config.ALCH_THRESHOLD.get(player)) {
             int finalValue = value;
             player.dialogue(new YesNoDialogue("Are you sure you want to alchemize that?",
                     item.getDef().name + " is above your alchemy warning threshold.", item, () -> {
-                alch(player, item, level, finalValue);
+                alch(player, item, runes, level, finalValue);
             }));
+            return;
         }
-        alch(player, item, level, value);
-        return true;
+        alch(player, item, runes, level, value);
     }
 
 
-    private static void alch(Player player, Item item, Level level, int value) {
+    private static void alch(Player player, Item item, RuneRemoval runes, Level level, int value) {
         if (item.getDef().stackable)
             item.remove(1);
         else
@@ -127,6 +136,9 @@ public class Alchemy extends Spell {
         player.getInventory().add(COINS_995, value);
         player.getPacketSender().sendClientScript(915, "i", 6);
         player.alchDelay.delay(level.delay);
+        if (runes != null)
+            runes.remove();
+        player.getStats().addXp(StatType.Magic, level.xp, true);
         if (level == Level.HIGH)
             player.getTaskManager().doLookupByUUID(148, 1); // Cast the High Level Alchemy spell
     }
