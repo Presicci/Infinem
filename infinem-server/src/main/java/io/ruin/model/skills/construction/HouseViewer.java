c35e2d62c3b42c63342091bc8827d155481af273
Thomas Presicci <thomaspresicci@gmail.com>
2023-05-29 01:07:07 -0500

    Updated house viewer interface to 199

--- infinem-server/src/main/java/io/ruin/model/skills/construction/HouseViewer.java
+++ infinem-server/src/main/java/io/ruin/model/skills/construction/HouseViewer.java
@@ -268,18 +268,18 @@ public class HouseViewer { // mostly just converted jordan's code to work with m
 
     static {
         InterfaceHandler.register(Interface.CONSTRUCTION_HOUSE_VIEWER, h -> {
-            h.actions[5] = (DefaultAction) (p, option, slot, itemId) -> addMoveRoom(p, slot, option == 1);
             for(int i = 6; i <= 38; i++) {
                 final int index = i - 6;
                 h.actions[i] = (SimpleAction) p -> viewRoom(p, index);
             }
-            h.actions[41] = (SimpleAction) HouseViewer::toPortal;
-            h.actions[59] = (SimpleAction) HouseViewer::rotate;
-            h.actions[60] = (SimpleAction) p -> rotate(p, true);
-            h.actions[61] = (SimpleAction) p -> rotate(p, false);
-            h.actions[62] = (SimpleAction) HouseViewer::delete;
-            h.actions[63] = (SimpleAction) HouseViewer::cancel;
-            h.actions[64] = (SimpleAction) HouseViewer::done;
+            h.actions[42] = (SimpleAction) HouseViewer::toPortal;
+            h.actions[59] = (DefaultAction) (p, option, slot, itemId) -> addMoveRoom(p, slot, option == 1);
+            h.actions[60] = (SimpleAction) HouseViewer::rotate;
+            h.actions[61] = (SimpleAction) p -> rotate(p, true);
+            h.actions[62] = (SimpleAction) p -> rotate(p, false);
+            h.actions[63] = (SimpleAction) HouseViewer::delete;
+            h.actions[64] = (SimpleAction) HouseViewer::cancel;
+            h.actions[65] = (SimpleAction) HouseViewer::done;
             h.closedAction = (p, i) -> {
                 p.houseViewerRooms = null;
                 p.houseViewerRoom = null;
