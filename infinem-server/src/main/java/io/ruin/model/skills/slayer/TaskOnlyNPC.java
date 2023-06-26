1c1d890b3c24b929e30d7b020072f0854c15eac6
Thomas Presicci <thomaspresicci@gmail.com>
2023-06-26 13:23:50 -0500

    Added BUGGY and SLIEVE task only npcs

--- infinem-server/src/main/java/io/ruin/model/skills/slayer/TaskOnlyNPC.java
+++ infinem-server/src/main/java/io/ruin/model/skills/slayer/TaskOnlyNPC.java
@@ -2,6 +2,8 @@ package io.ruin.model.skills.slayer;
 
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.inter.dialogue.NPCDialogue;
+import io.ruin.model.item.Items;
+import io.ruin.model.item.containers.Equipment;
 
 /**
  * @author Mrbennjerry - https://github.com/Presicci
@@ -9,7 +11,9 @@ import io.ruin.model.inter.dialogue.NPCDialogue;
  */
 public enum TaskOnlyNPC {
     NONE(-1, ""),
-    JELLY(7518, "Naughty human! You not hunting the Kurask! Jelly's Kurasks only for people on Slayer tasks.");
+    JELLY(7518, "Naughty human! You not hunting the Kurask! Jelly's Kurasks only for people on Slayer tasks."),
+    SLIEVE(7653, "Hey, go and train somewhere else! I'm not having people mess with my dragons unless a Slayer Master's told them to do it."),
+    BUGGY(491, "wooooo ooo woo-woo");
 
     private final int npcId;
     private final String dialogue;
@@ -23,9 +27,11 @@ public enum TaskOnlyNPC {
         if (npcId == -1) {
             player.sendMessage("You need to be on a slayer task to attack this monster.");
         } else {
-            player.dialogue(
-                    new NPCDialogue(npcId, dialogue)
-            );
+            if (this == BUGGY && player.getEquipment().getId(Equipment.SLOT_AMULET) == Items.GHOSTSPEAK_AMULET) {
+                player.dialogue(new NPCDialogue(npcId, "I didn't... create this cave for... you to do that... sorry, it's not for you."));
+            } else {
+                player.dialogue(new NPCDialogue(npcId, dialogue));
+            }
         }
     }
 }
