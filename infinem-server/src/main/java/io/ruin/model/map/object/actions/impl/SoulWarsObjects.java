bc197c0d6ddc4f8a743a40e96dd81600624836e2
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-02 18:54:54 -0500

    Added rest of soul wars portal options

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/SoulWarsObjects.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/SoulWarsObjects.java
@@ -1,5 +1,7 @@
 package io.ruin.model.map.object.actions.impl;
 
+import io.ruin.model.inter.dialogue.OptionsDialogue;
+import io.ruin.model.inter.utils.Option;
 import io.ruin.model.map.object.actions.ObjectAction;
 
 /**
@@ -10,11 +12,27 @@ public class SoulWarsObjects {
     static {
         // To soul wars lobby
         ObjectAction.register(40474, 1, (player, obj) -> {
-            player.getMovement().teleport(2206, 2857, player.getHeight());
+            player.getMovement().teleport(2206, 2857, 0);
+        });
+        ObjectAction.register(40475, 1, (player, obj) -> {
+            player.getMovement().teleport(2206, 2857, 0);
         });
         // To edgeville
+        ObjectAction.register(40476, 2, (player, obj) -> {
+            player.getMovement().teleport(3081, 3475, 0);
+        });
+        // To ferox
+        ObjectAction.register(40476, 3, (player, obj) -> {
+            player.getMovement().teleport(3158, 10027, 0);
+        });
+
         ObjectAction.register(40476, 1, (player, obj) -> {
-            player.getMovement().teleport(3082, 3476, player.getHeight());
+            player.dialogue(new OptionsDialogue("Where would you like to go?",
+                            new Option("Edgeville.", () -> player.getMovement().teleport(3081, 3475)),
+                            new Option("Ferox Enclave.", () -> player.getMovement().teleport(3158, 10027)),
+                            new Option("Nowhere.")
+                    )
+            );
         });
     }
 }
