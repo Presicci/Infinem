af554250eebf2226199e29799274fc64c39bb5cc
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-04 22:02:16 -0500

    Added nettle picking

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/Pickables.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/Pickables.java
@@ -3,8 +3,11 @@ package io.ruin.model.map.object.actions.impl;
 import io.ruin.api.utils.Random;
 import io.ruin.cache.ItemDef;
 import io.ruin.model.World;
+import io.ruin.model.combat.Hit;
+import io.ruin.model.combat.HitType;
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.item.Item;
+import io.ruin.model.item.containers.Equipment;
 import io.ruin.model.map.object.GameObject;
 import io.ruin.model.map.object.actions.ObjectAction;
 import lombok.AllArgsConstructor;
@@ -13,6 +16,7 @@ public class Pickables {
 
     @AllArgsConstructor
     public enum Nodes {
+        NETTLES(1181, 4241),
         FLAX(14896, 1779),
         CABBAGE(1161, 1965),
         WHEAT(15506, 1947),
@@ -32,6 +36,14 @@ public class Pickables {
                 player.lock();
                 player.animate(827);
                 event.delay(1);
+                if (this == NETTLES && player.getEquipment().get(Equipment.SLOT_HANDS) == null) {
+                    player.sendMessage("Ow! You prick your hand trying to pick the nettles.");
+                    Hit hit = new Hit(HitType.POISON);
+                    hit.randDamage(2);
+                    player.hit(hit);
+                    player.unlock();
+                    return;
+                }
                 player.getInventory().add(itemId, 1);
                 player.sendMessage("You pick some " + ItemDef.get(itemId).name.toLowerCase() + ".");
                 if(Random.rollDie(6, 1))
