a3fa00bc8f1fa30e5ea43d0cf5fb3f83482f3ef3
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-09 13:10:45 -0500

    Added turn in a pyramid top task handling

--- infinem-server/src/main/java/io/ruin/model/entity/npc/actions/SimonTempleton.java
+++ infinem-server/src/main/java/io/ruin/model/entity/npc/actions/SimonTempleton.java
@@ -46,6 +46,7 @@ public class SimonTempleton {
                                 int amt = player.getInventory().getAmount(Items.PYRAMID_TOP);
                                 player.getInventory().remove(Items.PYRAMID_TOP, amt);
                                 player.getInventory().add(995, PYRAMID_TOP_REWARD);
+                                player.getTaskManager().doLookupByUUID(653, 1); // Turn in a Pyramid Top to Simon Templeton
                                 player.dialogue(
                                         new ItemDialogue().one(Items.PYRAMID_TOP, "You hand over the artefact" + ((amt > 1) ? "s" : "") + " and Simon hands you " + NumberUtils.formatNumber((long) amt * PYRAMID_TOP_REWARD) + " coins."),
                                         new NPCDialogue(SIMON, "Ripper! Thanks a bundle, mate! Thanks to you I can fulfill me contract. You're a true blue! The boss will be pleased."),
