1c2439e321f16ca048a1b67d54971c75fd77d1fc
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-12 17:43:56 -0500

    Renamed MessageObject.java to DialogueObject.java, objects can now have dialogues instead of just message dialogues

---
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/DialogueObject.java
@@ -0,0 +1,38 @@
+package io.ruin.model.map.object.actions.impl;
+
+import io.ruin.model.inter.dialogue.Dialogue;
+import io.ruin.model.inter.dialogue.MessageDialogue;
+import io.ruin.model.map.Position;
+import io.ruin.model.map.object.actions.ObjectAction;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 7/1/2023
+ */
+public enum DialogueObject {
+    CRUMBLING_TOWER(1796, new Position(1936, 9444),
+            new MessageDialogue("Someone has crudely carved a message into the wall:<br><br>'The Soul Stone was here. Where is it now?")),
+    BRINE_RAT_CAVE_CHEST(14197, new Position(2740, 10164),
+            new MessageDialogue("An etching on the chest reads 'Titan.' There is nothing here to indicate how the ship got here or what they were searching for."));
+
+    private final int objectId, option;
+    private final Position position;
+    private final Dialogue[] dialogues;
+
+    DialogueObject(int objectId, Position position, Dialogue... dialogues) {
+        this(objectId, 1, position, dialogues);
+    }
+
+    DialogueObject(int objectId, int option, Position position, Dialogue... dialogues) {
+        this.objectId = objectId;
+        this.dialogues = dialogues;
+        this.position = position;
+        this.option = option;
+    }
+
+    static {
+        for (DialogueObject obj : values()) {
+            ObjectAction.register(obj.objectId, obj.position, obj.option, ((p, o) -> p.dialogue(obj.dialogues)));
+        }
+    }
+}
