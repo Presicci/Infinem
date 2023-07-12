7f69a31dbeab320949440377af18237c14e6f755
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-12 12:51:17 -0500

    Added giant's den entrance and exit

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/FadeTravelObject.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/FadeTravelObject.java
@@ -9,10 +9,19 @@ import io.ruin.model.map.object.actions.ObjectAction;
  * Created on 7/12/2023
  */
 public enum FadeTravelObject {
+    GIANTS_DEN_ENTRANCE(42248, new Position(1421, 3587), new Position(1432, 9913), 2796),
+    GIANTS_DEN_EXIT(42247, new Position(1431, 9914), new Position(1420, 3588), 2796),
     BRINE_RAT_CAVERN_EXIT_1(23158, new Position(2689, 10124), new Position(2729, 3734)),
     BRINE_RAT_CAVERN_EXIT_2(23157, new Position(2689, 10125), new Position(2729, 3734));
 
     FadeTravelObject(int objectId, Position objectPos, Position destination) {
         ObjectAction.register(objectId, objectPos, 1, ((player, obj) -> Traveling.fadeTravel(player, destination)));
+    }
+
+    FadeTravelObject(int objectId, Position objectPos, Position destination, int animation) {
+        ObjectAction.register(objectId, objectPos, 1, ((player, obj) -> {
+            player.animate(animation);
+            Traveling.fadeTravel(player, destination);
+        }));
     }
 }
