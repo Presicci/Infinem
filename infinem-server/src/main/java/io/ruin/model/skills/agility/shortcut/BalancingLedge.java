7c729f3dc5320dc5464011e84ac7df75361d7639
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-12 16:13:38 -0500

    Cleaned up the ledge balance code a bit, looks better

--- infinem-server/src/main/java/io/ruin/model/skills/agility/shortcut/BalancingLedge.java
+++ infinem-server/src/main/java/io/ruin/model/skills/agility/shortcut/BalancingLedge.java
@@ -17,15 +17,16 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum BalancingLedge {
 
-    WEISS_DOCK_1(68, 1, Position.of(2857, 3961)),
-    WEISS_DOCK_2(68, 1, Position.of(2853, 3961)),
-    YANILLE_DUNGEON_ENTRANCE_1(40, 1, Position.of(2580, 9512)),
-    YANILLE_DUNGEON_ENTRANCE_2(40, 1, Position.of(2580, 9520)),
+    WEISS_DOCK_1(68, 1, Position.of(2857, 3961), true),
+    WEISS_DOCK_2(68, 1, Position.of(2853, 3961), false),
+    YANILLE_DUNGEON_ENTRANCE_1(40, 1, Position.of(2580, 9512), false),
+    YANILLE_DUNGEON_ENTRANCE_2(40, 1, Position.of(2580, 9520), true),
     ;
 
-    private int level;
-    private double exp;
-    private Position to;
+    private final int level;
+    private final double exp;
+    private final Position to;
+    private final boolean reverse;
 
     public void traverse(Player player, GameObject object) {
         if (!player.getStats().check(StatType.Agility, level, "walk-across"))
@@ -33,12 +34,13 @@ public enum BalancingLedge {
         player.startEvent((event) -> {
             player.lock(LockType.FULL_DELAY_DAMAGE);
             Direction dir = Direction.getDirection(object.getPosition(), to);
-            boolean reverse = dir.equals(Direction.NORTH) || dir.equals(Direction.WEST);
+            player.face(dir);
+            event.delay(1);
             player.animate(reverse ? 753 : 752);
             player.stepAbs(object.getPosition().getX(), object.getPosition().getY(), StepType.FORCE_WALK);
             player.getAppearance().setCustomRenders(reverse ? Renders.AGILITY_WALL : Renders.AGILITY_JUMP);
             event.delay(2);
-            int stepsNeeded = object.getPosition().distance(to);
+            int stepsNeeded = player.getPosition().distance(to);
             for (int step = 0; step < stepsNeeded; step++) {
                 player.stepAbs(to.getX(), to.getY(), StepType.FORCE_WALK);
                 player.privateSound(2451, 2, 0);
