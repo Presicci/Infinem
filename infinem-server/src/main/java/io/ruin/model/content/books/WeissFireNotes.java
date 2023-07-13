231b7ac1a2e75e990d046f895e427c845f6a35d5
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-12 19:06:18 -0500

    Added Weiss Fire Notes book

---
+++ infinem-server/src/main/java/io/ruin/model/content/books/WeissFireNotes.java
@@ -0,0 +1,106 @@
+package io.ruin.model.content.books;
+
+import io.ruin.model.content.Book;
+import io.ruin.model.entity.player.Player;
+import io.ruin.model.item.actions.ItemAction;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 7/12/2023
+ */
+public class WeissFireNotes extends Book {
+
+    public WeissFireNotes(final Player player) {
+        super(player);
+    }
+
+    @Override
+    public String getString() {
+        return "<col=800000> Salts </col> \n" +
+                "The salts found in the caves around Weiss are fascinating. By burning them in the correct proportions, I can achieve incredible effects." +
+                " <br> " +
+                "Some combinations can counter the effects of cold weather, others can dispel darkness more effectively than any natural light source, and some seem able to imbue the rocks of the cave with magical-looking powers!" +
+                " <br> " +
+                "The challenge for me has been identifying the correct proportions. And, of course, finding suitable places to use them - my age makes it hard for me to explore beyond the boundaries of Weiss these days." +
+                " <br> " +
+                "Perhaps other combinations will be discovered in future, with even more helpful applications. Knowledge is valuable in itself, but that dolt Mother values only inventions that help him fight." +
+                " <br> " +
+                "<col=800000> Braziers </col> \n" +
+                "We trolls have never been skilled at Construction, and I get little opportunity to practice here." +
+                " <br> " +
+                "However, from my calculations, I believe the optimal site for my fires would be a brazier constructed from <col=272727>steel<col> and <col=3f2f1f>mahogany </col>. Two metal bars and two planks should suffice to construct the frame." +
+                " <br> " +
+                "One should then be able to ignite the salts in the brazier to get the desired effect." +
+                " <br> " +
+                "<col=800000> Fire of Eternal Light </col> \n" +
+                "This was one of the easiest fires to invent; by burning a stack of <col=800000>  300 Te Salt </col>, <col=000080>  100 Efh Salt </col> and <col=008000>  50 Urt Salt </col>, I produced a flame that lit up my entire cave." +
+                " <br> " +
+                "Its light shone more brightly than any torch or lantern I have seen, and as far as I can tell it shows no sign of ever burning out." +
+                " <br> " +
+                "<col=800000> Fire of Nourishment </col> \n" +
+                "Although we are surrounded by trees, related to the pine family, and there are a few pale flowers, little else seems to grow here. I originally believed this was due to the cold, and I invented the Fire of Nourishment to counteract it." +
+                " <br> " +
+                "The fire requires <col=800000> 50 Te Salt </col>, <col=000080> 100 Efh Salt </col> and <col=008000> 150 Urt Salt </col>." +
+                " <br> " +
+                "Unfortunately, I later found that the soil around Weiss is barren and unsuitable for crops. Mother called my work a complete waste of time - he cannot grasp that this knowledge may be useful in future, even if he can't use it right now." +
+                " <br> " +
+                "He is such a limited ruler!" +
+                " <br> " +
+                "<col=800000> Fire of Unseasonal Warmth </col> \n" +
+                "Living in the cold mountains, I naturally turned my attention to the matter of keeping warm, and succeeded beyond all expectations!" +
+                " <br> " +
+                "By burning a stack of <col=800000>  100 Te Salt </col>, <col=000080>  50 Efh Salt </col> and <col=008000>  300 Urt Salt </col>, I produced a flame that completely dispelled the chill of the weather." +
+                " <br> " +
+                "In some parts of the mountains, the cold is so intense that one loses all one's abilities. I feel sure that my Fire of Unseasonal Warmth would render one completely immune to that problem." +
+                " <br> " +
+                "<col=800000> Fire of Dehumidification </col> \n" +
+                "I stumbled upon this one by accident. A stack of <col=800000> 50 Te Salt </col>, <col=000080> 300 Efh Salt </col> and <col=008000> 100 Urt Salt </col> burnt with a strange heat that dried out my cave - obviously fire should do that, but this one did it much faster than normal. I imagine that if one were to light this fire in a damp environment, it would provide protection from rotting effects and decay." +
+                " <br> " +
+                "Maybe if I live long enough, I will find a way to travel the world and find such a place." +
+                " <br> " +
+                "<col=800000> Fire of Domination </col> \n" +
+                "Mother was very pleased with me when I presented him the recipe for this fire. It gave him simply incredible powers in combat, rendering him almost immune to any damage!" +
+                " <br> " +
+                "I also noticed that it disrupted my experiments with the Glowing Basalt teleporters; this would be helpful for Mother if he wished to prevent a mage from fleeing via their teleport magicks." +
+                " <br> " +
+                "The combination for this fire was extremely hard to get right, but I eventually -" +
+                " <br> " +
+                "Odd Mushroom's notes were damaged, and I cannot find the rest of this recipe. I fear it is lost forever. - Snowflake" +
+                " <br> " +
+                "<col=800000> Glowing Basalt </col> \n" +
+                "The basalt rocks beneath Weiss have been altered by the powers of the salts, probably over millennia. This means I can modify them further by treating them with carefully calibrated combinations of salts, achieving effects that - frankly - look indistinguishable from magic." +
+                " <br> " +
+                "With the correct combination of salts, the basalt can be made to transport matter - rocks, logs, even living creatures like goats - from place to place." +
+                " <br> " +
+                "Perhaps it truly is magic - it certainly acts like the teleport spells of which I have heard!" +
+                " <br> " +
+                "My pupil, Snowflake, has helped me greatly with these experiments. She is far more intelligent than her father, Mother. Perhaps she inherited her brains from her mother, not from Mother." +
+                " <br> " +
+                "In fact Snowflake did most of the work on refining the destinations of our matter transporters. Maybe she's cleverer than me... but I won't tell her that yet." +
+                " <br> " +
+                "There'll be plenty more time for us to chat - I'm not old enough to fear death yet." +
+                " <br> " +
+                "Anyway, Snowflake worked out that <col=000080> 3 Efh Salt </col> and <col=800000> 1 Te Salt </col> can make a matter transporter focused on the woods outside Weiss where she used to play, as a child." +
+                " <br> " +
+                "Maybe she will invent more combinations for other destinations soon." +
+                " <br> " +
+                "From what my darling My Arm has told me of his homeland, a dose of <col=008000> 3 Urt Salt </col> and <col=800000> 1 Te Salt </col> should direct the focus to the Troll Stronghold. - Snowflake" +
+                " <br> " +
+                "I saw Odd Mushroom invent many other fires, but I cannot find any more of his notes." +
+                " <br> " +
+                "Perhaps Father destroyed them, not understanding their value. Or perhaps they are merely lost, and will be found again." +
+                " <br> " +
+                "I shall continue to search when I can, or perhaps I will rediscover the secrets myself! If I do, I shall prepare an updated edition of this book for my friend." +
+                " <br> " +
+                "- Snowflake";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Weiss Fire Notes";
+    }
+
+    static {
+        ItemAction.registerInventory(22591, "read", ((p, item) -> Book.openBook(new WeissFireNotes(p))));
+    }
+}
