aba73327b3f2528c520b535b8a208ff848677838
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-14 12:37:38 -0500

    Added goat dung gathering

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/Dung.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/Dung.java
@@ -1,5 +1,6 @@
 package io.ruin.model.map.object.actions.impl;
 
+import io.ruin.model.World;
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.item.Items;
 import io.ruin.model.item.actions.ItemObjectAction;
@@ -12,21 +13,32 @@ import io.ruin.model.map.object.actions.ObjectAction;
  */
 public class Dung {
 
-    private static void pickup(Player player, GameObject object) {
+    private static void pickup(Player player, GameObject object, int productId, boolean removeForever) {
         if (!player.getInventory().contains(Items.BUCKET)) {
             player.sendMessage("You should really use a bucket for that.");
             return;
         }
         player.animate(827);
         player.getInventory().remove(Items.BUCKET, 1);
-        player.getInventory().add(Items.UGTHANKI_DUNG);
+        player.getInventory().add(productId);
         player.sendMessage("You very carefully scoop the dung up with the bucket.");
-        if (!object.isRemoved())
-            object.remove();
+        if (removeForever) {
+            if (!object.isRemoved())
+                object.remove();
+        } else {
+            World.startEvent(e -> {
+                object.setId(-1);
+                e.delay(15);
+                object.restore();
+            });
+        }
+
     }
 
     static {
-        ObjectAction.register(6257, "pick-up", Dung::pickup);
-        ItemObjectAction.register(1925, 6257, (player, item, obj) -> pickup(player, obj));
+        ObjectAction.register(6257, "pick-up", (player, object) -> pickup(player, object, Items.UGTHANKI_DUNG, true));
+        ObjectAction.register(33214, "pick-up", (player, object) -> pickup(player, object, 22590, false)); // Goat dung
+        ItemObjectAction.register(1925, 6257, (player, item, obj) -> pickup(player, obj, Items.UGTHANKI_DUNG, true));
+        ItemObjectAction.register(1925, 33214, (player, item, obj) -> pickup(player, obj, 22590, false));  // Goat dung
     }
 }
