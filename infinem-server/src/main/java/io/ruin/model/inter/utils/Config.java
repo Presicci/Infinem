f8b7c6a8cbbb85a87cf3acbf24af5ef48d7f7a9d
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-14 01:06:30 -0500

    Made Making Friends With My Arm config savable

--- infinem-server/src/main/java/io/ruin/model/inter/utils/Config.java
+++ infinem-server/src/main/java/io/ruin/model/inter/utils/Config.java
@@ -1107,7 +1107,7 @@ public class Config {
     public static final Config WITCHAVEN_DUNGEON = varpbit(2618, false).defaultValue(1);
     public static final Config KOUREND_STATUE = varpbit(4982, false).defaultValue(3);
     public static final Config KELDAGRIM_BOATMAN = varpbit(571, false).defaultValue(1); // Changes caves boatman to variant with travel option
-    public static final Config MAKING_FRIENDS_WITH_MY_ARM = varpbit(6528, false).defaultValue(75);
+    public static final Config MAKING_FRIENDS_WITH_MY_ARM = varpbit(6528, true).defaultValue(75);
     public static final Config COSTUME_DEPOSIT_SET = varpbit(10581, false);
 
     /**
