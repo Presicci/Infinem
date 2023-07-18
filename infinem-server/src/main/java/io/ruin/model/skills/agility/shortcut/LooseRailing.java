7179971fd4f927edc7949020abd398a686d995ce
Thomas Presicci <thomaspresicci@gmail.com>
2023-07-18 14:30:08 -0500

    Made railing shortcuts use proper directions and animations

--- infinem-server/src/main/java/io/ruin/model/skills/agility/shortcut/LooseRailing.java
+++ infinem-server/src/main/java/io/ruin/model/skills/agility/shortcut/LooseRailing.java
@@ -2,22 +2,40 @@ package io.ruin.model.skills.agility.shortcut;
 
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.entity.shared.LockType;
+import io.ruin.model.entity.shared.StepType;
+import io.ruin.model.map.Direction;
 import io.ruin.model.map.object.GameObject;
 import io.ruin.model.stat.StatType;
 
 public class LooseRailing {
 
-    public static void shortcut(Player p, GameObject looseRailing, int levelReq) {
-        if (!p.getStats().check(StatType.Agility, levelReq, "attempt this"))
+    public static void shortcut(Player player, GameObject obj, int levelReq) {
+        if (!player.getStats().check(StatType.Agility, levelReq, "attempt this"))
             return;
-        p.startEvent(e -> {
-            p.lock(LockType.FULL_DELAY_DAMAGE);
-            if(p.getAbsX() == looseRailing.x)
-                p.getMovement().teleport(p.getAbsX() - 1, p.getAbsY());
-            else
-                p.getMovement().teleport(p.getAbsX() + 1, p.getAbsY());
-            e.delay(1);
-            p.unlock();
+        int objDir = obj.direction;
+        boolean onObjectSide = (objDir == 0 && player.getAbsX() == obj.getPosition().getX())
+                || (objDir == 2 && player.getAbsX() <= obj.getPosition().getX())
+                || (objDir == 3 && player.getAbsY() >= obj.getPosition().getY())
+                || (objDir == 1 && player.getAbsY() <= obj.getPosition().getY());
+        int diffX = objDir == 0 ? player.getAbsX() >= obj.getPosition().getX() ? -1 : 1
+                : objDir == 2 ? player.getAbsX() <= obj.getPosition().getX() ? 1 : -1 : 0;
+        int diffY = objDir == 3 ? player.getAbsY() >= obj.getPosition().getY() ? -1 : 1
+                : objDir == 1 ? player.getAbsY() <= obj.getPosition().getY() ? 1 : -1 : 0;
+        player.lock(LockType.FULL_DELAY_DAMAGE);
+        player.startEvent(e -> {
+            if (onObjectSide && !player.getPosition().equals(obj.getPosition())) {
+                player.stepAbs(obj.getPosition().getX(), obj.getPosition().getY(), StepType.FORCE_WALK);
+                e.delay(1);
+                player.face(obj);
+                e.delay(1);
+            }
+            player.animate(1237);
+            Direction direction = Direction.getDirection(player.getPosition(), obj.getPosition());
+            if (direction == null)
+                direction = Direction.fromDoorDirection(objDir);
+            player.getMovement().force(player.getPosition().relative(diffX, diffY), 0, 60, direction);
+            e.delay(2);
+            player.unlock();
         });
 
     }
