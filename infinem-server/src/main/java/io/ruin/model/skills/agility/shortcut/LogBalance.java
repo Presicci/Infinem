3eb22e38dc9c38ab8a43c462b24de9a08f9fdd1d
Presicci <thomaspresicci@gmail.com>
2023-07-19 17:29:56 -0500

    Made LogBalance an enum and moved registration and shortcuts over

--- infinem-server/src/main/java/io/ruin/model/skills/agility/shortcut/LogBalance.java
+++ infinem-server/src/main/java/io/ruin/model/skills/agility/shortcut/LogBalance.java
@@ -7,6 +7,8 @@ import io.ruin.model.entity.shared.Renders;
 import io.ruin.model.entity.shared.StepType;
 import io.ruin.model.map.Position;
 import io.ruin.model.map.object.GameObject;
+import io.ruin.model.map.object.RegisterObject;
+import io.ruin.model.map.object.actions.ObjectAction;
 import io.ruin.model.stat.StatType;
 import lombok.AllArgsConstructor;
 
@@ -16,19 +18,48 @@ import lombok.AllArgsConstructor;
  */
 @AllArgsConstructor
 public enum LogBalance {
-
-    KARAMJA_LOG1(1,1, Position.of(2906, 3049), Position.of(2910, 3049)),
-    CAMELOT_LOG20(20, 1, Position.of(2598, 3477), Position.of(2603, 3477)),
-    BRIMHAVEN_LOG30(30, 1, Position.of(2682, 9506), Position.of(2687, 9506)),
-    ARDY_LOG33(33, 1, Position.of(2602, 3336), Position.of(2598, 3336)),
-    ISAFDAR_1_LOG45(45, 1, Position.of(2202, 3237), Position.of(2196, 3237)),
-    ISAFDAR_2_LOG45(45, 1, Position.of(2258, 3250), Position.of(2264, 3250)),
-    ISAFDAR_3_LOG45(45, 1, Position.of(2290, 3232), Position.of(2290, 3239)),
-    CAMELOT_LOG48(48, 1, Position.of(2722, 3592), Position.of(2722, 3596))
+    // TODO get coordinates
+    KARAMJA_LOG1(new RegisterObject[]{
+            new RegisterObject(23644, 0, 0) },
+            1,1, "cross",
+            Position.of(2906, 3049), Position.of(2910, 3049)),
+    CAMELOT_LOG20(new RegisterObject[]{
+            new RegisterObject(23274, 0, 0) },
+            20, 1, "walk-across",
+            Position.of(2598, 3477), Position.of(2603, 3477)),
+    BRIMHAVEN_LOG30(new RegisterObject[]{
+            new RegisterObject(20882, 0, 0),
+            new RegisterObject(20884, 0, 0) },
+            30, 1, "walk-across",
+            Position.of(2682, 9506), Position.of(2687, 9506)),
+    ARDY_LOG33(new RegisterObject[]{
+            new RegisterObject(16548, 0, 0),
+            new RegisterObject(16546, 0, 0) },
+            33, 1, "walk-across",
+            Position.of(2602, 3336), Position.of(2598, 3336)),
+    ISAFDAR_1_LOG45(new RegisterObject[]{
+            new RegisterObject(3931, 0, 0) },
+            45, 1, "cross",
+            Position.of(2202, 3237), Position.of(2196, 3237)),
+    ISAFDAR_2_LOG45(new RegisterObject[]{
+            new RegisterObject(3932, 0, 0), },
+            45, 1, "cross",
+            Position.of(2258, 3250), Position.of(2264, 3250)),
+    ISAFDAR_3_LOG45(new RegisterObject[]{
+            new RegisterObject(3933, 0, 0) },
+            45, 1, "cross",
+            Position.of(2290, 3232), Position.of(2290, 3239)),
+    CAMELOT_LOG48(new RegisterObject[]{
+            new RegisterObject(16542, 0, 0),
+            new RegisterObject(16540, 0, 0) },
+            48, 1, "walk-across",
+            Position.of(2722, 3592), Position.of(2722, 3596))
     ;
 
-    private int levelReq, xp;
-    private Position startPosition, endPosition;
+    private final RegisterObject[] objects;
+    private final int levelReq, xp;
+    private final String option;
+    private final Position startPosition, endPosition;
 
     public void traverse(Player player, GameObject obj){
         player.startEvent(e -> {
@@ -43,5 +74,13 @@ public enum LogBalance {
             player.getAppearance().removeCustomRenders();
             player.unlock();
         });
+    }
+
+    static {
+        for (LogBalance log : values()) {
+            for (RegisterObject object : log.objects) {
+                ObjectAction.register(object.getObjectId(), object.getPosition(), log.option, log::traverse);
+            }
+        }
     }
 }
