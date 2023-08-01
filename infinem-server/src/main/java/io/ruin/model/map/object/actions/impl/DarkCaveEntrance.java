7f993ed12ada79cc00cbb01bb7e9cfb58739f6bd
Thomas Presicci <thomaspresicci@gmail.com>
2023-08-01 14:31:22 -0500

    Added lumbridge swamp cave northern passage

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/DarkCaveEntrance.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/DarkCaveEntrance.java
@@ -7,8 +7,11 @@ import io.ruin.model.inter.dialogue.OptionsDialogue;
 import io.ruin.model.inter.utils.Option;
 import io.ruin.model.item.actions.impl.Lightables;
 import io.ruin.model.map.Position;
+import io.ruin.model.map.object.GameObject;
 import io.ruin.model.map.object.actions.ObjectAction;
 import lombok.AllArgsConstructor;
+
+import java.util.function.Consumer;
 
 /**
  * @author Mrbennjerry - https://github.com/Presicci
@@ -33,21 +36,59 @@ public enum DarkCaveEntrance {
         });
     }
 
+    static void use(Player player, GameObject object, DarkCaveEntrance entry) {
+        if (Lightables.hasLightSource(player)) {
+            entry.enter(player);
+        } else {
+            player.dialogue(
+                    new MessageDialogue("The cave is far too dark to see anything without a light source. Continue?"),
+                    new OptionsDialogue("Enter the cave?",
+                            new Option("Enter", () -> entry.enter(player)),
+                            new Option("Do not enter")
+                    )
+            );
+        }
+    }
+
+    static void use(Player player, GameObject object, Consumer<Player> runnable) {
+        if (Lightables.hasLightSource(player)) {
+            runnable.accept(player);
+        } else {
+            player.dialogue(
+                    new MessageDialogue("The cave is far too dark to see anything without a light source. Continue?"),
+                    new OptionsDialogue("Enter the cave?",
+                            new Option("Enter", () -> runnable.accept(player)),
+                            new Option("Do not enter")
+                    )
+            );
+        }
+    }
+
     static {
         for (DarkCaveEntrance entry : values()) {
-            ObjectAction.register(entry.objectId, entry.objectPos.getX(), entry.objectPos.getY(), entry.objectPos.getZ(), entry.option, ((player, obj) -> {
-                if (Lightables.hasLightSource(player)) {
-                    entry.enter(player);
-                } else {
-                    player.dialogue(
-                            new MessageDialogue("The cave is far too dark to see anything without a light source. Continue?"),
-                            new OptionsDialogue("Enter the cave?",
-                                    new Option("Enter", () -> entry.enter(player)),
-                                    new Option("Do not enter")
-                            )
-                    );
-                }
-            }));
+            ObjectAction.register(entry.objectId, entry.objectPos.getX(), entry.objectPos.getY(), entry.objectPos.getZ(), entry.option, (player, obj) -> use(player, obj, entry));
         }
+        // Lumbridge swamp cave northern passage
+        ObjectAction.register(6912, 3224, 9601, 0, "squeeze-through", ((player, obj) -> {
+            if (player.getAbsY() > obj.getPosition().getY()) {
+                use(player, obj, (p) -> {
+                    p.startEvent(e -> {
+                        p.lock(LockType.FULL_DELAY_DAMAGE);
+                        p.animate(749);
+                        e.delay(2);
+                        p.getMovement().teleport(3224, 9600);
+                        p.unlock();
+                    });
+                });
+            } else {
+                player.startEvent(e -> {
+                    player.lock(LockType.FULL_DELAY_DAMAGE);
+                    player.animate(749);
+                    e.delay(2);
+                    player.getMovement().teleport(3224, 9604);
+                    player.unlock();
+                });
+            }
+        }));
     }
 }
