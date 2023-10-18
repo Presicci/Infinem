eba8ae69a737e305301143dc54188f4c295ccd0d
Presicci <thomaspresicci@gmail.com>
2023-10-18 11:43:42 -0500

    Added enum for storing the different tiers of area tasks

---
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/areas/AreaTaskTier.java
@@ -0,0 +1,9 @@
+package io.ruin.model.content.tasksystem.tasks.areas;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 10/7/2023
+ */
+public enum AreaTaskTier {
+    EASY, MEDIUM, HARD, ELITE
+}
