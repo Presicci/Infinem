4a0db989575558deabe0474fa6a1f3ff979bc697
Thomas Presicci <thomaspresicci@gmail.com>
2023-10-26 12:55:00 -0600

    Changed client favicon

--- runelite/runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
+++ runelite/runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
@@ -109,7 +109,7 @@ public class ClientUI
 	private static final String CONFIG_CLIENT_BOUNDS = "clientBounds";
 	private static final String CONFIG_CLIENT_MAXIMIZED = "clientMaximized";
 	private static final String CONFIG_CLIENT_SIDEBAR_CLOSED = "clientSidebarClosed";
-	public static final BufferedImage ICON = ImageUtil.loadImageResource(ClientUI.class, "/openosrs.png");
+	public static final BufferedImage ICON = ImageUtil.loadImageResource(ClientUI.class, "/icon.png");
 
 	@Getter
 	private TrayIcon trayIcon;
