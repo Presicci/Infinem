069567db495c0024f85a1491267c6457ad6e096c
Thomas Presicci <thomaspresicci@gmail.com>
2023-10-31 22:26:18 -0600

    Added collectNumberOfItems() method

--- infinem-server/src/main/java/io/ruin/model/item/ItemContainer.java
+++ infinem-server/src/main/java/io/ruin/model/item/ItemContainer.java
@@ -1,5 +1,6 @@
 package io.ruin.model.item;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class ItemContainer extends ItemContainerG<Item> {
@@ -12,6 +13,28 @@ public class ItemContainer extends ItemContainerG<Item> {
     @Override
     protected Item[] newArray(int size) {
         return new Item[size];
+    }
+
+    public ArrayList<Item> collectNumberOfItems(int amount, int... ids) {
+        int count = 0;
+        ArrayList<Item> list = new ArrayList<>(ids.length);
+        for (Item item : items) {
+            if (item == null)
+                continue;
+            for (int id : ids) {
+                if (item.getId() == id) {
+                    if (count >= amount) return list;
+                    if (count + item.getAmount() >= amount) {
+                        list.add(new Item(item.getId(), amount - count));
+                        count = amount;
+                    } else {
+                        list.add(item);
+                        count += item.getAmount();
+                    }
+                }
+            }
+        }
+        return list.isEmpty() ? null : list;
     }
 
 }
