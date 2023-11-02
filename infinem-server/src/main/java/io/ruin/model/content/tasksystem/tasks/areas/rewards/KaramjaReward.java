c8b784b7b4ac9e918dd7198027aa508adef34e8e
Thomas Presicci <thomaspresicci@gmail.com>
2023-11-02 16:44:24 -0600

    Empty classes for the rest of the area rewards

---
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/areas/rewards/KaramjaReward.java
@@ -0,0 +1,8 @@
+package io.ruin.model.content.tasksystem.tasks.areas.rewards;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 10/31/2023
+ */
+public class KaramjaReward {
+}
---
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/areas/shops/MisthalinRewardShop.java
@@ -0,0 +1,10 @@
+package io.ruin.model.content.tasksystem.tasks.areas.shops;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 10/31/2023
+ */
+public enum MisthalinRewardShop {
+    ANCIENT_MACE,
+    BONE_CROSSBOW
+}
