e196a38d6d51b9c06b466bdd5e0123df7920d0a9
Thomas Presicci <thomaspresicci@gmail.com>
2023-11-04 21:57:10 -0600

    Added 'Ride a Minecart out of Keldagrim' task

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Keldagrim.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/Keldagrim.java
@@ -10,16 +10,22 @@ public class Keldagrim {
         // Ice mountain -> Keldagrim
         ObjectAction.register(7029, 2995, 9836, 0, "ride", (player, obj) -> Traveling.fadeTravel(player, new Position(2911, 10176, 0)));
         // Keldagrim -> Ice mountain
-        ObjectAction.register(7028, 2911, 10175, 0, "ride", (player, obj) -> Traveling.fadeTravel(player, new Position(2995, 9837, 0)));
+        ObjectAction.register(7028, 2911, 10175, 0, "ride", (player, obj) -> Traveling.fadeTravel(player, new Position(2995, 9837, 0), () -> {
+            player.getTaskManager().doLookupByUUID(923);    // Ride a Minecart out of Keldagrim
+        }));
         // GE -> Keldagrim
         ObjectAction.register(16168, "inspect", (player, obj) -> player.dialogue(new MessageDialogue("The trapdoor leads to Keldagrim.")));
         ObjectAction.register(16168, "travel", (player, obj) -> Traveling.fadeTravel(player, new Position(2909, 10172, 0)));
         // Keldagrim -> GE
-        ObjectAction.register(7029, 2909, 10171, 0, "ride", (player, obj) -> Traveling.fadeTravel(player, new Position(3141, 3504, 0)));
+        ObjectAction.register(7029, 2909, 10171, 0, "ride", (player, obj) -> Traveling.fadeTravel(player, new Position(3141, 3504, 0), () -> {
+            player.getTaskManager().doLookupByUUID(923);    // Ride a Minecart out of Keldagrim
+        }));
         // White wolf mountain -> Keldagrim
         ObjectAction.register(7030, 2875, 9868, 0, "ride", (player, obj) -> Traveling.fadeTravel(player, new Position(2911, 10170, 0)));
         // Keldagrim -> White wolf mountain
-        ObjectAction.register(7030, 2911, 10169, 0, "ride", (player, obj) -> Traveling.fadeTravel(player, new Position(2874, 9868, 0)));
+        ObjectAction.register(7030, 2911, 10169, 0, "ride", (player, obj) -> Traveling.fadeTravel(player, new Position(2874, 9868, 0), () -> {
+            player.getTaskManager().doLookupByUUID(923);    // Ride a Minecart out of Keldagrim
+        }));
 
         // Keldagrim entrance
         ObjectAction.register(5014, 2771, 10161,0, "enter", (player, obj) -> Traveling.fadeTravel(player, new Position(2730, 3713, 0)));
