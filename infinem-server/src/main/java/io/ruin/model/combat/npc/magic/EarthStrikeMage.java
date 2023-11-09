d77ce9687eccaa6d25f151f2532362e62e57506f
Thomas Presicci <thomaspresicci@gmail.com>
2023-11-09 16:38:09 -0700

    Renamed ZombieMage to EarthStrikeMage

--- infinem-server/src/main/java/io/ruin/model/combat/npc/magic/EarthStrikeMage.java
+++ infinem-server/src/main/java/io/ruin/model/combat/npc/magic/EarthStrikeMage.java
@@ -4,7 +4,7 @@ package io.ruin.model.combat.npc.magic;
  * @author Mrbennjerry - https://github.com/Presicci
  * Created on 11/9/2023
  */
-public class ZombieMage extends WizardCombat {
+public class EarthStrikeMage extends WizardCombat {
     @Override
     public void init() {
         standardCast = NPCCombatSpells.EARTH_STRIKE;
