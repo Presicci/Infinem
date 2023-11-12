74d3ee978f59be8ba53541754d198089fbf35a52
Thomas Presicci <thomaspresicci@gmail.com>
2023-11-12 15:07:12 -0700

    Added haunted mine entrance and exit

---
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/dungeons/HauntedMine.java
@@ -0,0 +1,14 @@
+package io.ruin.model.map.object.actions.impl.dungeons;
+
+import io.ruin.model.map.object.actions.ObjectAction;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 11/5/2023
+ */
+public class HauntedMine {
+
+    static {
+        // Entrance
+    }
+}
