d2f0364adb2627817fc7038392842b0ad8fbf9dd
Thomas Presicci <thomaspresicci@gmail.com>
2023-12-07 16:23:42 -0700

    Added kitchen cabinet in the gnome kitchen

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/DialogueObject.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/DialogueObject.java
@@ -1,9 +1,10 @@
 package io.ruin.model.map.object.actions.impl;
 
-import io.ruin.model.inter.dialogue.Dialogue;
-import io.ruin.model.inter.dialogue.MessageDialogue;
-import io.ruin.model.inter.dialogue.NPCDialogue;
-import io.ruin.model.inter.dialogue.PlayerDialogue;
+import io.ruin.model.entity.player.Player;
+import io.ruin.model.inter.dialogue.*;
+import io.ruin.model.inter.utils.Option;
+import io.ruin.model.item.Item;
+import io.ruin.model.item.Items;
 import io.ruin.model.map.Position;
 import io.ruin.model.map.object.actions.ObjectAction;
 
@@ -12,6 +13,13 @@ import io.ruin.model.map.object.actions.ObjectAction;
  * Created on 7/1/2023
  */
 public enum DialogueObject {
+    KITCHEN_CABINET(17132, new Position(2449, 3512, 1),
+            new OptionsDialogue("What would you like to take?",
+                    new Option("Knife", (player) -> takeItem(player, new Item(Items.KNIFE), "cabinet")),
+                    new Option("Batta tin", (player) -> takeItem(player, new Item(Items.BATTA_TIN), "cabinet")),
+                    new Option("Crunchy tray", (player) -> takeItem(player, new Item(Items.CRUNCHY_TRAY), "cabinet")),
+                    new Option("Gnomebowl mould", (player) -> takeItem(player, new Item(Items.GNOMEBOWL_MOULD), "cabinet"))
+            )),
     FRACTIONALISING_STILL(4026, new Position(2927, 3212),
             new PlayerDialogue("That is far too complicated for me to operate.")),
     TENT_DOOR(2700, new Position(3169, 3046),
@@ -41,5 +49,15 @@ public enum DialogueObject {
 
     DialogueObject(int objectId, int option, Dialogue... dialogues) {
         ObjectAction.register(objectId, option, ((p, o) -> p.dialogue(dialogues)));
+    }
+
+    private static void takeItem(Player player, Item item, String containerName) {
+        if (!player.getInventory().hasRoomFor(item)) {
+            player.dialogue(new MessageDialogue("You do not have enough inventory space to take that."));
+            return;
+        }
+        player.animate(3572);
+        player.getInventory().add(item);
+        player.sendMessage("You take a " + item.getDef().name.toLowerCase() + " from the " + containerName + ".");
     }
 }
