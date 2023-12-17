5e01b0144568bf97c2a05cdf67c7a812ace8ce72
Thomas Presicci <thomaspresicci@gmail.com>
2023-12-17 15:06:22 -0700

    Added task descriptions to EquipTask.java

--- infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/EquipTask.java
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/EquipTask.java
@@ -10,15 +10,14 @@ import io.ruin.model.item.containers.equipment.EquipAction;
  */
 public enum EquipTask {
 
-    FIRE_CAPE(418, Items.FIRE_CAPE),
-    INFERNAL_CAPE(429, Items.INFERNAL_CAPE, MaxCapeVariants.MaxCapes.INFERNAL.newCapeId),
-    SPINY_HELMET(29, Items.SPINY_HELMET),
-    DRAGON_DEFENDER(462, Items.DRAGON_DEFENDER),
-    FANCY_BOOTS(341, Items.FANCY_BOOTS),
-    FIGHTING_BOOTS(341, Items.FIGHTING_BOOTS),
-    BUG_LANTERN(125, Items.LIT_BUG_LANTERN),
-    MAPLE_SHORTBOW(62, Items.MAPLE_SHORTBOW),
-    ELEMENTAL_STAFF(63, Items.STAFF_OF_AIR, Items.STAFF_OF_EARTH, Items.STAFF_OF_FIRE, Items.STAFF_OF_WATER)
+    FIRE_CAPE(418, Items.FIRE_CAPE),    // Equip a Fire Cape
+    INFERNAL_CAPE(429, Items.INFERNAL_CAPE, MaxCapeVariants.MaxCapes.INFERNAL.newCapeId),   // Equip an Infernal Cape
+    SPINY_HELMET(29, Items.SPINY_HELMET),   // Equip a Spiny Helmet
+    DRAGON_DEFENDER(462, Items.DRAGON_DEFENDER),    // Equip a Dragon Defender
+    FANCY_BOOTS(341, Items.FANCY_BOOTS, Items.FIGHTING_BOOTS),    // Equip Some Fancy Boots or Fighting Boots
+    BUG_LANTERN(125, Items.LIT_BUG_LANTERN),    // Equip a Harpie Bug Lantern
+    MAPLE_SHORTBOW(62, Items.MAPLE_SHORTBOW),   // Equip a Maple Shortbow
+    ELEMENTAL_STAFF(63, Items.STAFF_OF_AIR, Items.STAFF_OF_EARTH, Items.STAFF_OF_FIRE, Items.STAFF_OF_WATER),     // Equip an Elemental Staff
     ;
 
     private final int uuid;
