73e91234f4a8f643f4e3261e68634b34731789ad
Thomas Presicci <thomaspresicci@gmail.com>
2023-12-19 12:30:46 -0700

    Added Ape Atoll gates

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/TwoWayTraversalObject.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/TwoWayTraversalObject.java
@@ -10,6 +10,8 @@ import io.ruin.model.map.object.actions.ObjectAction;
  * Created on 11/5/2023
  */
 public enum TwoWayTraversalObject {
+    APE_ATOLL_GATE_1(4787, new Position(2719, 2766), 2, -1, false),
+    APE_ATOLL_GATE_2(4788, new Position(2721, 2766), 2, -1, false),
     SOURHOG_CAVE(40331, new Position(3156, 9704), 2, 839, false),
     LUMBRIDGE_SWAMP_CAVE_TO_DORGESH_KAAN_CAVES(6912, new Position(3224, 9601), 4, 2, 2796, false);
 
@@ -20,9 +22,13 @@ public enum TwoWayTraversalObject {
             int yDiff = direction == Direction.SOUTH ? -distance : direction == Direction.NORTH ? distance : 0;
             player.startEvent(e -> {
                 player.lock(LockType.FULL_DELAY_DAMAGE);
-                player.animate(animation);
-                player.getMovement().force(xDiff, yDiff, 0, 0, 5, 80, direction);
-                e.delay(distance);
+                if (animation != -1) {
+                    player.animate(animation);
+                    player.getMovement().force(xDiff, yDiff, 0, 0, 5, 80, direction);
+                    e.delay(distance);
+                } else {
+                    player.getMovement().teleport(player.getPosition().relative(xDiff, yDiff));
+                }
                 player.unlock();
             });
         }));
