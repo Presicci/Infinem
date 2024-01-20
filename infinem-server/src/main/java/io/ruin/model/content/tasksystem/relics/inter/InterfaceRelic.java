0d2b699a1f14a91085e13cddfe165f31b19427c1
Thomas Presicci <thomaspresicci@gmail.com>
2024-01-20 04:45:15 -0700

    enum for storing relic interface data

---
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/relics/inter/InterfaceRelic.java
@@ -0,0 +1,23 @@
+package io.ruin.model.content.tasksystem.relics.inter;
+
+import io.ruin.model.content.tasksystem.relics.Relic;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 1/20/2024
+ */
+@Getter
+@AllArgsConstructor
+public enum InterfaceRelic {
+    EYE_OF_THE_ARTISAN(730, Relic.EYE_OF_THE_ARTISAN),
+    GIFT_OF_THE_GATHERER(731, Relic.GIFT_OF_THE_GATHERER),
+    WAY_OF_THE_WARRIOR(732, Relic.WAY_OF_THE_WARRIOR),
+    ENDLESS_HARVEST(1722, Relic.ENDLESS_HARVEST),
+    PRODUCTION_MASTER(1723, Relic.PRODUCTION_MASTER),
+    TRICKSTER(4730, Relic.TRICKSTER);
+
+    private final int struct;
+    private final Relic relic;
+}
