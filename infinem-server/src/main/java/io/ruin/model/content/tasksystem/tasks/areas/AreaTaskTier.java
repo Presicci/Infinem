113f79314305ea956efd6275a3d461f98167e9ca
Thomas Presicci <thomaspresicci@gmail.com>
2024-01-23 19:33:12 -0700

    Added preliminary area point thresholds

--- infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/areas/AreaTaskTier.java
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/areas/AreaTaskTier.java
@@ -1,9 +1,18 @@
 package io.ruin.model.content.tasksystem.tasks.areas;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @author Mrbennjerry - https://github.com/Presicci
  * Created on 10/7/2023
  */
+@AllArgsConstructor
 public enum AreaTaskTier {
-    EASY, MEDIUM, HARD, ELITE
+    EASY(250),
+    MEDIUM(600),
+    HARD(1000),
+    ELITE(1500);
+
+    @Getter private final int pointThreshold;
 }
