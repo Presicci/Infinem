c2044ecc1ad88c98404a632e5c9e3ab62e415a11
Thomas Presicci <thomaspresicci@gmail.com>
2024-01-25 02:03:37 -0700

    Slayer tower chain shortcuts now require agility levels Dont cause damage anymore

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/SlayerTower.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/SlayerTower.java
@@ -6,6 +6,7 @@ import io.ruin.model.inter.dialogue.OptionsDialogue;
 import io.ruin.model.inter.utils.Option;
 import io.ruin.model.map.object.actions.ObjectAction;
 import io.ruin.model.map.object.actions.impl.Ladder;
+import io.ruin.model.skills.slayer.Slayer;
 import io.ruin.model.stat.StatType;
 
 public class SlayerTower {
@@ -30,9 +31,10 @@ public class SlayerTower {
          */
         ObjectAction.register(16537, 1, (player, obj) -> {
             if (obj.x == 3422 && obj.y == 3550) {
-                if (player.getEquipment().hasId(4168)) {
+                if (!player.getStats().check(StatType.Agility, 61, "climb the chain")) return;
+                if (player.getEquipment().hasId(4168) || Slayer.hasSlayerHelmEquipped(player)) {
                     Ladder.climb(player, player.getAbsX(), player.getAbsY(), player.getHeight() + 1, true, true, false);
-                    player.getStats().addXp(StatType.Agility, 4.0, false);
+                    player.getStats().addXp(StatType.Agility, 3.0, true);
                 } else {
                     player.dialogue(
                             new MessageDialogue("A foul stench seems to be seeping down from the floor above... it could be dangerous up there without a nosepeg."),
@@ -44,22 +46,21 @@ public class SlayerTower {
                                     new Option("No", player::closeDialogue))
                     );
                 }
-            } else if(obj.x == 3447 && obj.y == 3576 && player.getStats().get(StatType.Agility).currentLevel >= 71) {
+            } else if(obj.x == 3447 && obj.y == 3576) {
+                if (!player.getStats().check(StatType.Agility, 71, "climb the chain")) return;
                 Ladder.climb(player, player.getAbsX(), player.getAbsY(), player.getHeight() + 1, true, true, false);
-                player.getStats().addXp(StatType.Agility, 4.0, false);
-            } else {
-                player.sendFilteredMessage("You need an Agility level of 71 to negotiate this obstacle.");
+                player.getStats().addXp(StatType.Agility, 3.0, true);
             }
         });
         ObjectAction.register(16538, 1, (player, obj) -> {
             if(obj.x == 3422 && obj.y == 3550) {
+                if (!player.getStats().check(StatType.Agility, 61, "climb the chain")) return;
                 player.hit(new Hit().randDamage(5));
                 Ladder.climb(player, player.getAbsX(), player.getAbsY(), player.getHeight() - 1, false, true, false);
-            } else if(obj.x == 3447 && obj.y == 3576 && player.getStats().get(StatType.Agility).currentLevel >= 71) {
+            } else if(obj.x == 3447 && obj.y == 3576) {
+                if (!player.getStats().check(StatType.Agility, 71, "climb the chain")) return;
                 player.hit(new Hit().randDamage(5));
                 Ladder.climb(player, player.getAbsX(), player.getAbsY(), player.getHeight() - 1, false, true, false);
-            } else {
-                player.sendFilteredMessage("You need an agility level of 71 to negotiate this obstacle.");
             }
         });
     }
