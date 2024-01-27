70ca866a3a055e45810012df2b944654f2ab012b
Thomas Presicci <thomaspresicci@gmail.com>
2024-01-26 17:22:20 -0700

    Fixed al kharid gates and removed pay-toll option

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/AlkharidGate.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/AlkharidGate.java
@@ -1,5 +1,6 @@
 package io.ruin.model.map.object.actions.impl;
 
+import io.ruin.model.inter.utils.Config;
 import io.ruin.model.map.Position;
 import io.ruin.model.map.object.actions.ObjectAction;
 
@@ -8,21 +9,17 @@ import io.ruin.model.map.object.actions.ObjectAction;
  * Created on 1/18/2022
  */
 public class AlkharidGate {
+
+    // Makes the gate object only have open as option 1
+    private static final Config PRICE_ALI_RESCUE = Config.varp(273, false).defaultValue(100);
+
     static {
-        ObjectAction.register(2883, 3268, 3228, 0, "open", ((player, obj) -> {
-            DoubleDoor.passThroughLateral(player, obj, 2883, 2882, new Position(3268, 3228), new Position(3268, 3227));
+        ObjectAction.register(44599, 3268, 3228, 0, 1, ((player, obj) -> {
+            DoubleDoor.passThroughLateral(player, obj, 44599, 44598, new Position(3268, 3228), new Position(3268, 3227));
             player.getTaskManager().doLookupByUUID(637, 1);
         }));
-        ObjectAction.register(2882, 3268, 3227, 0, "open", ((player, obj) -> {
-            DoubleDoor.passThroughLateral(player, obj, 2883, 2882, new Position(3268, 3228), new Position(3268, 3227));
-            player.getTaskManager().doLookupByUUID(637, 1);
-        }));
-        ObjectAction.register(2883, 3268, 3228, 0, "pay-toll(10gp)", ((player, obj) -> {
-            DoubleDoor.passThroughLateral(player, obj, 2883, 2882, new Position(3268, 3228), new Position(3268, 3227));
-            player.getTaskManager().doLookupByUUID(637, 1);
-        }));
-        ObjectAction.register(2882, 3268, 3227, 0, "pay-toll(10gp)", ((player, obj) -> {
-            DoubleDoor.passThroughLateral(player, obj, 2883, 2882, new Position(3268, 3228), new Position(3268, 3227));
+        ObjectAction.register(44598, 3268, 3227, 0, 1, ((player, obj) -> {
+            DoubleDoor.passThroughLateral(player, obj, 44599, 44598, new Position(3268, 3228), new Position(3268, 3227));
             player.getTaskManager().doLookupByUUID(637, 1);
         }));
     }
