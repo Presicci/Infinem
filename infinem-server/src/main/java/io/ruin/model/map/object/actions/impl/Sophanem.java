5a5ddff4e401914a2f6bd8c4609c0dfb01c2d832
Thomas Presicci <thomaspresicci@gmail.com>
2024-01-27 15:29:15 -0700

    Updated sophanem front gate

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/Sophanem.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/Sophanem.java
@@ -11,9 +11,9 @@ public class Sophanem {
 
     static {
         // Front gate
-        ObjectAction.register(20391, 3284, 2809, 0, "open",
-                ((player, obj) -> DoubleDoor.passThroughLongitudinal(player, obj, 20391, 20391, new Position(3283, 2809), new Position(3284, 2809))));
-        ObjectAction.register(20391, 3283, 2809, 0, "open",
-                ((player, obj) -> DoubleDoor.passThroughLongitudinal(player, obj, 20391, 20391, new Position(3283, 2809), new Position(3284, 2809))));
+        ObjectAction.register(44038, 3284, 2809, 0, "open",
+                ((player, obj) -> DoubleDoor.passThroughLongitudinal(player, obj, 44036, 44038, new Position(3283, 2809), new Position(3284, 2809))));
+        ObjectAction.register(44036, 3283, 2809, 0, "open",
+                ((player, obj) -> DoubleDoor.passThroughLongitudinal(player, obj, 44036, 44038, new Position(3283, 2809), new Position(3284, 2809))));
     }
 }
