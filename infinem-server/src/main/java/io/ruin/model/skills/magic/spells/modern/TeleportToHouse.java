1977c3df6e94a6a862636c9d37b1bdd667de738d
Thomas Presicci <thomaspresicci@gmail.com>
2024-01-29 18:27:42 -0700

    Fixed teleport to house right click options and made them respect house teleport options

--- infinem-server/src/main/java/io/ruin/model/skills/magic/spells/modern/TeleportToHouse.java
+++ infinem-server/src/main/java/io/ruin/model/skills/magic/spells/modern/TeleportToHouse.java
@@ -16,22 +16,24 @@ public class TeleportToHouse extends Spell {
         registerClick(LVL_REQ, XP, true, RUNES, TeleportToHouse::teleport);
     }
 
-    private static boolean teleport(Player player, Integer i) {
+    public static boolean teleport(Player player, Integer i) {
         if (player.house == null) {
             player.sendMessage("You don't have a house to teleport to.");
             return false;
         }
-        if (player.isInOwnHouse()) {
+        boolean inside = i == 0 || i == 2;
+        if (player.isInOwnHouse() && inside) {
             player.sendMessage("You're already in your house!");
             return false;
         }
+        System.out.println(i);
         return player.getMovement().startTeleport(e -> {
             player.animate(714);
             player.graphics(111, 92, 0);
             player.publicSound(200);
             e.delay(2);
             player.getTaskManager().doLookupByUUID(147, 1); // Teleport Using Law Runes
-            if (Config.TELEPORT_INSIDE.get(player) == 0) {
+            if (inside)  {
                 player.house.buildAndEnter(player, false);
                 while (player.isLocked())
                     e.delay(1);
