955394a6296d77dde523f83eab3475275f9458f2
Thomas Presicci <thomaspresicci@gmail.com>
2024-02-05 01:10:16 -0700

    Splatters now do between 5-24 damage and can hit other npcs

--- infinem-server/src/main/java/io/ruin/model/activities/combat/pestcontrol/npcs/Splatter.java
+++ infinem-server/src/main/java/io/ruin/model/activities/combat/pestcontrol/npcs/Splatter.java
@@ -1,11 +1,14 @@
 package io.ruin.model.activities.combat.pestcontrol.npcs;
 
+import io.ruin.api.utils.Random;
 import io.ruin.model.combat.Hit;
 import io.ruin.model.entity.Entity;
 import io.ruin.model.entity.npc.NPCCombat;
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.entity.shared.listeners.HitListener;
 import io.ruin.utility.Misc;
+
+import java.util.Arrays;
 
 /**
  * The combat script for the Splatter pest.
@@ -45,7 +48,13 @@ public class Splatter extends NPCCombat {
 		npc.graphics(650);
 		npc.localPlayers().forEach(p -> {
 			if (Misc.getEffectiveDistance(npc, p) <= 1) {
-				p.hit(new Hit(npc).fixedDamage(20));
+				p.hit(new Hit(npc).fixedDamage(Random.get(5, 24)));
+			}
+		});
+		npc.localNpcs().forEach(n -> {
+			if (!Arrays.asList(2950, 2951, 2952, 2953).contains(n.getId()) // Can't hit the void knight
+					&& Misc.getEffectiveDistance(npc, n) <= 1) {
+				n.hit(new Hit(npc).fixedDamage(Random.get(5, 24)));
 			}
 		});
 	}
