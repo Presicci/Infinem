f2a2dbf0fc71c9d62b57f5e8cd4491061c71d364
Thomas Presicci <thomaspresicci@gmail.com>
2024-02-09 01:27:58 -0700

    Imcando hammer now now be used for opening infernal eels

--- infinem-server/src/main/java/io/ruin/model/item/actions/impl/InfernalEel.java
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/InfernalEel.java
@@ -1,5 +1,6 @@
 package io.ruin.model.item.actions.impl;
 
+import io.ruin.model.entity.player.Player;
 import io.ruin.model.item.Item;
 import io.ruin.model.item.actions.ItemItemAction;
 import io.ruin.model.item.loot.LootItem;
@@ -16,18 +17,21 @@ public class InfernalEel {
             new LootItem(11994, 1, 5, 1)    // lava scale shard
     );
 
-    static {
-        ItemItemAction.register(INFERNAL_EEL, Tool.HAMMER, (player, primary, secondary) -> {
-            Item loot = chanceTable.rollItem();
-            player.startEvent(event -> {
-                player.lock();
-                player.animate(7553);
-                player.getInventory().remove(primary.getId(), 1);
-                player.getInventory().add(loot);
-                player.sendFilteredMessage("You break open the infernal eel and extract some " + loot.getDef().name + ".");
-                event.delay(1);
-                player.unlock();
-            });
+    private static void open(Player player, Item eel) {
+        Item loot = chanceTable.rollItem();
+        player.startEvent(event -> {
+            player.lock();
+            player.animate(7553);
+            player.getInventory().remove(eel.getId(), 1);
+            player.getInventory().add(loot);
+            player.sendFilteredMessage("You break open the infernal eel and extract some " + loot.getDef().name + ".");
+            event.delay(1);
+            player.unlock();
         });
+    }
+
+    static {
+        ItemItemAction.register(INFERNAL_EEL, Tool.HAMMER, (player, primary, secondary) -> open(player, primary));
+        ItemItemAction.register(INFERNAL_EEL, 25644, (player, primary, secondary) -> open(player, primary));
     }
 }
