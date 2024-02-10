3459204fa19acd2757b258390c82c1eb8198ea10
Thomas Presicci <thomaspresicci@gmail.com>
2024-02-10 14:12:46 -0700

    Removed bmCost from InstanceType

--- infinem-server/src/main/java/io/ruin/model/activities/combat/pvminstance/InstanceType.java
+++ infinem-server/src/main/java/io/ruin/model/activities/combat/pvminstance/InstanceType.java
@@ -14,35 +14,38 @@ import java.util.*;
 
 
 public enum InstanceType {
-    GIANT_MOLE("Giant Mole", new Bounds(1728, 5120, 1791, 5247, -1), 300_000, 2000, 60, new Position(1752, 5236, 0), new Position(2985, 3316, 0)),
-    DAGANNOTH_KINGS("Dagannoth Kings", 11589, 400_000, 2_000, 60, new Position(2899, 4449, 0), new Position(1912, 4367, 0)),
-    KRAKEN("Kraken", 9116, 400_000, 1_500, 60, new Position(2280, 10022, 0), new Position(2280, 10016, 0)),
-    KBD("King Black Dragon", 9033, 500_000, 2_500, 60, new Position(2271, 4680, 0),  new Position(3067, 10253, 0)),
-    THERMONUCLEAR_SMOKE_DEVIL("Thermonuclear Smoke Devil", new Bounds(2344, 9432, 2376, 9456, 0), 400_000, 1_500, 60, new Position(2376, 9452, 0), new Position(2379, 9452, 0)),
-    CERBERUS("Cerberus", 5140, 600_000, 3000, 60, new Position(1304, 1290, 0), new Position(1310, 1274, 0)),
-    CORP("Corporeal Beast", 11844, 1_000_000, 5_000, 60, new Position(2974, 4384, 2), new Position(2970, 4384, 2)),
-    KALPHITE_QUEEN("Kalphite Queen", 13972, 500_000, 2_500, 60, new Position(3507, 9494, 0), new Position(3509, 9496, 2)),
-    SARACHNIS("Sarachnis", 7322, 50_000, 0, 60, new Position(1842, 9911, 0), new Position(1842, 9912, 0)),
+    GIANT_MOLE("Giant Mole", new Bounds(1728, 5120, 1791, 5247, -1), 300_000, 60, new Position(1752, 5236, 0), new Position(2985, 3316, 0)),
+    DAGANNOTH_KINGS("Dagannoth Kings", 11589, 400_000, 60, new Position(2899, 4449, 0), new Position(1912, 4367, 0)),
+    KRAKEN("Kraken", 9116, 400_000, 60, new Position(2280, 10022, 0), new Position(2280, 10016, 0)),
+    KBD("King Black Dragon", 9033, 500_000, 60, new Position(2271, 4680, 0),  new Position(3067, 10253, 0)),
+    THERMONUCLEAR_SMOKE_DEVIL("Thermonuclear Smoke Devil", new Bounds(2344, 9432, 2376, 9456, 0), 400_000, 60, new Position(2376, 9452, 0), new Position(2379, 9452, 0)),
+    CERBERUS("Cerberus", 5140, 600_000, 60, new Position(1304, 1290, 0), new Position(1310, 1274, 0)),
+    CORP("Corporeal Beast", 11844, 1_000_000, 60, new Position(2974, 4384, 2), new Position(2970, 4384, 2)),
+    KALPHITE_QUEEN("Kalphite Queen", 13972, 500_000, 60, new Position(3507, 9494, 0), new Position(3509, 9496, 2)),
+    SARACHNIS("Sarachnis", 7322, 50_000, 60, new Position(1842, 9911, 0), new Position(1842, 9912, 0)),
 
-    OBOR("Obor", new Bounds(3072, 9792, 3107, 9817, 0), 0, 0, 60, new Position(3091, 9815, 0), new Position(3095, 9832, 0)),
-    BRYOPHYTA("Bryophyta", new Bounds(3200, 9920, 3235, 9951, -1), 0, 0, 60, new Position(3214, 9937, 0), new Position(3174, 9900, 0)),
+    OBOR("Obor", new Bounds(3072, 9792, 3107, 9817, 0), 0, 60, new Position(3091, 9815, 0), new Position(3095, 9832, 0)),
+    BRYOPHYTA("Bryophyta", new Bounds(3200, 9920, 3235, 9951, -1), 0, 60, new Position(3214, 9937, 0), new Position(3174, 9900, 0)),
 
-    HESPORI("Hespori", new Bounds(1232, 10072, 1264, 10104, -1), 0, 0, 60, new Position(1243, 10081), new Position(1230, 3729)),
+    HESPORI("Hespori", new Bounds(1232, 10072, 1264, 10104, -1), 0, 60, new Position(1243, 10081), new Position(1230, 3729)),
 
-    BANDOS_GWD("General Graardor", new Bounds(2856, 5344, 2877, 5374, 2), 500_000, 3_000, 60, Config.GWD_BANDOS_KC, "Bandos", new Position(2864, 5354, 2), new Position(2862, 5354, 2)),
-    ZAMORAK_GWD("K'ril Tsutsaroth", new Bounds(2912, 5312, 2944, 5336, 2), 500_000, 3_000, 60, Config.GWD_ZAMORAK_KC, "Zamorak", new Position(2925, 5331, 2), new Position(2925, 5333, 2)),
-    ARMADYL_GWD("Kree'arra", new Bounds(2816, 5288, 2840, 5304, 2), 500_000, 3_000, 60, Config.GWD_ARMADYL_KC, "Armadyl", new Position(2839, 5296, 2), new Position(2839, 5294, 2)),
-    SARADOMIN_GWD("Commander Zilyana", new Bounds(2880, 5248, 2911, 5278, -1), 500_000, 3_000, 60, Config.GWD_SARADOMIN_KC, "Saradomin", new Position(2907, 5265, 0), new Position(2909, 5265, 0));
+    BANDOS_GWD("General Graardor", new Bounds(2856, 5344, 2877, 5374, 2), 500_000, 60, Config.GWD_BANDOS_KC, "Bandos", new Position(2864, 5354, 2), new Position(2862, 5354, 2)),
+    ZAMORAK_GWD("K'ril Tsutsaroth", new Bounds(2912, 5312, 2944, 5336, 2), 500_000, 60, Config.GWD_ZAMORAK_KC, "Zamorak", new Position(2925, 5331, 2), new Position(2925, 5333, 2)),
+    ARMADYL_GWD("Kree'arra", new Bounds(2816, 5288, 2840, 5304, 2), 500_000, 60, Config.GWD_ARMADYL_KC, "Armadyl", new Position(2839, 5296, 2), new Position(2839, 5294, 2)),
+    SARADOMIN_GWD("Commander Zilyana", new Bounds(2880, 5248, 2911, 5278, -1), 500_000, 60, Config.GWD_SARADOMIN_KC, "Saradomin", new Position(2907, 5265, 0), new Position(2909, 5265, 0));
 
-    InstanceType(String name, Bounds bounds, int coinCost, int bmCost, int duration, Position entryPosition, Position exitPosition) {
-        this(name, bounds, coinCost, bmCost, duration, null, "", entryPosition, exitPosition);
+    InstanceType(String name, Bounds bounds, int coinCost, int duration, Position entryPosition, Position exitPosition) {
+        this(name, bounds, coinCost, duration, null, "", entryPosition, exitPosition);
     }
 
-    InstanceType(String name, Bounds bounds, int coinCost, int bmCost, int duration, Config godwarsConfig, String godwarsGodName, Position entryPosition, Position exitPosition) {
+    InstanceType(String name, int regionId, int coinCost, int duration, Position entryPosition, Position exitPosition) {
+        this(name, Bounds.fromRegion(regionId), coinCost, duration, entryPosition, exitPosition);
+    }
+
+    InstanceType(String name, Bounds bounds, int coinCost, int duration, Config godwarsConfig, String godwarsGodName, Position entryPosition, Position exitPosition) {
         this.name = name;
         this.bounds = bounds;
         this.coinCost = coinCost;
-        this.bmCost = bmCost;
         this.duration = duration * 100;
         this.entryPosition = entryPosition;
         this.exitPosition = exitPosition;
@@ -70,10 +73,6 @@ public enum InstanceType {
         });
     }
 
-    InstanceType(String name, int regionId, int coinCost, int bmCost, int duration, Position entryPosition, Position exitPosition) {
-        this(name, Bounds.fromRegion(regionId), coinCost, bmCost, duration, entryPosition, exitPosition);
-    }
-
     public void enterPublic(Player player) {
         if (godwarsConfig != null && !GodwarsBossEntrance.enter(player, godwarsConfig, godwarsGodName)) {
             return;
@@ -95,11 +94,6 @@ public enum InstanceType {
      * Entry position
      */
     private Position entryPosition;
-
-    /**
-     * How much blood money it costs to create this instance (only used if world is set to PvP)
-     */
-    private int bmCost;
 
     /**
      * How many coins it costs to create this instance (only used if world is set to Eco)
