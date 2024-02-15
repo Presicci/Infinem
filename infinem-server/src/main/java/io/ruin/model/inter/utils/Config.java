91a6cec89a20091c43437ea57e5ee13aa3ab294c
Thomas Presicci <thomaspresicci@gmail.com>
2024-02-14 18:31:01 -0700

    Camdozaal entrance is now open

--- infinem-server/src/main/java/io/ruin/model/inter/utils/Config.java
+++ infinem-server/src/main/java/io/ruin/model/inter/utils/Config.java
@@ -1107,6 +1107,7 @@ public class Config {
     public static final Config KARAMJA_GLOVES_GEM_MINE = varpbit(3610, true);
     private static final Config EDGEVILLE_PORTAL = varpbit(4153, false).defaultValue(1);    // Hides a portal in the upstairs of a house in edge
     private static final Config VARROCK_GARDEN_STATUE = varpbit(963, false).defaultValue(2);
+    private static final Config CAMDOZAAL_ENTRANCE = varpbit(12063, false).defaultValue(50);    // Opens the entrance to the city
 
     /**
      * Elemental workshop
