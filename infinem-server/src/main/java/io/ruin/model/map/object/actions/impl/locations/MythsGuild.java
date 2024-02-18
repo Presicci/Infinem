e027a0c58b0d47a87c5fd51c0324c393650a0f35
Thomas Presicci <thomaspresicci@gmail.com>
2024-02-18 13:11:32 -0700

    Added myths' guild entrance

---
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/MythsGuild.java
@@ -0,0 +1,44 @@
+package io.ruin.model.map.object.actions.impl.locations;
+
+import io.ruin.model.entity.player.Player;
+import io.ruin.model.entity.shared.StepType;
+import io.ruin.model.inter.dialogue.MessageDialogue;
+import io.ruin.model.map.object.GameObject;
+import io.ruin.model.map.object.actions.ObjectAction;
+import io.ruin.model.stat.StatList;
+import io.ruin.model.stat.StatType;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 2/18/2024
+ */
+public class MythsGuild {
+
+    private static void guildGate(Player player, GameObject obj) {
+        StatList stats = player.getStats();
+        if (player.getPosition().getY() == 2862
+                && (stats.get(StatType.Magic).currentLevel < 75) || stats.get(StatType.Smithing).currentLevel < 70
+            || stats.get(StatType.Mining).currentLevel < 68 || stats.get(StatType.Crafting).currentLevel < 62
+                || stats.get(StatType.Agility).currentLevel < 60 || stats.get(StatType.Thieving).currentLevel < 60
+                || stats.get(StatType.Construction).currentLevel < 50) {
+            player.dialogue(new MessageDialogue("You need 75 magic, 70 smithing, 68 mining, 62 crafting, 60 agility, 60 thieving, and 50 construction to enter the Myths' guild."));
+            return;
+        }
+        player.lock();
+        player.startEvent(e -> {
+            e.delay(1);
+            player.stepAbs(
+                    player.getAbsX(),
+                    player.getAbsY() + (player.getAbsY() <= obj.getPosition().getY() ? 2 : -2),
+                    StepType.FORCE_WALK);
+            e.delay(2);
+            player.unlock();
+        });
+    }
+
+    static {
+        ObjectAction.register(31616, 2456, 2861, 0, "pass", MythsGuild::guildGate);
+        ObjectAction.register(31616, 2457, 2861, 0, "pass", MythsGuild::guildGate);
+        ObjectAction.register(31616, 2458, 2861, 0, "pass", MythsGuild::guildGate);
+    }
+}
