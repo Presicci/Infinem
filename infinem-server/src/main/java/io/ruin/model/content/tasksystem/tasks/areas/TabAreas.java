71bd6c1dfeeddb79f69d98c38426b0fc48eb9924
Thomas Presicci <thomaspresicci@gmail.com>
2024-02-29 11:19:40 -0700

    Clicking an area in the area tab now opens the reward diary

---
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/areas/TabAreas.java
@@ -0,0 +1,24 @@
+package io.ruin.model.content.tasksystem.tasks.areas;
+
+import io.ruin.model.content.tasksystem.tasks.TaskArea;
+import io.ruin.model.entity.player.Player;
+import io.ruin.model.inter.Interface;
+import io.ruin.model.inter.InterfaceHandler;
+import io.ruin.model.inter.actions.SlotAction;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 2/29/2024
+ */
+public class TabAreas {
+
+    private static void openEntry(Player player, int slot) {
+        AreaReward.openRewards(player, TaskArea.values()[slot]);
+    }
+
+    static {
+        InterfaceHandler.register(Interface.ACHIEVEMENT, h -> {
+            h.actions[2] = (SlotAction) TabAreas::openEntry;
+        });
+    }
+}
