1353ce8d2ff2c8d435b210b7734a91dc152d95b7
Thomas Presicci <thomaspresicci@gmail.com>
2024-03-02 22:05:02 -0700

    Changed 'Equip a Piece of a Mystic Set' task to 'Get a Mystic Piece Drop'

--- infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/EquipTask.java
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/EquipTask.java
@@ -36,13 +36,6 @@ public enum EquipTask {
             Items.ADAMANT_HALBERD, Items.ADAMANT_2H_SWORD, Items.ADAMANT_CROSSBOW, Items.ADAMANT_KNIFE, Items.ADAMANT_DART, Items.ADAMANT_THROWNAXE
     ),
     YEW_SHORTBOW(144, Items.YEW_SHORTBOW),  // Equip a Yew Shortbow
-    MYSTIC(149, // Equip a Piece of a Mystic Set
-            Items.MYSTIC_HAT, Items.MYSTIC_ROBE_TOP, Items.MYSTIC_ROBE_BOTTOM, Items.MYSTIC_GLOVES, Items.MYSTIC_BOOTS,
-            Items.MYSTIC_HAT_LIGHT, Items.MYSTIC_ROBE_TOP_LIGHT, Items.MYSTIC_ROBE_BOTTOM_LIGHT, Items.MYSTIC_GLOVES_LIGHT, Items.MYSTIC_BOOTS_LIGHT,
-            Items.MYSTIC_HAT_DARK, Items.MYSTIC_ROBE_TOP_DARK, Items.MYSTIC_ROBE_BOTTOM_DARK, Items.MYSTIC_GLOVES_DARK, Items.MYSTIC_BOOTS_DARK,
-            23047, 23050, 23053, 23056, 23059,  // Dusk
-            26531, 26533, 26535, 26537, 26539   // Shattered
-    )
     ;
 
     private final int uuid;
