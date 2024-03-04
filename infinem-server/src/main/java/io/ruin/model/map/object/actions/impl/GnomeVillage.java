505fbe30bbd44db38735ce4d627e5d43925d746e
Thomas Presicci <thomaspresicci@gmail.com>
2024-03-04 14:37:04 -0700

    Player's have to make it through the maze once before Elkoy will lead the player

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/GnomeVillage.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/GnomeVillage.java
@@ -12,31 +12,40 @@ import io.ruin.model.inter.utils.Option;
 public class GnomeVillage {
 
     static {
-        NPCAction toMaze = (player, npc) -> player.startEvent(event -> {
-            player.lock();
-            player.sendMessage("Elkoy leads you through the maze.");
-            player.getPacketSender().fadeOut();
-            event.delay(2);
-            if (player.getAbsY() > 3174) {
-                player.getMovement().teleport(2515, 3160, 0);
-            } else {
-                player.getMovement().teleport(2503, 3192, 0);
+        NPCAction toMaze = (player, npc) -> {
+            if (!player.getTaskManager().hasCompletedTask(564)) {  // Make it through the Tree Gnome Village Maze
+                player.dialogue(new NPCDialogue(npc, "You should find your way through the first time on your own, then I can help you."));
+                return;
             }
-            event.delay(2);
-            player.getPacketSender().fadeIn();
-            player.unlock();
-            player.dialogue(new NPCDialogue(npc,"Here we are."));
-        });
+            player.startEvent(event -> {
+                player.lock();
+                player.sendMessage("Elkoy leads you through the maze.");
+                player.getPacketSender().fadeOut();
+                event.delay(2);
+                if (player.getAbsY() > 3174) {
+                    player.getMovement().teleport(2515, 3160, 0);
+                } else {
+                    player.getMovement().teleport(2503, 3192, 0);
+                }
+                event.delay(2);
+                player.getPacketSender().fadeIn();
+                player.unlock();
+                player.dialogue(new NPCDialogue(npc,"Here we are."));
+            });
+        };
 
         NPCAction.register(4968, "follow", toMaze);
         NPCAction.register(4968, "talk-to", (player, npc) -> {
+            if (!player.getTaskManager().hasCompletedTask(564)) {  // Make it through the Tree Gnome Village Maze
+                player.dialogue(new NPCDialogue(npc, "You should find your way through the first time on your own, then I can help you."));
+                return;
+            }
             player.dialogue(new NPCDialogue(npc, "I can lead you through the maze, if you'd like."),
                     new OptionsDialogue(
-                            new Option("Yes, please.", () -> {
-                                toMaze.handle(player, npc);
-                            }),
-                            new Option("No thanks.", () -> {})
-                    ));
+                            new Option("Yes, please.", () -> toMaze.handle(player, npc)),
+                            new Option("No thanks.")
+                    )
+            );
         });
     }
 }
