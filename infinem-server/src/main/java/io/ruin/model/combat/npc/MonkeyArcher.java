f9d867559662d0ee5ce6526a9663be8f91eee2b6
Thomas Presicci <thomaspresicci@gmail.com>
2024-03-04 15:48:37 -0700

    Monkey archer will now no longer attack if a greegree is active

--- infinem-server/src/main/java/io/ruin/model/combat/npc/MonkeyArcher.java
+++ infinem-server/src/main/java/io/ruin/model/combat/npc/MonkeyArcher.java
@@ -1,6 +1,7 @@
 package io.ruin.model.combat.npc;
 
 import io.ruin.model.entity.npc.NPCCombat;
+import io.ruin.model.item.actions.impl.MonkeyGreeGree;
 import io.ruin.model.map.Projectile;
 
 /**
@@ -21,6 +22,17 @@ public class MonkeyArcher extends NPCCombat {
 
     @Override
     public boolean attack() {
+        if (target.isPlayer()) {
+            int targetAppearance = target.player.getAppearance().getNpcId();
+            if (targetAppearance > -1) {
+                for (MonkeyGreeGree greeGree : MonkeyGreeGree.values()) {
+                    if (targetAppearance == greeGree.npcId) {
+                        return false;
+                    }
+                }
+            }
+        }
+
         if(withinDistance(8)) {
             projectileAttack(PROJECTILE, info.attack_animation, info.attack_style, info.max_damage);
             return true;
