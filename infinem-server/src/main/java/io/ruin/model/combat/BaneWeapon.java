94b138a6f9376f5aead74f145cd657e3655ac60d
Thomas Presicci <thomaspresicci@gmail.com>
2024-03-06 15:38:12 -0700

    Changed BaneWeapons.java into an enum and made the adding of bane weapons much easier

---
+++ infinem-server/src/main/java/io/ruin/model/combat/BaneWeapon.java
@@ -0,0 +1,43 @@
+package io.ruin.model.combat;
+
+import io.ruin.cache.ItemDef;
+import io.ruin.model.entity.Entity;
+import io.ruin.model.item.Items;
+import lombok.AllArgsConstructor;
+
+import java.util.function.Predicate;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 3/6/2024
+ */
+@AllArgsConstructor
+public enum BaneWeapon {
+    SILVERLIGHT(Items.SILVERLIGHT, "DEMON", AttackStyle::isMelee, 0, 0.6),
+    DARKLIGHT(Items.DARKLIGHT, "DEMON", AttackStyle::isMelee, 0, 0.6)
+    ;
+
+    private final int itemId;
+    private final String tagKey;
+    private final Predicate<AttackStyle> predicate;
+    private final double attackBoost, damageBoost;
+
+    static {
+        for (BaneWeapon weapon : values()) {
+            ItemDef.get(weapon.itemId).addPreTargetDefendListener((player, item, hit, target) -> onHit(hit, target, weapon));
+        }
+    }
+
+    private static void onHit(Hit hit, Entity target, BaneWeapon weapon) {
+        if (target == null || target.isPlayer() || !target.npc.getDef().hasCustomValue(weapon.tagKey) || hit.attackStyle == null || !weapon.predicate.test(hit.attackStyle))
+            return;
+        if (weapon.attackBoost > 0) {
+            System.out.println(weapon.name() + " boosting attack by " + weapon.attackBoost);
+            hit.boostAttack(weapon.attackBoost);
+        }
+        if (weapon.damageBoost > 0) {
+            System.out.println(weapon.name() + " boosting damage by " + weapon.damageBoost);
+            hit.boostDamage(weapon.damageBoost);
+        }
+    }
+}
