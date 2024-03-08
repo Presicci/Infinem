8831a2ba60d8e22719c270355bafb284788f66ce
Thomas Presicci <thomaspresicci@gmail.com>
2024-03-08 16:23:46 -0700

    Used lombok annotations to reduce size

--- infinem-server/src/main/java/io/ruin/model/skills/hunter/traps/Trap.java
+++ infinem-server/src/main/java/io/ruin/model/skills/hunter/traps/Trap.java
@@ -4,61 +4,24 @@ import io.ruin.model.entity.player.Player;
 import io.ruin.api.utils.AttributeKey;
 import io.ruin.model.map.object.GameObject;
 import io.ruin.model.skills.hunter.creature.Creature;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 public class Trap {
 
-    private Player owner;
-    private TrapType trapType;
-    private GameObject object;
-    private Creature trappedCreature;
-    private boolean busy;
+    private final Player owner;
+    private final TrapType trapType;
+    @Setter private GameObject object;
+    @Setter private Creature trappedCreature;
+    @Setter private boolean busy;
 
-    private boolean removed = false;
+    @Setter private boolean removed = false;
 
     public Trap(Player owner, TrapType trapType, GameObject object) {
         this.owner = owner;
         this.trapType = trapType;
         this.object = object;
         object.putTemporaryAttribute(AttributeKey.OBJECT_TRAP, this);
-    }
-
-    public Player getOwner() {
-        return owner;
-    }
-
-    public TrapType getTrapType() {
-        return trapType;
-    }
-
-    public GameObject getObject() {
-        return object;
-    }
-
-    public void setObject(GameObject object) {
-        this.object = object;
-    }
-
-    public Creature getTrappedCreature() {
-        return trappedCreature;
-    }
-
-    public void setTrappedCreature(Creature trappedCreature) {
-        this.trappedCreature = trappedCreature;
-    }
-
-    public boolean isBusy() {
-        return busy;
-    }
-
-    public void setBusy(boolean busy) {
-        this.busy = busy;
-    }
-
-    public boolean isRemoved() {
-        return removed;
-    }
-
-    public void setRemoved(boolean removed) {
-        this.removed = removed;
     }
 }
