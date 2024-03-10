2683ddc8f4cff971fd6f6177a5b721ceb5ce01ad
Thomas Presicci <thomaspresicci@gmail.com>
2024-03-09 17:44:11 -0700

    Added guard option for passing through shantay pass

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/ShantayPass.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/locations/ShantayPass.java
@@ -20,6 +20,20 @@ public class ShantayPass {
             }
             player.step(0, player.getAbsY() > obj.y ? -2 : 2, StepType.FORCE_WALK);
         });
+        NPCAction.register(4648, "pass", (player, npc) -> {
+            if (player.getInventory().getAmount(Items.SHANTAY_PASS) < 1) {
+                player.dialogue(new NPCDialogue(4648, "You'll need a Shantay pass to go through the gate into the desert. See Shantay, he'll sell you one for a very reasonable price."));
+                return;
+            }
+            player.startEvent(e -> {
+                player.lock();
+                player.getInventory().remove(Items.SHANTAY_PASS, 1);
+                player.sendMessage("The guard takes your Shantay Pass as you go through the gate.");
+                player.faceNone(false);
+                player.stepAbs(3304, 3115, StepType.FORCE_WALK);
+                e.delay(3);
+                player.unlock();
+            });
+        });
     }
-
 }
