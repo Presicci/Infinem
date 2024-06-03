22c833e5f7de0f89084d98d62742f2f34ac75e31
Thomas Presicci <thomaspresicci@gmail.com>
2024-06-03 17:45:28 -0600

    Added missing breakable items

--- infinem-server/src/main/java/io/ruin/model/item/actions/impl/ItemBreaking.java
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/ItemBreaking.java
@@ -8,16 +8,25 @@ import io.ruin.model.skills.construction.Buildable;
 import io.ruin.model.skills.construction.actions.Workshop;
 
 public enum ItemBreaking {
+    BARRONITE_MACE(25641, 25625, 10500),
 
-    /* capes */
+    // Capes
     FIRE_CAPE(6570, 20445, 150000),
     INFERNAL_CAPE(21295, 21287, 225000),
+    AVAS_ASSEMBLER(22109, 21914, 240000),
+    IMBUED_GUTHIX_CAPE(21793, 24240, 75000),
+    IMBUED_SARA_CAPE(21791, 24236, 75000),
+    IMBUED_ZAMMY_CAPE(21795, 24244, 75000),
+
+    // Max capes
     FIRE_MAX_CAPE(13329, 20447, 99000),
     INFERNAL_MAX_CAPE(21284, 21289, 99000),
-    AVAS_ASSEMBLER(22109, 21914, 240000),
     ASSEMBLER_MAX_CAPE(21898, 21916, 99000),
+    ASSEMBLER_GUTHIX_CAPE(21784, 24242, 99000),
+    ASSEMBLER_SARADOMIN_CAPE(21776, 24238, 99000),
+    ASSEMBLER_ZAMORAK_CAPE(21780, 24246, 99000),
 
-    /* defenders */
+    // Defenders
     BRONZE_DEFENDER(8844, 20449, 1000),
     IRON_DEFENDER(8845, 20451, 2000),
     STEEL_DEFENDER(8846, 20453, 2500),
@@ -28,7 +37,7 @@ public enum ItemBreaking {
     DRAGON_DEFENDER(12954, 20463, 240000),
     AVERNIC_DEFENDER(22322, 22441, 1000000),
 
-    /* void */
+    // Void
     VOID_KNIGHT_MELEE_HELMET(11665, 20481, 160000),
     VOID_KNIGHT_MAGE_HELMET(11663, 20477, 160000),
     VOID_KNIGHT_RANGE_HELMET(11664, 20479, 160000),
@@ -39,14 +48,14 @@ public enum ItemBreaking {
     ELITE_VOID_TOP(13072, 20467, 250000),
     ELITE_VOID_ROBE(13073, 20471, 250000),
 
-    /* barbarian assault */
+    // Barbarian assault
     FIGHTER_HAT(10548, 20507, 45000),
     RANGER_HAT(10550, 20509, 45000),
     HEALER_HAT(10547, 20511, 45000),
     FIGHTER_TORSO(10551, 20513, 150000),
     PENANCE_SKIRT(10555, 20515, 20000),
 
-    /* decorative armour */
+    // Decorative
     DECORATIVE_SWORD(4508, 20483, 5000),
     DECORATIVE_TOP_GOLD(4509, 20485, 5000),
     DECORATIVE_LEGS_GOLD(4510, 20487, 5000),
@@ -60,12 +69,17 @@ public enum ItemBreaking {
     DECORATIVE_CHAPS(11900, 20503, 5000),
     DECORATIVE_QUIVER(11901, 20505, 5000),
 
-    /* halos */
+    // Halos
+    GUTHIX_HALO(12639, 20541, 25000),
     SARADOMIN_HALO(12637, 20537, 25000),
     ZAMORAK_HALO(12638, 20539, 25000),
-    GUTHIX_HALO(12639, 20541, 25000);
+    ARMADYL_HALO(24192, 24147, 25000),
+    BANDOS_HALO(24195, 24149, 25000),
+    SEREN_HALO(24198, 24151, 25000),
+    ANCIENT_HALO(24201, 24153, 25000),
+    BRASSICA_HALO(24204, 24155, 25000);
 
-    public final int brokenId, fixedId, coinRepairCost;
+    public final int fixedId, brokenId, coinRepairCost;
 
     ItemBreaking(int fixedId, int brokenId, int coinRepairCost) {
         this.fixedId = fixedId;
