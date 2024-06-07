e0d2f862c4491a34751effd065a6bf235f1a879a
Thomas Presicci <thomaspresicci@gmail.com>
2024-06-07 00:25:57 -0600

    Merged rdt tables into one

--- infinem-server/src/main/java/io/ruin/model/item/loot/RareDropTable.java
+++ infinem-server/src/main/java/io/ruin/model/item/loot/RareDropTable.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 public class RareDropTable {
 
     public static LootTable RARE_DROP_TABLE = new LootTable()
-            .addTable(100,  // Runes table
+            .addTable(1000,  // Runes table
                     new LootItem(Items.NATURE_RUNE, 25, 60, 3),
                     new LootItem(Items.LAW_RUNE, 25, 65,3),
                     new LootItem(Items.DEATH_RUNE, 25, 40,3),
@@ -29,7 +29,7 @@ public class RareDropTable {
                     new LootItem(Items.EARTH_RUNE, 250, 500, 10),
                     new LootItem(Items.FIRE_RUNE, 200, 400,10)
             )
-            .addTable(60,  // Ammo table
+            .addTable(600,  // Ammo table
                     new LootItem(Items.RUNE_ARROW, 30, 80, 5),
                     new LootItem(Items.ADAMANT_ARROW, 50, 125, 10),
                     new LootItem(Items.STEEL_ARROW, 100, 250, 15),
@@ -37,7 +37,7 @@ public class RareDropTable {
                     new LootItem(Items.ADAMANT_BOLTS, 50, 125, 10),
                     new LootItem(Items.ONYX_BOLT_TIPS, 5, 15, 1)
             )
-            .addTable(25,  // Potion table
+            .addTable(250,  // Potion table
                     new LootItem(Items.PRAYER_POTION_4_NOTE, 5, 15,2),
                     new LootItem(Items.SUPER_RESTORE_4_NOTE, 4, 13,1),
                     new LootItem(Items.SUPER_ATTACK_4_NOTE, 5, 15,5),
@@ -46,14 +46,14 @@ public class RareDropTable {
                     new LootItem(Items.RANGING_POTION_4_NOTE, 5, 15,5),
                     new LootItem(Items.MAGIC_POTION_4_NOTE, 5, 15,5)
             )
-            .addTable(45,  // Gem table
+            .addTable(450,  // Gem table
                     new LootItem(Items.UNCUT_DRAGONSTONE_NOTE, 1, 2,1),
                     new LootItem(Items.UNCUT_DIAMOND_NOTE, 1, 5,5),
                     new LootItem(Items.UNCUT_RUBY_NOTE, 5, 15,10),
                     new LootItem(Items.UNCUT_EMERALD_NOTE, 10, 20,10),
                     new LootItem(Items.UNCUT_SAPPHIRE_NOTE, 15, 30, 10)
             )
-            .addTable(20,  // Rune equipment table
+            .addTable(200,  // Rune equipment table
                     new LootItem(Items.RUNE_FULL_HELM, 1,1),
                     new LootItem(Items.RUNE_MED_HELM, 1, 2),
                     new LootItem(Items.RUNE_PLATEBODY, 1, 1),
@@ -65,23 +65,16 @@ public class RareDropTable {
                     new LootItem(Items.RUNE_BATTLEAXE, 1, 1),
                     new LootItem(Items.RUNE_SCIMITAR, 1, 1)
             )
-            .addTable(5,  // Dragon equipment table
+            .addTable(50,  // Dragon equipment table
                     new LootItem(Items.DRAGON_MED_HELM, 1, 2),
                     new LootItem(Items.SHIELD_LEFT_HALF, 1, 1),
                     new LootItem(Items.DRAGON_SPEAR, 1, 1)
             )
-            .addTable(50,  // Key table
+            .addTable(500,  // Key table
                     new LootItem(Items.LOOP_HALF_OF_KEY, 1,1 ),
                     new LootItem(Items.TOOTH_HALF_OF_KEY, 1, 1)
             )
-            // 10 in 315, 3.17% roll
-            .addTable(10,  // MEGA TABLE ROLL
-                    new LootItem(-1, 1,1)
-            )
-            ;
-
-    public static LootTable MEGA_RARE_DROP_TABLE = new LootTable()
-            .addTable(100,  // Skilling table
+            .addTable(100,  // MEGA TABLE ROLL
                     new LootItem(Items.PALM_TREE_SEED, 7, 12,1),
                     new LootItem(Items.MAGIC_SEED, 3, 5, 2),
                     new LootItem(Items.UNCUT_DRAGONSTONE_NOTE, 45, 55, 1),
@@ -98,17 +91,8 @@ public class RareDropTable {
                     new LootItem(Items.PURE_ESSENCE_NOTE, 150, 225, 2),
                     new LootItem(Items.SOFT_CLAY_NOTE, 300, 450, 2),
                     new LootItem(Items.FLAX_NOTE, 450, 550, 3)
-
             )
-            // 1 in 101, 0.03% roll or 1 in 3181
             .addTable(1,  // GIGA TABLE ROLL
-                    new LootItem(-1, 1,1 )
-            )
-            ;
-
-    public static LootTable GIGA_RARE_DROP_TABLE = new LootTable()
-            // 1 in 10, total is 1 in 31,810
-            .addTable(0,
                     new LootItem(Items.BLURBERRY_SPECIAL, 1, 9),  // Blurberry Special
                     new LootItem(773, 1, 1)   // Hazelmere's signet ring
             );
@@ -155,12 +139,6 @@ public class RareDropTable {
             return Optional.empty();
         }
         Item item = RARE_DROP_TABLE.rollItem();
-        if (item.getId() == -1) {
-            item = MEGA_RARE_DROP_TABLE.rollItem();
-            if (item.getId() == -1) {
-                item = GIGA_RARE_DROP_TABLE.rollItem(); // 1 in 10 baby
-            }
-        }
         player.getTaskManager().doLookupByUUID(905, 1); // Get a Drop from the Rare Drop Table
         return Optional.of(item);
     }
