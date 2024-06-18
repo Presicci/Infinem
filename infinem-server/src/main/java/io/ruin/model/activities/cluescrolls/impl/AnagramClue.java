a0c222acd640dc635bcc18ef629b010a470118aa
Thomas Presicci <thomaspresicci@gmail.com>
2024-06-18 00:42:31 -0600

    Moved AnagramClue registrations to an enum

--- infinem-server/src/main/java/io/ruin/model/activities/cluescrolls/impl/AnagramClue.java
+++ infinem-server/src/main/java/io/ruin/model/activities/cluescrolls/impl/AnagramClue.java
@@ -12,9 +12,63 @@ import java.util.List;
 
 public class AnagramClue extends Clue {
 
+    private enum AnagramClueData {
+        // Easy
+        HANS("Hans", "Snah", ClueType.EASY),
+        LOWE("Lowe", "El Ow", ClueType.EASY),
+        PARTY_PETE("Party Pete", "Peaty Pert", ClueType.EASY),
+        // Medium
+        BRUNDT("Brundt the Chieftain", "Dt Run B", ClueType.MEDIUM),
+        ZOO_KEEPER("Zoo keeper", "Eek Zero Op", ClueType.MEDIUM),
+        SABA("Saba", "A Bas", ClueType.MEDIUM),
+        BARAEK("Baraek", "A Baker", ClueType.MEDIUM),
+        TOBIAS("Captain Tobias", "A Basic Anti Pot", ClueType.MEDIUM),
+        ZENESHA("Zenesha", "A Zen She", ClueType.HARD),
+        JARAAH("Jaraah", "Aha Jar", ClueType.MEDIUM),
+        CAROLINE("Caroline", "Arc O Line", ClueType.MEDIUM),
+        ORACLE("Oracle", "Are Col", ClueType.MEDIUM),
+        HICKTON("Hickton", "Thickno", ClueType.MEDIUM),
+        SIGLI("Sigli the Huntsman", "Unleash Night Mist", ClueType.MEDIUM),
+        TARIA("Taria", "Ratai", ClueType.MEDIUM),
+        RECRUITER("Recruiter", "Err Cure It", ClueType.MEDIUM),
+        GABOOTY("Gabooty", "Got A Boy", ClueType.MEDIUM),
+        OTTO("Otto Godblessed", "Goblets Odd Toes", ClueType.MEDIUM),
+        LUTHAS("Luthas", "Halt Us", ClueType.MEDIUM),
+        NIEVE(490, "I Even", ClueType.MEDIUM),
+        KAYLEE("Kaylee", "Leakey", ClueType.MEDIUM),
+        FEMI("Femi", "Me if", ClueType.MEDIUM),
+        KARIM("Karim", "R Ak Mi", ClueType.MEDIUM),
+        COOK(4626, "Ok Co", ClueType.MEDIUM),//Cook
+        // Hard
+        PERCY("Prospector Percy", "Copper Ore Crypts", ClueType.HARD),
+        DOOMSAYER("Doomsayer", "Do Say More", ClueType.HARD),
+        GNOME_COACH("Gnome Coach", "C On Game Hoc", ClueType.HARD),
+        RAMARA("Ramara du Croissant", "Arr! So I am a crust, and?", ClueType.HARD),
+        STRANGE_OLD_MAN("Strange Old Man", "Dragons Lament", ClueType.HARD),
+        IZZY_NO_BEARD("Cap'n Izzy No-Beard", "O Birdz A Zany En Pc", ClueType.HARD),
+        ONGLEWIP("Professor Onglewip", "Profs Lose Wrong Pie", ClueType.HARD),
+        OMAD("Brother Omad", "Motherboard", ClueType.HARD),
+        MARTIN_THWAIT("Martin Thwait", "Rat Mat Within", ClueType.HARD),
+        STAN("Trader Stan", "Red Art Tans", ClueType.HARD),
+        ODD_OLD_MAN("Odd Old Man", "Land Doomd", ClueType.HARD),
+        // Elite
+        ONEIROMANCER("Oneiromancer", "Career In Moon", ClueType.ELITE),
+        OLD_CRONE("Old Crone", "Cool Nerd", ClueType.ELITE), //Check after restart
+        MANDRITH("Mandrith", "Dr Hitman", ClueType.ELITE),
+        CAM("Cam the Camel", "Machete Clam", ClueType.ELITE);
+
+        AnagramClueData(String npcName, String clue, ClueType type) {
+            register(npcName, clue, type);
+        }
+
+        AnagramClueData(int npcId, String clue, ClueType type) {
+            register(npcId, clue, type);
+        }
+    }
+
     private final String clue;
 
-    private AnagramClue(String clue, ClueType type) {
+    AnagramClue(String clue, ClueType type) {
         super(type, StepType.ANAGRAM);
         this.clue = clue.toUpperCase();
     }
@@ -53,51 +107,5 @@ public class AnagramClue extends Clue {
         if(def.anagram != null)
             System.err.println(def.name + " has duplicate anagram clues set!");
         def.anagram = anagram;
-    }
-
-    static {
-        // Easy
-        register("Hans", "Snah", ClueType.EASY);
-        register("Lowe", "El Ow", ClueType.EASY);
-        register("Party Pete", "Peaty Pert", ClueType.EASY);
-        // Medium
-        register("Brundt the Chieftain", "Dt Run B", ClueType.MEDIUM);
-        register("Zoo keeper", "Eek Zero Op", ClueType.MEDIUM);
-        register("Saba", "A Bas", ClueType.MEDIUM);
-        register("Baraek", "A Baker", ClueType.MEDIUM);
-        register("Captain Tobias", "A Basic Anti Pot", ClueType.MEDIUM);
-        register("Zenesha", "A Zen She", ClueType.HARD);
-        register("Jaraah", "Aha Jar", ClueType.MEDIUM);
-        register("Caroline", "Arc O Line", ClueType.MEDIUM);
-        register("Oracle", "Are Col", ClueType.MEDIUM);
-        register("Hickton", "Thickno", ClueType.MEDIUM);
-        register("Sigli the Huntsman", "Unleash Night Mist", ClueType.MEDIUM);
-        register("Taria", "Ratai", ClueType.MEDIUM);
-        register("Recruiter", "Err Cure It", ClueType.MEDIUM);
-        register("Gabooty", "Got A Boy", ClueType.MEDIUM);
-        register("Otto Godblessed", "Goblets Odd Toes", ClueType.MEDIUM);
-        register("Luthas", "Halt Us", ClueType.MEDIUM);
-        register(490, "I Even", ClueType.MEDIUM);
-        register("Kaylee", "Leakey", ClueType.MEDIUM);
-        register("Femi", "Me if", ClueType.MEDIUM);
-        register("Karim", "R Ak Mi", ClueType.MEDIUM);
-        register(4626, "Ok Co", ClueType.MEDIUM);//Cook
-        // Hard
-        register("Prospector Percy", "Copper Ore Crypts", ClueType.HARD);
-        register("Doomsayer", "Do Say More", ClueType.HARD);
-        register("Gnome Coach", "C On Game Hoc", ClueType.HARD);
-        register("Ramara du Croissant", "Arr! So I am a crust, and?", ClueType.HARD);
-        register("Strange Old Man", "Dragons Lament", ClueType.HARD);
-        register("Cap'n Izzy No-Beard", "O Birdz A Zany En Pc", ClueType.HARD);
-        register("Professor Onglewip", "Profs Lose Wrong Pie", ClueType.HARD);
-        register("Brother Omad", "Motherboard", ClueType.HARD);
-        register("Martin Thwait", "Rat Mat Within", ClueType.HARD);
-        register("Trader Stan", "Red Art Tans", ClueType.HARD);
-        register("Odd Old Man", "Land Doomd", ClueType.HARD);
-        // Elite
-        register("Oneiromancer", "Career In Moon", ClueType.ELITE);
-        register("Old Crone", "Cool Nerd", ClueType.ELITE); //Check after restart
-        register("Mandrith", "Dr Hitman", ClueType.ELITE);
-        register("Cam the Camel", "Machete Clam", ClueType.ELITE);
     }
 }
