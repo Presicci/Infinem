522cf53537ca83a3c8a01afb09f2cb9d10ff0e91
Thomas Presicci <thomaspresicci@gmail.com>
2024-06-19 03:36:56 -0600

    Trickster relic now automatically picks up marks of grace

--- infinem-server/src/main/java/io/ruin/model/skills/agility/courses/MarkOfGrace.java
+++ infinem-server/src/main/java/io/ruin/model/skills/agility/courses/MarkOfGrace.java
@@ -2,9 +2,11 @@ package io.ruin.model.skills.agility.courses;
 
 import io.ruin.api.utils.Random;
 import io.ruin.model.content.ActivitySpotlight;
+import io.ruin.model.content.tasksystem.relics.Relic;
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.entity.player.PlayerGroup;
 import io.ruin.model.item.Item;
+import io.ruin.model.item.Items;
 import io.ruin.model.map.Position;
 import io.ruin.model.map.ground.GroundItem;
 
@@ -18,7 +20,13 @@ public class MarkOfGrace {
         double chance = levelReq / 2D / 100D;
         if (Random.get() <= chance) {
             Position spawn = Random.get(spawns);
-            new GroundItem(new Item(11849, (Random.get(1, 4) + markOfGraceDonatorIncrease(player)) * (ActivitySpotlight.isActive(ActivitySpotlight.DOUBLE_MARKS_OF_GRACE) ? 2 : 1))).owner(player).position(spawn).addToCL().spawn(2);
+            int amount = (Random.get(1, 4) + markOfGraceDonatorIncrease(player)) * (ActivitySpotlight.isActive(ActivitySpotlight.DOUBLE_MARKS_OF_GRACE) ? 2 : 1);
+            if (player.getRelicManager().hasRelicEnalbed(Relic.TRICKSTER) && player.getInventory().hasRoomFor(11849)) {
+                player.getInventory().add(Items.MARK_OF_GRACE, amount);
+                player.getCollectionLog().collect(Items.MARK_OF_GRACE, amount);
+            } else {
+                new GroundItem(new Item(Items.MARK_OF_GRACE, amount)).owner(player).position(spawn).addToCL().spawn(2);
+            }
         }
     }
 
