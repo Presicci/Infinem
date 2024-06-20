c050d05e6ec16525af480178ba6f17fb7e280aae
Thomas Presicci <thomaspresicci@gmail.com>
2024-06-20 00:36:51 -0600

    Fixed lizardman handholds

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/zeah/HandHolds.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/zeah/HandHolds.java
@@ -13,18 +13,41 @@ public class HandHolds {
                     player.lock();
                     player.face(1462, 3690);
                     event.delay(1);
-                    player.animate(1148, 20);
-                    player.getMovement().force(-6, 0, 0, 0, 25, 126, Direction.EAST);
+                    player.animate(1148);
+                    player.getMovement().force(-4, 0, 0, 0, 25, 126, Direction.EAST);
                     event.delay(4);
                     player.unlock();
                 });
             } else {
                 player.startEvent(event -> {
                     player.lock();
-                    player.face(1462, 3690);
+                    player.face(obj);
                     event.delay(1);
-                    player.animate(1148, 15);
-                    player.getMovement().force(6, 0, 0, 0, 25, 135, Direction.EAST);
+                    player.animate(1148);
+                    player.getMovement().force(-5, 0, 0, 0, 25, 135, Direction.WEST);
+                    event.delay(4);
+                    player.unlock();
+                });
+            }
+        });
+        ObjectAction.register(42009, "climb", (player, obj) -> {
+            if (obj.x == 1471 && obj.y == 3687) {
+                player.startEvent(event -> {
+                    player.lock();
+                    player.face(1469, 3687);
+                    event.delay(1);
+                    player.animate(1148);
+                    player.getMovement().force(5, 0, 0, 0, 25, 126, Direction.WEST);
+                    event.delay(4);
+                    player.unlock();
+                });
+            } else {
+                player.startEvent(event -> {
+                    player.lock();
+                    player.face(obj);
+                    event.delay(1);
+                    player.animate(1148);
+                    player.getMovement().force(4, 0, 0, 0, 25, 135, Direction.EAST);
                     event.delay(4);
                     player.unlock();
                 });
