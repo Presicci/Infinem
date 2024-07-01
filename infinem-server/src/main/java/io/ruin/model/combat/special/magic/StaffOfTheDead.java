f98f5d245c5a1f305aebb3726da820a2db685af4
Thomas Presicci <mrbennjerry@gmail.com>
2024-07-01 14:03:28 -0600

    Added correct animation to charged toxic staff of the dead special

--- infinem-server/src/main/java/io/ruin/model/combat/special/magic/StaffOfTheDead.java
+++ infinem-server/src/main/java/io/ruin/model/combat/special/magic/StaffOfTheDead.java
@@ -4,6 +4,7 @@ import io.ruin.cache.def.ItemDefinition;
 import io.ruin.model.activities.duelarena.DuelRule;
 import io.ruin.model.combat.special.Special;
 import io.ruin.model.entity.player.Player;
+import io.ruin.model.item.Items;
 import io.ruin.model.item.containers.Equipment;
 
 //Power of Death: Reduce all melee damage you receive by 50% for the next
@@ -12,7 +13,7 @@ public class StaffOfTheDead implements Special {
 
     @Override
     public boolean accept(ItemDefinition def, String name) {
-        return def.id == 11791 || def.id == 12904 || def.id == 12902 || def.id == 22296;
+        return def.id == Items.STAFF_OF_THE_DEAD || def.id == Items.TOXIC_STAFF_OF_THE_DEAD || def.id == Items.TOXIC_STAFF_UNCHARGED || def.id == Items.STAFF_OF_LIGHT;
     }
 
     @Override
@@ -29,7 +30,10 @@ public class StaffOfTheDead implements Special {
             return false;
         player.sotdDelay.delay(100);
         int staff = player.getEquipment().getId(Equipment.SLOT_WEAPON);
-        player.animate(staff == 11791 ? 7083 : (staff == 12902 ? 1719 : 1720), 0);
+        player.animate(staff == Items.STAFF_OF_THE_DEAD ? 7083
+                : staff == Items.TOXIC_STAFF_OF_THE_DEAD || staff == Items.TOXIC_STAFF_UNCHARGED ? 1720
+                : staff == Items.STAFF_OF_LIGHT ? 7967
+                : 1720, 0);
         player.graphics(1228, 300, 0);
         player.publicSound(1595);
         player.sendMessage("<col=3d5d2b>Spirits of deceased evildoers offer you their protection.");
