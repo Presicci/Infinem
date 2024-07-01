03c957161b194a1fec4483f6dbc5aba552dc7034
Thomas Presicci <mrbennjerry@gmail.com>
2024-06-30 20:30:46 -0600

    Fixed linear climbing spots not being registered correctly Now works for any direction Fixed timing

--- infinem-server/src/main/java/io/ruin/model/skills/agility/shortcut/LinearClimbingSpot.java
+++ infinem-server/src/main/java/io/ruin/model/skills/agility/shortcut/LinearClimbingSpot.java
@@ -16,33 +16,41 @@ import lombok.AllArgsConstructor;
  */
 @AllArgsConstructor
 public enum LinearClimbingSpot {
-
     KARUULM29(34397, new Position(1324, 3778),
             34397, new Position(1324, 3784),
-            29, 1),
+            29, 1, Direction.NORTH),
     KARUULM62(34396, new Position(1324, 3788),
             34396, new Position(1324, 3794),
-            62, 1),;
+            62, 1, Direction.NORTH),;
 
     private final int bottomId;
     private final Position bottomPos;
     private final int topId;
     private final Position topPos;
     private final int levelRequirement, experience;
+    private final Direction faceDirection;
 
     public void climb(Player player, GameObject object) {
         if (!player.getStats().check(StatType.Agility, levelRequirement, "attempt this"))
             return;
         int distance = bottomPos.distance(topPos) + 2;
+        Direction direction = player.getAbsX() > object.x ? Direction.WEST
+                : player.getAbsX() < object.x ? Direction.EAST
+                : player.getAbsY() < object.y ? Direction.NORTH
+                : Direction.SOUTH;
         player.startEvent(e -> {
             e.waitForMovement(player);
             player.lock(LockType.FULL_DELAY_DAMAGE);
             player.animate(4435);
-            if (object.getPosition().equals(bottomPos))
-                player.getMovement().force(0, distance, 0, 0, 0, 180, Direction.NORTH);
-            else
-                player.getMovement().force(0, -distance, 0, 0, 0, 180, Direction.NORTH);
-            e.delay(distance - 2);
+            player.getMovement().force(
+                    direction == Direction.EAST ? distance
+                            : direction == Direction.WEST ? -distance
+                            : 0,
+                    direction == Direction.NORTH ? distance
+                            : direction == Direction.SOUTH ? -distance
+                            : 0,
+                    0, 0, 0, distance * 25, faceDirection);
+            e.delay(distance - 1);
             player.animate(-1);
             player.getStats().addXp(StatType.Agility, experience, true);
             player.unlock();
@@ -52,6 +60,7 @@ public enum LinearClimbingSpot {
     static {
         for (LinearClimbingSpot spot : values()) {
             ObjectAction.register(spot.bottomId, spot.bottomPos, "climb", (spot::climb));
+            ObjectAction.register(spot.topId, spot.topPos, "climb", (spot::climb));
         }
     }
 }
