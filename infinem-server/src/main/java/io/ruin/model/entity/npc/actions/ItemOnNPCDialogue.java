5612c714df972b897e41a7dd75fa01dabc811f61
Thomas Presicci <mrbennjerry@gmail.com>
2024-07-03 15:22:31 -0600

    Added amy ItemOnNPC dialogues

--- infinem-server/src/main/java/io/ruin/model/entity/npc/actions/ItemOnNPCDialogue.java
+++ infinem-server/src/main/java/io/ruin/model/entity/npc/actions/ItemOnNPCDialogue.java
@@ -1,6 +1,7 @@
 package io.ruin.model.entity.npc.actions;
 
 import io.ruin.model.inter.dialogue.NPCDialogue;
+import io.ruin.model.item.Items;
 import io.ruin.model.item.actions.ItemNPCAction;
 
 /**
@@ -8,6 +9,13 @@ import io.ruin.model.item.actions.ItemNPCAction;
  * Created on 7/3/2024
  */
 public enum ItemOnNPCDialogue {
+    AMY(7417,
+            new ItemDialogue("That's a plank, you'll need a lot of those.", Items.PLANK, Items.OAK_PLANK, Items.TEAK_PLANK, Items.MAHOGANY_PLANK),
+            new ItemDialogue("That's a saw. It's pretty useful.", Items.SAW),
+            new ItemDialogue("That's a fascinating looking saw, it looks like it would break though.", Items.CRYSTAL_SAW),
+            new ItemDialogue("Oh you have have one of my saws, I hope you enjoy it!", 24880),
+            new ItemDialogue("That's a hammer. It's pretty useful.", Items.HAMMER)
+    )
     ;
 
     ItemOnNPCDialogue(int npcId, ItemDialogue... itemDialogues) {
