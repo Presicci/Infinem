53cd43e4ad8b360ec0ed69e97fb66f3b77301685
Thomas Presicci <mrbennjerry@gmail.com>
2024-07-05 15:51:48 -0600

    Lowered redwood respawn times to 199 ticks

--- infinem-server/src/main/java/io/ruin/model/skills/woodcutting/Tree.java
+++ infinem-server/src/main/java/io/ruin/model/skills/woodcutting/Tree.java
@@ -19,7 +19,7 @@ public enum Tree {
     YEW(1515, "yew logs", 60, 1.6, 0.04, 175.0, 190, 99, false, 145013, PlayerCounter.CHOPPED_YEW),
     SULLIUSCEP(6004, "", 65, 16.02, 0.178, 127.0, 0.0625, 100, false, 343000, PlayerCounter.CHOPPED_SULLIUSCEP),
     MAGIC(1513, "magic logs", 75, 2.34, 0.033, 250.0, 390, 199, false, 72321, PlayerCounter.CHOPPED_MAGIC),
-    REDWOOD(19669, "redwood logs", 90, 2.73, 0.026, 380.0, 440, 200, false, 72321, PlayerCounter.CHOPPED_REDWOOD),
+    REDWOOD(19669, "redwood logs", 90, 2.73, 0.026, 380.0, 440, 199, false, 72321, PlayerCounter.CHOPPED_REDWOOD),
     ENTTRUNK(-1, "ent trunk", -1, 16.8, 0.27, 25, -1, 0, true, 317647, PlayerCounter.CHOPPED_ENTS), // Used for algo only;
     HOLLOW_TREE(3239, "bark", 45, 9, 0.033, 82.5, 60, 45, false, 214367, PlayerCounter.CHOPPED_REGULAR),
     DRAMEN_TREE(771, "dramen branch", 36, 16.8, 0.27, 0, 0.0, 0, false, 317647, PlayerCounter.CHOPPED_REGULAR);
