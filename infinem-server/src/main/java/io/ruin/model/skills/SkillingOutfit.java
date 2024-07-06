069a5d9d8e97c4cc0ec637d0f8d4603e78147540
Thomas Presicci <mrbennjerry@gmail.com>
2024-07-06 16:58:08 -0600

    Fixed wonky xp multipliers from skilling outfits :)

--- infinem-server/src/main/java/io/ruin/model/skills/SkillingOutfit.java
+++ infinem-server/src/main/java/io/ruin/model/skills/SkillingOutfit.java
@@ -11,26 +11,26 @@ import lombok.Getter;
  */
 @Getter
 public enum SkillingOutfit {
-    CONSTRUCTION(StatType.Construction, 0.5,
-            new SkillingOutfitPiece(0.4, 24872),
-            new SkillingOutfitPiece(0.8, 24874),
-            new SkillingOutfitPiece(0.6, 24876),
-            new SkillingOutfitPiece(0.2, 24878)
+    CONSTRUCTION(StatType.Construction, 0.005,
+            new SkillingOutfitPiece(0.004, 24872),
+            new SkillingOutfitPiece(0.008, 24874),
+            new SkillingOutfitPiece(0.006, 24876),
+            new SkillingOutfitPiece(0.002, 24878)
     ),
-    GRACEFUL(StatType.Agility, 0.4, // 3% total boost
+    GRACEFUL(StatType.Agility, 0.004, // 3% total boost
             // Normal, arceuus, port piscarilius, lovakengj, shayzien, hosidius, kourend, brimhaven agility, hallowed sepulchre, trailblazer
-            new SkillingOutfitPiece(0.35, 11850, 13580, 13592, 13604, 13616, 13628, 13668, 21063, 24745, 25071),            // Hood
-            new SkillingOutfitPiece(0.4, 11852, 13582, 13594, 13606, 13618, 13630, 13670, 21066, 24748, 25074),             // Cape
-            new SkillingOutfitPiece(0.55, 11854, 13584, 13596, 13608, 13620, 13632, 13672, 21069, 24751, 25077),            // Top
-            new SkillingOutfitPiece(0.6, 11856, 13586, 13598, 13610, 13622, 13634, 13674, 21072, 24754, 25080),             // Legs
-            new SkillingOutfitPiece(0.3, 11858, 13588, 13600, 13612, 13624, 13636, 13676, 21075, 24757, 25083),             // Gloves
-            new SkillingOutfitPiece(0.4, 11860, 13590, 13602, 13614, 13626, 13638, 13678, 21078, 24760, 25086)              // Boots
+            new SkillingOutfitPiece(0.0035, 11850, 13580, 13592, 13604, 13616, 13628, 13668, 21063, 24745, 25071),            // Hood
+            new SkillingOutfitPiece(0.004, 11852, 13582, 13594, 13606, 13618, 13630, 13670, 21066, 24748, 25074),             // Cape
+            new SkillingOutfitPiece(0.0055, 11854, 13584, 13596, 13608, 13620, 13632, 13672, 21069, 24751, 25077),            // Top
+            new SkillingOutfitPiece(0.006, 11856, 13586, 13598, 13610, 13622, 13634, 13674, 21072, 24754, 25080),             // Legs
+            new SkillingOutfitPiece(0.003, 11858, 13588, 13600, 13612, 13624, 13636, 13676, 21075, 24757, 25083),             // Gloves
+            new SkillingOutfitPiece(0.004, 11860, 13590, 13602, 13614, 13626, 13638, 13678, 21078, 24760, 25086)              // Boots
     ),
-    PROSPECTOR(StatType.Mining, 0.5,
-            new SkillingOutfitPiece(0.4, Items.PROSPECTOR_HELMET, 25549),
-            new SkillingOutfitPiece(0.8, Items.PROSPECTOR_JACKET, Items.VARROCK_ARMOUR_4, 25551),
-            new SkillingOutfitPiece(0.6, Items.PROSPECTOR_LEGS, 25553),
-            new SkillingOutfitPiece(0.2, Items.PROSPECTOR_BOOTS, 25555)
+    PROSPECTOR(StatType.Mining, 0.005,
+            new SkillingOutfitPiece(0.004, Items.PROSPECTOR_HELMET, 25549),
+            new SkillingOutfitPiece(0.008, Items.PROSPECTOR_JACKET, Items.VARROCK_ARMOUR_4, 25551),
+            new SkillingOutfitPiece(0.006, Items.PROSPECTOR_LEGS, 25553),
+            new SkillingOutfitPiece(0.002, Items.PROSPECTOR_BOOTS, 25555)
     );
 
     private final StatType statType;
@@ -58,7 +58,7 @@ public enum SkillingOutfit {
         if (setBonus > 0D && equippedPieces >= pieces.length) {
             bonus += setBonus;
         }
-        player.sendMessage(bonus + "");
+        player.sendMessage("in" + bonus);
         return bonus;
     }
 
@@ -66,9 +66,10 @@ public enum SkillingOutfit {
         double bonus = 1D;
         for (SkillingOutfit outfit : values()) {
             if (outfit.statType == statType) {
-                bonus += outfit.getExperienceBonus(player);
+                bonus *= outfit.getExperienceBonus(player);
             }
         }
+        player.sendMessage("out " + bonus);
         return bonus;
     }
 
