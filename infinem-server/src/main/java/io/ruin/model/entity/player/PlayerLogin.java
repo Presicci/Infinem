2ac7c5d69be2456b0ee026b45770f403b4d5e516
Thomas Presicci <mrbennjerry@gmail.com>
2024-07-09 02:10:38 -0600

    Added phpbb authentication

--- infinem-server/src/main/java/io/ruin/model/entity/player/PlayerLogin.java
+++ infinem-server/src/main/java/io/ruin/model/entity/player/PlayerLogin.java
@@ -10,6 +10,18 @@ import io.ruin.network.central.CentralClient;
 import io.ruin.utility.OfflineMode;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
+
 @Slf4j
 public class PlayerLogin extends LoginRequest {
 
@@ -67,12 +79,17 @@ public class PlayerLogin extends LoginRequest {
                 deny(Response.ERROR_LOADING_ACCOUNT);
                 return;
             }
-            if(player.getPassword() != null) {  // TODO BCrypt encryption
+            int returnCode = phpbbAuth();
+            if (returnCode == 0) {
+                deny(Response.INVALID_LOGIN);
+                return;
+            }
+            /*if(player.getPassword() != null) {  // TODO BCrypt encryption
                 if (!info.password.equalsIgnoreCase(player.getPassword())) {
                     deny(Response.INVALID_LOGIN);
                     return;
                 }
-            }
+            }*/
             player.setIndex(index);
             player.init(info);
             World.players.set(index, player);
@@ -80,6 +97,60 @@ public class PlayerLogin extends LoginRequest {
             player.getPacketSender().sendLogin(info);
             player.getChannel().pipeline().replace("decoder", "decoder", player.getDecoder());
         });
+    }
+
+    private static final BigInteger AUTH_KEY = new BigInteger("104669708125332383643049895335043994044443794620533303994927");
+
+    private int phpbbAuth() {
+        try {
+            String urlString = "https://www.infinem.net/client_auth.php?auth_key="+AUTH_KEY+"name="+info.name.toLowerCase().replace(" ","_")+"&pass="+encrypt("6YUYrFblfufvV0m9", "o3ZXgtKvts1YRYQT", info.password);
+            HttpURLConnection conn = (HttpURLConnection) new URL(urlString).openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = in.readLine().trim();
+            return Integer.parseInt(line);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    private static final String CIPHER_NAME = "AES/CBC/PKCS5PADDING";
+    private static final int CIPHER_KEY_LEN = 16; //128 bits
+
+    public static String encrypt(String key, String iv, String data) {
+        try {
+            if (key.length() < CIPHER_KEY_LEN) {
+                int numPad = CIPHER_KEY_LEN - key.length();
+
+                StringBuilder keyBuilder = new StringBuilder(key);
+                for(int i = 0; i < numPad; i++){
+                    keyBuilder.append("0"); //0 pad to len 16 bytes
+                }
+                key = keyBuilder.toString();
+
+            } else if (key.length() > CIPHER_KEY_LEN) {
+                key = key.substring(0, CIPHER_KEY_LEN); //truncate to 16 bytes
+            }
+
+
+            IvParameterSpec initVector = new IvParameterSpec(iv.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance(CIPHER_NAME);
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, initVector);
+
+            byte[] encryptedData = cipher.doFinal((data.getBytes()));
+
+            String base64_EncryptedData = Base64.getEncoder().encodeToString(encryptedData);
+            String base64_IV = Base64.getEncoder().encodeToString(iv.getBytes("UTF-8"));
+
+            return base64_EncryptedData + ":" + base64_IV;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
     }
 
     @Override
