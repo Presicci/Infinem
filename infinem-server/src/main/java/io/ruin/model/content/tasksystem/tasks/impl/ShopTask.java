c927c21a8eb8e88cc3922c2e2c66cea1efb78bc0
Thomas Presicci <mrbennjerry@gmail.com>
2024-07-15 18:07:28 -0600

    Added 'Purchase a Dragon Scimitar' task handling

--- infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/ShopTask.java
+++ infinem-server/src/main/java/io/ruin/model/content/tasksystem/tasks/impl/ShopTask.java
@@ -1,8 +1,10 @@
 package io.ruin.model.content.tasksystem.tasks.impl;
 
 import io.ruin.cache.def.ItemDefinition;
+import io.ruin.model.item.Items;
 
 public enum ShopTask {
+    DRAGON_SCIMITAR(579, Items.DRAGON_SCIMITAR),
     CRYSTAL_CROWN(830, 23911);
 
     ShopTask(int uuid, int... itemIds) {
