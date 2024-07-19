f1ae0133b6904cf810361a20ae7c9600c1f6aafb
Thomas Presicci <mrbennjerry@gmail.com>
2024-07-19 15:46:13 -0600

    Todo spell now sends an appropriate message to normal players

--- infinem-server/src/main/java/io/ruin/model/skills/magic/spells/TodoSpell.java
+++ infinem-server/src/main/java/io/ruin/model/skills/magic/spells/TodoSpell.java
@@ -1,14 +1,22 @@
 package io.ruin.model.skills.magic.spells;
 
+import io.ruin.model.entity.player.Player;
 import io.ruin.model.skills.magic.Spell;
 
 public class TodoSpell extends Spell {
 
-    public TodoSpell(String name) {
-        clickAction = (p, i) -> p.sendMessage("Todo click action spell: " + name);
-        itemAction = (p, i) -> p.sendMessage("Todo item action spell: " + name);
-        entityAction = (p, e) -> p.sendMessage("Todo entity action spell: " + name);
-        objectAction = (p, o) -> p.sendMessage("Todo object action spell: " + name);
+    private void message(Player player, String action, String name) {
+        if (player.isStaff()) {
+            player.sendMessage("Todo " + action + " action spell: " + name);
+        } else {
+            player.sendMessage("This spell is not yet implemented.");
+        }
     }
 
+    public TodoSpell(String name) {
+        clickAction = (p, i) -> message(p, "click", name);
+        itemAction = (p, i) -> message(p, "item", name);
+        entityAction = (p, e) -> message(p, "entity", name);
+        objectAction = (p, o) -> message(p, "object", name);
+    }
 }
