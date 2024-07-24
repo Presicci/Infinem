08d7402085d617ed1f3c066dadb68436569b6f71
Thomas Presicci <mrbennjerry@gmail.com>
2024-07-24 08:14:54 -0600

    Trimmed and gilded mith platelegs can now be used for emote steps

--- infinem-server/src/main/java/io/ruin/model/activities/cluescrolls/impl/EmoteClue.java
+++ infinem-server/src/main/java/io/ruin/model/activities/cluescrolls/impl/EmoteClue.java
@@ -154,6 +154,7 @@ public class EmoteClue extends Clue {
             .put(Items.DRAGON_BATTLEAXE, Arrays.asList(Items.DRAGON_BATTLEAXE, 28037))
             .put(Items.BRONZE_FULL_HELM, Arrays.asList(Items.BRONZE_FULL_HELM, Items.BRONZE_FULL_HELM_T, Items.BRONZE_FULL_HELM_G))
             .put(Items.BRONZE_PLATELEGS, Arrays.asList(Items.BRONZE_PLATELEGS, Items.BRONZE_PLATELEGS_T, Items.BRONZE_PLATELEGS_G))
+            .put(Items.MITHRIL_PLATELEGS, Arrays.asList(Items.MITHRIL_PLATELEGS, Items.MITHRIL_PLATELEGS_T, Items.MITHRIL_PLATELEGS_G))
             .put(Items.ADAMANT_FULL_HELM, Arrays.asList(Items.ADAMANT_FULL_HELM, Items.ADAMANT_FULL_HELM_T, Items.ADAMANT_FULL_HELM_G))
             .put(Items.RUNE_KITESHIELD, Arrays.asList(
                     Items.RUNE_KITESHIELD, Items.RUNE_KITESHIELD_T, Items.RUNE_KITESHIELD_G,
