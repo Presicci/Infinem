d2fd53b9c423f0549c22b88d31c92af46760d492
Thomas Presicci <mrbennjerry@gmail.com>
2024-08-08 04:08:33 -0600

    Grubby chest now has proper rates and drops are dropped together properly

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/chests/GrubbyChest.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/chests/GrubbyChest.java
@@ -5,6 +5,8 @@ import io.ruin.model.entity.player.Player;
 import io.ruin.model.item.Item;
 import io.ruin.model.item.Items;
 import io.ruin.model.item.loot.LootItem;
+import io.ruin.model.item.loot.LootItemPair;
+import io.ruin.model.item.loot.LootItemSet;
 import io.ruin.model.item.loot.LootTable;
 import io.ruin.model.map.object.GameObject;
 import io.ruin.model.map.object.actions.ObjectAction;
@@ -19,36 +21,38 @@ import java.util.List;
 public class GrubbyChest {
 
     public static final LootTable FOOD = new LootTable().addTable(0,
-            new LootItem(Items.EGG_POTATO, 4, 11),
+            new LootItem(Items.EGG_POTATO, 4, 12),
             new LootItem(Items.SHARK, 4, 7),
-            new LootItem(Items.SARADOMIN_BREW_2, 3, 1),
-            new LootItem(Items.SUPER_RESTORE_2, 1, 1)
+            new LootItemPair(Items.SARADOMIN_BREW_2, 3, 3, Items.SUPER_RESTORE_2, 1, 1, 1)
     );
 
     public static final LootTable POTION = new LootTable().addTable(0,
-            new LootItem(Items.SUPER_DEFENCE_2, 1, 8),
-            new LootItem(Items.SUPER_ATTACK_2, 1, 3),
-            new LootItem(Items.SUPER_STRENGTH_2, 1, 3),
-            new LootItem(Items.RANGING_POTION_2, 1, 3),
-            new LootItem(Items.PRAYER_POTION_3, 2, 2),
+            new LootItemSet(8,
+                    new LootItem(Items.SUPER_ATTACK_2, 1, 1),
+                    new LootItem(Items.SUPER_STRENGTH_2, 1, 1),
+                    new LootItem(Items.SUPER_DEFENCE_2, 1, 1)),
+            new LootItemSet(8,
+                    new LootItem(Items.SUPER_RANGING_2, 1, 1),
+                    new LootItem(Items.SUPER_DEFENCE_2, 1, 1)),
+            new LootItem(Items.PRAYER_POTION_3, 2, 3),
             new LootItem(Items.SUPER_RESTORE_3, 2, 1)
     );
 
     public static final LootTable MAIN = new LootTable().addTable(0,
-            new LootItem(Items.LAW_RUNE, 200, 6),
-            new LootItem(Items.DEATH_RUNE, 200, 6),
-            new LootItem(Items.ASTRAL_RUNE, 200, 6),
-            new LootItem(Items.BLOOD_RUNE, 200, 6),
-            new LootItem(Items.GRIMY_TOADFLAX_NOTE, 10, 4),
-            new LootItem(Items.GRIMY_RANARR_WEED_NOTE, 10, 4),
-            new LootItem(Items.COINS, 10000, 4),
-            new LootItem(Items.GRIMY_SNAPDRAGON_NOTE, 10, 3),
-            new LootItem(Items.GRIMY_TORSTOL_NOTE, 5, 3),
-            new LootItem(Items.CRYSTAL_KEY, 1, 2),
-            new LootItem(Items.DRAGON_BONES_NOTE, 10, 2),
-            new LootItem(Items.RED_DRAGONHIDE_NOTE, 10, 2),
-            new LootItem(Items.DRAGON_DART_TIP, 50, 1),
-            new LootItem(Items.DRAGON_ARROWTIPS, 50, 1)
+            new LootItem(Items.LAW_RUNE, 200, 50),
+            new LootItem(Items.DEATH_RUNE, 200, 50),
+            new LootItem(Items.ASTRAL_RUNE, 200, 50),
+            new LootItem(Items.BLOOD_RUNE, 200, 50),
+            new LootItem(Items.GRIMY_TOADFLAX_NOTE, 10, 40),
+            new LootItem(Items.GRIMY_RANARR_WEED_NOTE, 10, 40),
+            new LootItem(Items.COINS, 10000, 40),
+            new LootItem(Items.GRIMY_SNAPDRAGON_NOTE, 10, 35),
+            new LootItem(Items.GRIMY_TORSTOL_NOTE, 5, 35),
+            new LootItem(Items.CRYSTAL_KEY, 1, 30),
+            new LootItem(Items.DRAGON_BONES_NOTE, 10, 30),
+            new LootItem(Items.RED_DRAGONHIDE_NOTE, 10, 30),
+            new LootItem(Items.DRAGON_DART_TIP, 50, 10),
+            new LootItem(Items.DRAGON_ARROWTIPS, 50, 10)
     );
 
     private static void openChest(Player player, GameObject obj) {
@@ -66,10 +70,10 @@ public class GrubbyChest {
 
             List<Item> loot = new ArrayList<>();
             // Two food rolls
-            loot.add(FOOD.rollItem());
-            loot.add(FOOD.rollItem());
+            loot.addAll(FOOD.rollItems(false));
+            loot.addAll(FOOD.rollItems(false));
             // One potion roll
-            loot.add(POTION.rollItem());
+            loot.addAll(POTION.rollItems(false));
             // One main roll
             loot.add(MAIN.rollItem());
 
