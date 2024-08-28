b14c7e00aa4cd55e7c7f396cecb7e430c36ef4ab
Thomas Presicci <mrbennjerry@gmail.com>
2024-08-27 22:33:57 -0600

    Replaced generic skill lookup with STRING_BOW lookup

--- infinem-server/src/main/java/io/ruin/model/skills/fletching/StringBow.java
+++ infinem-server/src/main/java/io/ruin/model/skills/fletching/StringBow.java
@@ -1,5 +1,7 @@
 package io.ruin.model.skills.fletching;
 
+import io.ruin.cache.def.ItemDefinition;
+import io.ruin.model.content.tasksystem.tasks.TaskCategory;
 import io.ruin.model.entity.player.Player;
 import io.ruin.model.inter.dialogue.skill.SkillDialogue;
 import io.ruin.model.inter.dialogue.skill.SkillItem;
@@ -43,7 +45,7 @@ public enum StringBow {
         player.sendFilteredMessage("You add a string to the bow.");
         player.animate(animation);
         player.getStats().addXp(StatType.Fletching, exp, true);
-        player.getTaskManager().doSkillItemLookup(strung);
+        player.getTaskManager().doLookupByCategoryAndTrigger(TaskCategory.STRING_BOW, ItemDefinition.get(strung).name);
     }
 
     private static final int BOW_STRING = 1777;
