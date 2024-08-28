f563ba24c6e19832b19c3685cc922b1373b8bc17
Thomas Presicci <mrbennjerry@gmail.com>
2024-08-28 01:45:38 -0600

    Changed option names to match new cache definitions

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/CoalTruck.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/CoalTruck.java
@@ -1,6 +1,7 @@
 package io.ruin.model.map.object.actions.impl;
 
 import io.ruin.model.content.tasksystem.areas.AreaReward;
+import io.ruin.model.entity.player.Player;
 import io.ruin.model.inter.dialogue.ItemDialogue;
 import io.ruin.model.inter.dialogue.MessageDialogue;
 import io.ruin.model.item.Items;
@@ -14,41 +15,40 @@ import io.ruin.model.map.object.actions.ObjectAction;
  */
 public class CoalTruck {
 
-    static {
-        ObjectAction.register(10836, "remove-coal", (player, obj) -> {
-            if (!AreaReward.COAL_TRUCKS.checkReward(player, "use these trucks.")) return;
-            player.dialogue(new MessageDialogue("The dwarf will take your coal to the bank, no need to worry about it."));
+    private static void deposit(Player player) {
+        if (!AreaReward.COAL_TRUCKS.checkReward(player, "use these trucks.")) return;
+        int bankAmt = player.getBank().getAmount(Items.COAL);
+        int amt = Math.min(Integer.MAX_VALUE - bankAmt, player.getInventory().getAmount(Items.COAL));
+        if (amt <= 0) {
+            player.dialogue(new MessageDialogue("Your bank has too much coal!"));
+            return;
+        }
+        player.startEvent(e -> {
+            player.lock();
+            player.animate(832);
+            e.delay(1);
+            int bagAmt = 0;
+            if (CoalBag.hasBag(player)) {
+                bagAmt = Math.min(Integer.MAX_VALUE - bankAmt, player.getAttributeIntOrZero("BAGGED_COAL"));
+                if (bagAmt > 0) {
+                    player.incrementNumericAttribute("BAGGED_COAL", -bagAmt);
+                    player.getBank().add(Items.COAL, amt);
+                }
+            }
+            player.getInventory().remove(Items.COAL, amt);
+            player.getBank().add(Items.COAL, amt);
+            player.dialogue(new ItemDialogue().one(Items.COAL, "You deposit " + (amt + bagAmt) + " coal.<br>Your bank now has " + (bankAmt + amt + bagAmt) + " coal stored."));
+            player.getTaskManager().doLookupByUUID(914, 1); // Use a Coal Truck at the Coal Mine
+            player.unlock();
         });
+    }
+
+    static {
+        ObjectAction.register(10836, "deposit", (player, obj) -> deposit(player));
         ObjectAction.register(10836, "investigate", (player, obj) -> {
             if (!AreaReward.COAL_TRUCKS.checkReward(player, "use these trucks.")) return;
             player.dialogue(new MessageDialogue("If you deposit coal into the truck, the dwarves will bring it to your bank for you, free of charge."));
         });
-        ItemObjectAction.register(Items.COAL, 10836, ((player, item, obj) -> {
-            if (!AreaReward.COAL_TRUCKS.checkReward(player, "use these trucks.")) return;
-            int bankAmt = player.getBank().getAmount(Items.COAL);
-            int amt = Math.min(Integer.MAX_VALUE - bankAmt, player.getInventory().getAmount(Items.COAL));
-            if (amt <= 0) {
-                player.dialogue(new MessageDialogue("Your bank has too much coal!"));
-                return;
-            }
-            player.startEvent(e -> {
-                player.lock();
-                player.animate(832);
-                e.delay(1);
-                int bagAmt = 0;
-                if (CoalBag.hasBag(player)) {
-                    bagAmt = Math.min(Integer.MAX_VALUE - bankAmt, player.getAttributeIntOrZero("BAGGED_COAL"));
-                    if (bagAmt > 0) {
-                        player.incrementNumericAttribute("BAGGED_COAL", -bagAmt);
-                        player.getBank().add(Items.COAL, amt);
-                    }
-                }
-                player.getInventory().remove(Items.COAL, amt);
-                player.getBank().add(Items.COAL, amt);
-                player.dialogue(new ItemDialogue().one(Items.COAL, "You deposit " + (amt + bagAmt) + " coal.<br>Your bank now has " + (bankAmt + amt + bagAmt) + " coal stored."));
-                player.getTaskManager().doLookupByUUID(914, 1); // Use a Coal Truck at the Coal Mine
-                player.unlock();
-            });
-        }));
+        ItemObjectAction.register(Items.COAL, 10836, ((player, item, obj) -> deposit(player)));
     }
 }
