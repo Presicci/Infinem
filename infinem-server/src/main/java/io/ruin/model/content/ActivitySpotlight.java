6ae8aa38c015760c9a2ee73dcdb0f5cb127b2054
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-01 17:02:24 -0600

    Disabled activity spotlights will no longer show up

--- infinem-server/src/main/java/io/ruin/model/content/ActivitySpotlight.java
+++ infinem-server/src/main/java/io/ruin/model/content/ActivitySpotlight.java
@@ -37,7 +37,7 @@ public enum ActivitySpotlight {
     }
 
     public static void cycleSpotlight() {
-        activeSpotlight = ActivitySpotlight.values()[Random.get(ActivitySpotlight.values().length - 1)];
+        activeSpotlight = Random.get(getPossibleActivities());
         Broadcast.ACTIVITY_SPOTLIGHT.sendNews("<col=d9a414><shad=0>The activity spotlight is now: " + StringUtils.capitalizeFirst(activeSpotlight.toString().toLowerCase().replace("_", " ")));
     }
 
