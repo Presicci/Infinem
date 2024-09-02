d9b2abe0f859053a7fdbd415b253804633ed7866
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-02 16:07:43 -0600

    Added unimplemented messages for temp

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/UnimplementedObject.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/UnimplementedObject.java
@@ -10,7 +10,10 @@ import io.ruin.model.map.object.actions.ObjectAction;
 public enum UnimplementedObject {
     Soul_Wars(41199, new Position(2199, 2842)),
     Soul__Wars(41200, new Position(2220, 2842)),
-    The_Shayzien_Agility_Course(42209, new Position(1554, 3631));
+    The_Shayzien_Agility_Course(42209, new Position(1554, 3631)),
+    Tempeross(41326, new Position(3194, 2841)),
+    Tempeross$(41311, new Position(3269, 3141)),
+    ;
 
     private final int objectId, option;
     private final Position objectPosition;
@@ -27,7 +30,7 @@ public enum UnimplementedObject {
 
     static {
         for (UnimplementedObject obj : values()) {
-            ObjectAction.register(obj.objectId, obj.objectPosition, obj.option, ((p, o) -> p.sendMessage(obj.name().replace("__", " ").replace("_", " ") + " is not implemented yet.")));
+            ObjectAction.register(obj.objectId, obj.objectPosition, obj.option, ((p, o) -> p.sendMessage(obj.name().replace("__", " ").replace("_", " ").replace("$", "") + " is not implemented yet.")));
         }
     }
 }
