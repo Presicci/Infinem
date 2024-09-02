517c0a8e81ae8fdbe32ab0143bdaabea84f93e72
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-01 19:27:14 -0600

    Enabled impling spawning and npc random walk

--- infinem-server/src/main/java/io/ruin/model/WorldConstants.java
+++ infinem-server/src/main/java/io/ruin/model/WorldConstants.java
@@ -5,6 +5,6 @@ package io.ruin.model;
  * Created on 2/15/2023
  */
 public interface WorldConstants {
-    public static boolean SPAWN_IMPLINGS = false;
-    public static boolean ALLOW_NPC_RANDOM_WALK = false;
+    public static boolean SPAWN_IMPLINGS = true;
+    public static boolean ALLOW_NPC_RANDOM_WALK = true;
 }
