54a04a1e1780a79b9b8d296f5925d7fdaa93c8bc
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-02 16:25:54 -0600

    'Obtain a Mark of Grace' task is now obtainable with trickster

--- infinem-server/src/main/java/io/ruin/model/skills/agility/courses/MarkOfGrace.java
+++ infinem-server/src/main/java/io/ruin/model/skills/agility/courses/MarkOfGrace.java
@@ -24,6 +24,7 @@ public class MarkOfGrace {
             if (player.getRelicManager().hasRelicEnalbed(Relic.TRICKSTER) && player.getInventory().hasRoomFor(11849)) {
                 player.getInventory().add(Items.MARK_OF_GRACE, amount);
                 player.getCollectionLog().collect(Items.MARK_OF_GRACE, amount);
+                player.getTaskManager().doLookupByUUID(52); // Obtain a Mark of Grace
             } else {
                 new GroundItem(new Item(Items.MARK_OF_GRACE, amount)).owner(player).position(spawn).addToCL().spawn(2);
             }
