c282941689a8b5a27f30956d0748afcc3ed76ded
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-02 18:33:50 -0600

    Added week one login rewards

--- infinem-server/src/main/java/io/ruin/model/entity/player/Player.java
+++ infinem-server/src/main/java/io/ruin/model/entity/player/Player.java
@@ -1257,8 +1257,14 @@ public class Player extends PlayerAttributes {
          */
         sendMessage("Welcome to " + World.type.getWorldName() + "!" + (" Have fun!"));
 
-        if(World.weekendExpBoost)
-            player.sendMessage(Color.COOL_BLUE.wrap("The 25% experience weekend boost is currently active!"));
+        sendMessage(Color.COOL_BLUE.wrap("<shad=0>Until next Monday, everyone gets a 31% experience multiplier, thank you everyone for a great launch!"));
+
+        if (!hasAttribute("WEEKONE")) {
+            getBank().add(11863, 1);    // Rainbow partyhat
+            getBank().add(32029, 10);              // Vote tickets
+            sendMessage(Color.RED.wrap("<shad=0>Thank you for logging in for our launch! A rainbow partyhat and some vote tickets have been added to your bank!"));
+            putAttribute("WEEKONE", 1);
+        }
 
         World.sendLoginMessages(this);
 
