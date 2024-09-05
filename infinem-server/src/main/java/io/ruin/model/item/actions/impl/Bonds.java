472249971e6e37ddd41216eb2ef320e0a47035d8
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-04 22:10:24 -0600

    Added bonds

--- infinem-server/src/main/java/io/ruin/model/item/actions/impl/Bonds.java
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/Bonds.java
@@ -1,11 +1,13 @@
 package io.ruin.model.item.actions.impl;
 
 import io.ruin.model.entity.player.Player;
+import io.ruin.model.entity.player.PlayerGroup;
 import io.ruin.model.inter.dialogue.ItemDialogue;
 import io.ruin.model.inter.dialogue.OptionsDialogue;
 import io.ruin.model.inter.utils.Option;
 import io.ruin.model.item.Item;
 import io.ruin.model.item.actions.ItemAction;
+import io.ruin.services.Store;
 
 /*
  * @project Kronos
@@ -16,25 +18,27 @@ public class Bonds {
 
     private static void redeem(Player player, int amount, Item item) {
         player.dialogue(new ItemDialogue().one(item.getId(), "You are about to redeem this bond<br>" +
-                "adding $"+amount+" to your amount donated.<br>" +
+                "adding "+amount+" to your purchased credits.<br>" +
                 "This will consume the bond forever."),
         new OptionsDialogue(
                 new Option("Yes!", (player1 -> {
                     player.getInventory().remove(item);
                     player.storeAmountSpent += amount;
-                    player.sendMessage("You have redeemed a $"+amount+" Bond. Your new total is: $"+player.storeAmountSpent);
+                    player.sendMessage("You have redeemed a $" + (amount / 10) + " Bond. Your new total credits: "+player.storeAmountSpent);
+                    PlayerGroup group = Store.getGroup(player);
+                    if(group != null && !player.isGroup(group)) {
+                        player.join(group);
+                        group.sync(player);
+                        player.sendMessage("Congratulations, you've unlocked a new donator rank: <img=" + group.clientImgId + ">");
+                    }
                 })),
                 new Option("I'll keep it for now.", player::closeDialogue)
         ));
     }
 
     static {
-        /*ItemAction.registerInventory(30276, 1, ((player, item) -> redeem(player, 5, item)));
-        ItemAction.registerInventory(30279, 1, ((player, item) -> redeem(player, 10, item)));
-        ItemAction.registerInventory(30282, 1, ((player, item) -> redeem(player, 25, item)));
-        ItemAction.registerInventory(30285, 1, ((player, item) -> redeem(player, 50, item)));
-        ItemAction.registerInventory(30288, 1, ((player, item) -> redeem(player, 100, item)));
-        ItemAction.registerInventory(30291, 1, ((player, item) -> redeem(player, 250, item)));
-        ItemAction.registerInventory(30294, 1, ((player, item) -> redeem(player, 500, item)));*/
+        ItemAction.registerInventory(32033, 1, ((player, item) -> redeem(player, 50, item)));
+        ItemAction.registerInventory(32035, 1, ((player, item) -> redeem(player, 100, item)));
+        ItemAction.registerInventory(32037, 1, ((player, item) -> redeem(player, 250, item)));
     }
 }
