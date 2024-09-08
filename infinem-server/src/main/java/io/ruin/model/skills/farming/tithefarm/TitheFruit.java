3de51e4142abc4b9466a695f35cf237cba686910
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-08 01:34:04 -0600

    Started tithe fruit object ids

---
+++ infinem-server/src/main/java/io/ruin/model/skills/farming/tithefarm/TitheFruit.java
@@ -0,0 +1,25 @@
+package io.ruin.model.skills.farming.tithefarm;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 9/8/2024
+ */
+public class TitheFruit {
+    /*
+     * 27383 - Empty patch
+     * Golovanova
+     * 27384 - seedling
+     * 27385 - watered
+     * 27386 - dead
+     * 27387 - plant
+     * 88 - watered
+     * 89 - dead
+     * 90 - big plant
+     * 91 - watered
+     * 92 - dead
+     * 93 - harvest
+     * 94 - dead
+     * Bologano
+     * 95
+     */
+}
