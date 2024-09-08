97fcd6a0798a7bb711efa0991738b96d8538c78e
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-08 04:45:29 -0600

    Fixed option scrolls disabling chat

--- infinem-server/src/main/java/io/ruin/model/inter/handlers/OptionScroll.java
+++ infinem-server/src/main/java/io/ruin/model/inter/handlers/OptionScroll.java
@@ -67,7 +67,6 @@ public class OptionScroll {
     static {
         InterfaceHandler.register(187, h -> {
             h.closedAction = (player, integer) -> {
-                if (player.isVisibleInterface(187)) return;
                 player.getPacketSender().sendClientScript(927, "1",  1);
                 player.optionScroll = null;
             };
