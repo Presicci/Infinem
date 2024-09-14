966e46378d6e5f5787aa12a9b4d20cd11ee76001
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-13 20:39:02 -0600

    Added vine clearing in the Fossil island tar swamp

--- infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/fossilisland/FossilIsland.java
+++ infinem-server/src/main/java/io/ruin/model/map/object/actions/impl/fossilisland/FossilIsland.java
@@ -1,9 +1,14 @@
 package io.ruin.model.map.object.actions.impl.fossilisland;
 
+import io.ruin.model.World;
 import io.ruin.model.entity.npc.actions.traveling.Traveling;
+import io.ruin.model.entity.player.Player;
 import io.ruin.model.map.Position;
+import io.ruin.model.map.object.GameObject;
 import io.ruin.model.map.object.actions.ObjectAction;
 import io.ruin.model.map.object.actions.impl.Ladder;
+import io.ruin.model.skills.Tool;
+import io.ruin.model.stat.StatType;
 
 /**
  * @author Mrbennjerry - https://github.com/Presicci
@@ -11,7 +16,28 @@ import io.ruin.model.map.object.actions.impl.Ladder;
  */
 public class FossilIsland {
 
+    private static void rakeVines(Player player, GameObject object) {
+        if (!player.getInventory().hasId(Tool.RAKE)) {
+            player.sendMessage("You need a rake to clear these vines.");
+            return;
+        }
+        player.startEvent(e -> {
+            player.lock();
+            player.animate(2273);
+            e.delay(2);
+            player.unlock();
+            object.setId(-1);
+            player.getStats().addXp(StatType.Farming, 1, true);
+            player.sendFilteredMessage("You rake the vines aside.");
+            World.startEvent(we -> {
+                we.delay(120);
+                object.setId(object.originalId);
+            });
+        });
+    }
+
     static {
+        ObjectAction.register(30644, "clear", FossilIsland::rakeVines);
         // House on the hill stairs
         ObjectAction.register(30681, 3754, 3868, 0, "climb", (player, obj) -> player.getMovement().teleport(3757, 3869, 1));
         ObjectAction.register(30682, 3754, 3868, 1, "descend", (player, obj) -> player.getMovement().teleport(3753, 3869, 0));
