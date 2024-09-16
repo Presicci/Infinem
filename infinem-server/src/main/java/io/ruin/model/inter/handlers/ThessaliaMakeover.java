9052c5a17fb029ad1081fc8de33098fb7ebf2b25
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-16 01:05:45 -0600

    Thessalia will now exchange frog tokens for outfit pieces/books of knowledge

--- infinem-server/src/main/java/io/ruin/model/inter/handlers/ThessaliaMakeover.java
+++ infinem-server/src/main/java/io/ruin/model/inter/handlers/ThessaliaMakeover.java
@@ -12,11 +12,13 @@ import io.ruin.model.inter.Interface;
 import io.ruin.model.inter.InterfaceHandler;
 import io.ruin.model.inter.InterfaceType;
 import io.ruin.model.inter.actions.DefaultAction;
+import io.ruin.model.inter.dialogue.ActionDialogue;
 import io.ruin.model.inter.dialogue.NPCDialogue;
 import io.ruin.model.inter.dialogue.OptionsDialogue;
 import io.ruin.model.inter.dialogue.PlayerDialogue;
 import io.ruin.model.inter.utils.Config;
 import io.ruin.model.inter.utils.Option;
+import io.ruin.model.item.Items;
 
 /**
  * @author Mrbennjerry - https://github.com/Presicci
@@ -26,7 +28,36 @@ public class ThessaliaMakeover {
 
     private static final int PRICE = 250;
 
+    private static void giveFrogReward(Player player, NPC npc, int... itemIds) {
+        if (!player.getInventory().hasId(Items.FROG_TOKEN)) {
+            player.dialogue(new NPCDialogue(npc, "You need a frog token to purchase that."));
+            return;
+        }
+        if (!player.getInventory().hasFreeSlots(itemIds.length - 1)) {
+            player.dialogue(new NPCDialogue(npc, "You need " + (itemIds.length - 1) + " free slots to claim those items."));
+            return;
+        }
+        player.getInventory().remove(Items.FROG_TOKEN, 1);
+        for (int itemId : itemIds) {
+            player.getInventory().add(itemId, 1);
+        }
+    }
+
     private static void dialogue(Player player, NPC npc) {
+        if (player.getInventory().hasId(Items.FROG_TOKEN)) {
+            player.dialogue(
+                    new PlayerDialogue("I have a frog token..."),
+                    new NPCDialogue(npc, "That entitles you to a free costume! Do you want a frog mask, a royal tunic & leggings or a royal blouse & skirt? I've also got some books a little red imp gave me."),
+                    new OptionsDialogue(
+                            new Option("A frog mask, please!", new PlayerDialogue("A frog mask, please!"), new ActionDialogue(() -> giveFrogReward(player, npc, Items.FROG_MASK))),
+                            new Option("A royal tunic & leggings, please.", new PlayerDialogue("A royal tunic & leggings, please."), new ActionDialogue(() -> giveFrogReward(player, npc, 6184, 6185))),
+                            new Option("A royal blouse & skirt, please.", new PlayerDialogue("A royal blouse & skirt, please."), new ActionDialogue(() -> giveFrogReward(player, npc, 6186, 6187))),
+                            new Option("I'll take a book, thanks.", new PlayerDialogue("I'll take the book, thanks."), new ActionDialogue(() -> giveFrogReward(player, npc, Items.BOOK_OF_KNOWLEDGE))),
+                            new Option("Maybe another time.", new PlayerDialogue("Maybe another time."))
+                    )
+            );
+            return;
+        }
         player.dialogue(
                 new NPCDialogue(npc, "Do you want to buy any fine clothes?"),
                 new OptionsDialogue(
