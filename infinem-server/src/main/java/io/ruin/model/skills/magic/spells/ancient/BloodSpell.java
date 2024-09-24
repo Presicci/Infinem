18494376958c2cd532d4934228f8dc288d414c84
Thomas Presicci <mrbennjerry@gmail.com>
2024-09-23 18:19:02 -0600

    Fixed blood bark set effect implementation

--- infinem-server/src/main/java/io/ruin/model/skills/magic/spells/ancient/BloodSpell.java
+++ infinem-server/src/main/java/io/ruin/model/skills/magic/spells/ancient/BloodSpell.java
@@ -10,13 +10,14 @@ public abstract class BloodSpell extends TargetSpell {
     @Override
     protected void afterHit(Hit hit, Entity target) {
         if(hit.damage > 0) {
-            int healAmount = hit.damage / 4;
+            double healPercentage = 0.25;
             if (hit.attacker.player != null) {
                 int bloodbarkPieceCount = SetEffect.BLOODBARK.numberOfPieces(hit.attacker.player);
                 if (bloodbarkPieceCount > 0) {
-                    healAmount = (int) (healAmount * (1D + (bloodbarkPieceCount * 0.02)));
+                    healPercentage += bloodbarkPieceCount * 0.02;
                 }
             }
+            int healAmount = (int) (hit.damage * healPercentage);
             hit.attacker.incrementHp(healAmount);
         }
     }
