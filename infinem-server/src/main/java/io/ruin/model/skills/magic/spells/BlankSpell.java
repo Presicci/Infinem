610455358acb9ff27e683e77688a19193c2258aa
Thomas Presicci <mrbennjerry@gmail.com>
2024-10-17 12:35:06 -0600

    Added BlankSpell class

---
+++ infinem-server/src/main/java/io/ruin/model/skills/magic/spells/BlankSpell.java
@@ -0,0 +1,12 @@
+package io.ruin.model.skills.magic.spells;
+
+import io.ruin.model.skills.magic.Spell;
+
+/**
+ * @author Mrbennjerry - https://github.com/Presicci
+ * Created on 10/17/2024
+ */
+public class BlankSpell extends Spell {
+
+    public BlankSpell() {}
+}
