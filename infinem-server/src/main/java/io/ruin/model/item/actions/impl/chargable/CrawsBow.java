7c3ce5c98a3e711914494613b1449fad266577c6
Thomas Presicci <mrbennjerry@gmail.com>
2024-10-18 17:30:35 -0600

    Deadeye now provies a 50% chance to save craw's bow charges

--- infinem-server/src/main/java/io/ruin/model/item/actions/impl/chargable/CrawsBow.java
+++ infinem-server/src/main/java/io/ruin/model/item/actions/impl/chargable/CrawsBow.java
@@ -1,5 +1,7 @@
 package io.ruin.model.item.actions.impl.chargable;
 
+import io.ruin.api.utils.Random;
+import io.ruin.model.content.tasksystem.relics.Relic;
 import io.ruin.model.item.Items;
 import io.ruin.utility.Color;
 import io.ruin.cache.def.ItemDefinition;
@@ -88,6 +90,7 @@ public class CrawsBow {
     }
 
     public static boolean consumeCharge(Player player, Item item) {
+        if (player.getRelicManager().hasRelicEnalbed(Relic.DEADEYE) && Random.rollDie(2)) return true;
         int currentCharges = AttributeExtensions.getCharges(item);
         if (currentCharges <= 1000) {
             player.sendMessage(Color.DARK_RED.wrap("Your weapon has run out of revenant ether!"));
