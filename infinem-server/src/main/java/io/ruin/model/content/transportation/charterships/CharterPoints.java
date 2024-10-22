d3bb63fb57a7b0295d8340d1c08c85760e3e599f
Thomas Presicci <mrbennjerry@gmail.com>
2024-10-21 19:15:19 -0600

    Added kourend charter locations

--- infinem-server/src/main/java/io/ruin/model/content/transportation/charterships/CharterPoints.java
+++ infinem-server/src/main/java/io/ruin/model/content/transportation/charterships/CharterPoints.java
@@ -9,18 +9,20 @@ import lombok.AllArgsConstructor;
  */
 @AllArgsConstructor
 public enum CharterPoints {
-        BRIMHAVEN(new Position(2760, 3238, 0), 17),
-        CATHERBY(new Position(2792, 3414, 0), 8),
-        MOS_LEHARMLESS(new Position(3671, 2931, 0), 26),
-        MUSA_POINT(new Position(2954, 3158, 0), 14),
-        PORT_KHAZARD(new Position(2674, 3144, 0), 20),
-        PORT_PHASMATYS(new Position(3702, 3503, 0), 5),
-        PORT_SARIM(new Position(3038, 3192, 0), 23),
-        SHIP_YARD(new Position(3001, 3032, 0), 11),
-        PORT_TYRAS(new Position(2142, 3122, 0), 2),
-        CORSAIR_COVE(new Position(2589, 2851, 0), 32),
-        PRIF(new Position(2157, 3330, 0), 33),
-        CRANDOR(new Position(3853, 3235, 0), 29);
+        BRIMHAVEN(new Position(2760, 3238, 0), 22),
+        CATHERBY(new Position(2792, 3414, 0), 9),
+        MOS_LEHARMLESS(new Position(3671, 2931, 0), 30),
+        MUSA_POINT(new Position(2954, 3158, 0), 15),
+        PORT_KHAZARD(new Position(2674, 3144, 0), 24),
+        PORT_PHASMATYS(new Position(3702, 3503, 0), 6),
+        PORT_SARIM(new Position(3038, 3192, 0), 27),
+        SHIP_YARD(new Position(3001, 3032, 0), 13),
+        PORT_TYRAS(new Position(2142, 3122, 0), 3),
+        CORSAIR_COVE(new Position(2589, 2851, 0), 34),
+        PRIF(new Position(2157, 3330, 0), 37),
+        CRANDOR(new Position(3853, 3235, 0), 29),
+        PORT_PISCARILIUS(new Position(1808, 3679, 0), 39),
+        LANDS_END(new Position(1497, 3403, 0), 42);
 
         public final Position pos;
         public final int component;
