13d9216538679eb553f49c766c142f6c65917ace
Thomas Presicci <mrbennjerry@gmail.com>
2024-10-28 19:00:55 -0600

    Updated bug report packet handler to 225

--- infinem-server/src/main/java/io/ruin/network/incoming/handlers/BugReportHandler.java
+++ infinem-server/src/main/java/io/ruin/network/incoming/handlers/BugReportHandler.java
@@ -3,20 +3,21 @@ package io.ruin.network.incoming.handlers;
 import io.ruin.api.buffer.InBuffer;
 import io.ruin.model.entity.player.Player;
 import io.ruin.network.incoming.Incoming;
+import io.ruin.network.ClientPacket;
 import io.ruin.services.discord.impl.BugReportEmbedMessage;
-import io.ruin.utility.IdHolder;
+import io.ruin.utility.ClientPacketHolder;
 
 /**
  * @author Mrbennjerry - https://github.com/Presicci
  * Created on 1/16/2024
  */
-@IdHolder(ids = {71})
+@ClientPacketHolder(packets = {ClientPacket.BUG_REPORT})
 public class BugReportHandler implements Incoming {
     @Override
     public void handle(Player player, InBuffer in, int opcode) {
         String first = in.readString();
         String second = in.readString();
-        in.readByte();
+        in.readByteAdd();
         BugReportEmbedMessage.sendDiscordMessage(player, first, second);
     }
 }
