eb7887a04e06cb0c2e2a4c5518f686c4130c9d1a
Thomas Presicci <mrbennjerry@gmail.com>
2024-10-30 14:45:33 -0600

    All konar tasks now have locations

--- infinem-server/src/main/java/io/ruin/model/skills/slayer/KonarData.java
+++ infinem-server/src/main/java/io/ruin/model/skills/slayer/KonarData.java
@@ -41,15 +41,14 @@ public class KonarData {
         ABYSSAL_DEMON(42, TaskLocation.CATACOMBS_OF_KOUREND, TaskLocation.ABYSSAL_AREA, TaskLocation.SLAYER_TOWER),
         ADAMANT_DRAGON(108, TaskLocation.LITHKREN_VAULT),
         ANKOU(79, TaskLocation.STRONGHOLD_OF_SECURITY, TaskLocation.STRONGHOLD_SLAYER_CAVE, TaskLocation.CATACOMBS_OF_KOUREND),
+        AVIANSIES(94, TaskLocation.GODWARS_DUNGEON),
+        BASILISKS(43, TaskLocation.JORMUNGANDS_PRISON, TaskLocation.FREMENNIK_DUNGEON),
         BLACK_DEMON(30, TaskLocation.TAVERLY_DUNGEON, TaskLocation.BRIMHAVEN_DUNGEON, TaskLocation.CATACOMBS_OF_KOUREND),
         BLACK_DRAGON(27, TaskLocation.TAVERLY_DUNGEON, TaskLocation.EVIL_CHICKEN_LAIR, TaskLocation.MYTHS_GUILD_DUNGEON),
         BLOODVELD(48, TaskLocation.STRONGHOLD_SLAYER_CAVE, TaskLocation.GODWARS_DUNGEON, TaskLocation.SLAYER_TOWER, TaskLocation.CATACOMBS_OF_KOUREND),
         BLUE_DRAGON(25, TaskLocation.TAVERLY_DUNGEON, TaskLocation.OGRE_ENCLAVE, TaskLocation.MYTHS_GUILD_DUNGEON, TaskLocation.CATACOMBS_OF_KOUREND),
         //BRINE_RAT("brine rat", TaskLocation.BRINE_RAT_CAVERN),
         BRONZE_DRAGON(58, TaskLocation.BRIMHAVEN_DUNGEON, TaskLocation.CATACOMBS_OF_KOUREND),
-        IRON_DRAGON(59, TaskLocation.BRIMHAVEN_DUNGEON, TaskLocation.CATACOMBS_OF_KOUREND),
-        STEEL_DRAGON(60, TaskLocation.BRIMHAVEN_DUNGEON, TaskLocation.CATACOMBS_OF_KOUREND),
-        JELLY(50, TaskLocation.RELEKKA_SLAYER_DUNGEON, TaskLocation.CATACOMBS_OF_KOUREND),
         CAVE_KRAKEN(92, TaskLocation.KRAKEN_COVE),
         DAGANNOTH(35, TaskLocation.CATACOMBS_OF_KOUREND, TaskLocation.WATERBIRTH_ISLAND),
         DARK_BEAST(66, TaskLocation.CATACOMBS_OF_KOUREND, TaskLocation.MOURNER_TUNNELS),
@@ -61,16 +60,20 @@ public class KonarData {
         GREATER_DEMON(29, TaskLocation.CHASM_OF_FIRE, TaskLocation.CATACOMBS_OF_KOUREND, TaskLocation.KARUULM_SLAYER_DUNGEON, TaskLocation.BRIMHAVEN_DUNGEON),
         HELLHOUND(31, TaskLocation.STRONGHOLD_SLAYER_CAVE, TaskLocation.CATACOMBS_OF_KOUREND, TaskLocation.TAVERLY_DUNGEON, TaskLocation.KARUULM_SLAYER_DUNGEON, TaskLocation.WITCHAVEN_DUNGEON),
         HYDRA(113, TaskLocation.KARUULM_SLAYER_DUNGEON),
+        IRON_DRAGON(59, TaskLocation.BRIMHAVEN_DUNGEON, TaskLocation.CATACOMBS_OF_KOUREND),
+        JELLY(50, TaskLocation.RELEKKA_SLAYER_DUNGEON, TaskLocation.CATACOMBS_OF_KOUREND),
         KALPHITE(53, TaskLocation.KALPHITE_LAIR, TaskLocation.KALPHITE_CAVE),
         KURASK(45, TaskLocation.RELEKKA_SLAYER_DUNGEON),
         LIZARDMEN(90, TaskLocation.BATTLEFRONT, TaskLocation.LIZARDMAN_CANYON, TaskLocation.LIZARDMAN_SETTLEMENT, TaskLocation.KEBOS_SWAMP, TaskLocation.MOLCH),
+        MITHRIL_DRAGON(93, TaskLocation.ANCIENT_CAVERN),
         NECHRYAEL(52, TaskLocation.CATACOMBS_OF_KOUREND, TaskLocation.SLAYER_TOWER),
         RUNE_DRAGON(109, TaskLocation.LITHKREN_VAULT),
         SKELETAL_WYVERN(72, TaskLocation.ASGARNIAN_ICE_DUNGEON),
         SMOKE_DEVIL(95, TaskLocation.SMOKE_DEVIL_DUNGEON),
+        STEEL_DRAGON(60, TaskLocation.BRIMHAVEN_DUNGEON, TaskLocation.CATACOMBS_OF_KOUREND),
         TROLL(18, TaskLocation.TROLL_STRONGHOLD, TaskLocation.DEATH_PLATEAU),
         TUROTH(36, TaskLocation.RELEKKA_SLAYER_DUNGEON),
-        //WATERFIEND("waterfiend", TaskLocation.KRAKEN_COVE),
+        VAMPYRES(34, TaskLocation.MEIYERDITCH, TaskLocation.DARKMEYER, TaskLocation.SLEPE),
         WYRM(111, TaskLocation.KARUULM_SLAYER_DUNGEON);
 
         private final int uid;
@@ -126,6 +129,12 @@ public class KonarData {
         LIZARDMAN_SETTLEMENT("Lizardman Settlement", 5175, 5431, 5275),
         KEBOS_SWAMP("Kebos Swamp", new Bounds(1232, 3589, 1332, 3644, -1)),
         MOLCH("Molch", new int[] { 5277 }, new Bounds(1272, 3639, 1328, 3690, -1)),
+        ANCIENT_CAVERN("Ancient Cavern", new Bounds(1730, 5274, 1790, 5370, -1)),
+        FREMENNIK_DUNGEON("Fremennik Slayer Dungeon", new Bounds(2691, 9987, 2814, 10046, -1)),
+        JORMUNGANDS_PRISON("Jormungand's Prison", new Bounds(2394, 10368, 2493, 10467, -1)),
+        DARKMEYER("Darkmeyer", new Bounds(3587, 3329, 3670, 3398, -1)),
+        MEIYERDITCH("Meiyerditch", new Bounds(3589, 3169, 3647, 3328, -1)),
+        SLEPE("Slepe", new Bounds(3681, 3264, 3775, 3413, -1))
         ;
 
         @Getter private final String name;
