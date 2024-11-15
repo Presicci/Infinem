b372e2bea1dfe0986d59bc553b4632f3f1c3acbd
Thomas Presicci <mrbennjerry@gmail.com>
2024-11-14 18:47:22 -0700

    Added support for automatically paginating options dialogues that are too long

--- infinem-server/src/main/java/io/ruin/model/inter/dialogue/OptionsDialogue.java
+++ infinem-server/src/main/java/io/ruin/model/inter/dialogue/OptionsDialogue.java
@@ -8,6 +8,8 @@ import io.ruin.model.inter.actions.SlotAction;
 import io.ruin.model.inter.utils.Option;
 import io.ruin.model.inter.utils.Unlock;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class OptionsDialogue extends Dialogue {
@@ -17,6 +19,7 @@ public class OptionsDialogue extends Dialogue {
     private final Option[] options;
 
     private boolean resend;
+    private int page;
 
     public OptionsDialogue(List<Option> options) {
         this(options.toArray(new Option[options.size()]));
@@ -34,8 +37,14 @@ public class OptionsDialogue extends Dialogue {
         params = new Object[]{title, ""};
         for(int i = 0; i < options.length; i++) {
             params[1] += options[i].name;
+            if (i == 3 && options.length > 5) {
+                params[1] += "|More...";
+            }
             if(i != options.length - 1)
                 params[1] += "|";
+        }
+        if (options.length > 5) {
+            params[1] += "|Back...";
         }
         this.options = options;
     }
@@ -45,7 +54,14 @@ public class OptionsDialogue extends Dialogue {
         player.optionsDialogue = this;
         player.openInterface(InterfaceType.CHATBOX, Interface.OPTIONS_DIALOGUE);
         player.getPacketSender().setAlignment(Interface.OPTIONS_DIALOGUE, 1, 0, -10);
-        player.getPacketSender().sendClientScript(58, "ss", params);
+        if (page == 0) {
+            player.getPacketSender().sendClientScript(58, "ss", params);
+        } else {
+            List<String> paramElements = Arrays.asList(((String) params[1]).split("\\|"));
+            String optionString = String.join("|", paramElements.subList(page * 5, paramElements.size()));
+            Object[] pageParams = new Object[]{params[0], optionString};
+            player.getPacketSender().sendClientScript(58, "ss", pageParams);
+        }
         new Unlock(Interface.OPTIONS_DIALOGUE, 1).children(1, 5).unlockSingle(player);
     }
 
@@ -54,9 +70,19 @@ public class OptionsDialogue extends Dialogue {
     }
 
     private void handle(Player player, int slot) {
-        int index = slot - 1;
+        int index = slot - 1 + (4 * page);
+        if (index == options.length && page > 0) {
+            page = 0;
+            open(player);
+            return;
+        }
         if(index < 0 || index >= options.length) {
             player.closeDialogue();
+            return;
+        }
+        if (index % 5 == 4 && options.length > (page + 1) * 5) {
+            ++page;
+            open(player);
             return;
         }
         options[index].select(player);
